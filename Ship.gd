453d014ee2da60d3f08920c2abe74d62c48509a3
nitaku <matteo.abrate@gmail.com>
2018-03-14 21:20:28 +0100

    Wrap around

--- Ship.gd
+++ Ship.gd
@@ -9,12 +9,18 @@ const ROTATION_SPEED = 0.1
 
 const RADIUS = 16
 
+var width
+var height
+
 export var color = Color(0,0,0)
 export var player = 'p1'
 
 func _ready():
 	$Circle.color = color
 	$Circle.radius = RADIUS
+	width = get_viewport().size.x
+	height = get_viewport().size.y
+	print(width)
 
 func _physics_process(delta):
 	left = Input.is_action_pressed(player+'_left')
@@ -30,6 +36,17 @@ func _physics_process(delta):
 		speed_multiplier = 3
 		
 	move_and_collide(velocity * speed_multiplier)
+	
+	# wrap
+	if position.x > width:
+		position.x -= width
+	elif position.x <= width:
+		position.x += width
+		
+	if position.y > height:
+		position.y -= height
+	elif position.y <= height:
+		position.y += height
 	
 	# dash recover
 	speed_multiplier = max(1, speed_multiplier - 0.1)
