11c7a60d45e72098e1b8a5c6814bd84897e9454e
nitaku <matteo.abrate@gmail.com>
2018-03-21 20:57:20 +0100

    Cleanup

--- Bomb.gd
+++ Bomb.gd
@@ -15,7 +15,6 @@ func _ready():
 func _physics_process(delta):
 	var collision = move_and_collide(velocity)
 	if collision != null:
-		print(collision.collider.name)
 		detonate()
 		
 	# remove bomb if far outside the screen
