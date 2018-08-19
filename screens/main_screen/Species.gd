192567e39d9f33852b26caca2895db15925ce3f7
salvob <tholalak@yahoo.it>
2018-08-19 20:22:56 +0200

    add shader in order to flip the Image. added p1 and p2 to the Menu

--- screens/main_screen/Species.gd
+++ screens/main_screen/Species.gd
@@ -1,13 +1,23 @@
 extends CenterContainer
 
+export (String) var left = "A"
+export (String) var right = "D"
+export (String) var fire = "1"
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 export (String) var species = "MANTIACS"
-export (String) var side = -1
+export (int) var side = -1
 func _ready():
-	$VBoxContainer/MarginContainer/TextureRect
+	print("My name is " + species + " and I have this side " + str(side))
+	var mater = $VBoxContainer/MarginContainer/TextureRect.get_material() 
+	if mater:
+		print(species)
+		mater.set_shader_param("flip",side < 0)
 	$VBoxContainer/Controls/VBoxContainer/Label.text = species
+	$VBoxContainer/Controls/VBoxContainer/Right/Panel/Key.text = right
+	$VBoxContainer/Controls/VBoxContainer/Left/Panel/Key.text = left
+	$VBoxContainer/Controls/CenterContainer/Fire/Panel/Key.text = fire
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	pass
