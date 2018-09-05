9e31f8156b60571061229135a9489d8f2dc17c41
salvob <tholalak@yahoo.it>
2018-09-05 20:22:30 +0200

    fix initial position unlocked sprites

--- screens/special_screen/Unlocked.gd
+++ screens/special_screen/Unlocked.gd
@@ -11,15 +11,18 @@ func _ready():
 	$Character.visible = false
 	global.unlocked_species.append(new_species)
 	global.unlocked += 1
-	yield(get_tree().create_timer(2.0), "timeout")
-	$Ship.texture = load("res://actors/"+new_species+"_ship.png")
-	$Character.texture = load("res://assets/character_"+new_species+"_1.png")
-	var sprite = get_node("Ship")
+	yield(get_tree().create_timer(1.0), "timeout")
 	var tween = get_node("Tween")
-	sprite.visible = true
-	$Character.visible = true
 	var size = get_viewport().size
-	tween.interpolate_method(sprite, "set_position", Vector2(0, 0), get_viewport().size/2, 2, Tween.TRANS_LINEAR, Tween.EASE_IN)
+
+	$Ship.texture = load("res://actors/"+new_species+"_ship.png")
+	$Ship.visible = true
+	$Ship.position = Vector2(0,0)
+	$Character.texture = load("res://assets/character_"+new_species+"_1.png")
+	$Character.visible = true
+	$Character.position = Vector2(0,size.y)
+	
+	tween.interpolate_method($Ship, "set_position", Vector2(0, 0), get_viewport().size/2, 2, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	#tween.interpolate_property(sprite, "position", get_viewport().size/2, Vector2(0, 0), 2, Tween.TRANS_LINEAR, Tween.EASE_IN, 2)
 	tween.interpolate_method($Character, "set_position", Vector2(0, size.y), Vector2(size.x/2+200, size.y/2), 2, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	tween.start()
