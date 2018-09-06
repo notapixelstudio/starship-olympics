15167330065d1ef295f736ad64988fc84e2a0734
nitaku <matteo.abrate@gmail.com>
2018-09-06 21:24:06 +0200

    multiple bombs level + configuration for two-player testing

--- screens/game_screen/levels/arena_mantiacs_1.gd
+++ screens/game_screen/levels/arena_mantiacs_1.gd
@@ -5,9 +5,9 @@ extends "res://screens/game_screen/Arena.gd"
 # var b = "textvar"
 
 func _ready():
+	pass
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	pass
 	
 	
 #func _process(delta):
