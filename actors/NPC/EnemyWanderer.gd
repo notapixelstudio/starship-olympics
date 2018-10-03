5bfff5c4e6bfcb20c7ada04319b9a95b03e9ac42
salvob <tholalak@yahoo.it>
2018-10-03 23:01:38 +0200

    added possibility to create a new random wanderer, pressing BACKSPACE

--- actors/NPC/EnemyWanderer.gd
+++ actors/NPC/EnemyWanderer.gd
@@ -3,14 +3,16 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-export (Vector2) var velocity = Vector2(6,6)
+export (Vector2) var velocity #  = Vector2(6,6)
+var possible_values = [-4, -5, -6, -7, -8, -9, 4, 5, 6, 7, 8, 9]
 
 var stopped = false
 var window_size = OS.window_size
 
 func _ready():
+	randomize()
 	if not velocity:
-		velocity = Vector2()
+		velocity = Vector2(randi() % len(possible_values), randi() % len(possible_values))
 
 func _physics_process(delta):
 	position += velocity
--- screens/game_screen/levels/arena_mantiacs_1.gd
+++ screens/game_screen/levels/arena_mantiacs_1.gd
@@ -9,8 +9,15 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	
-	
-#func _process(delta):
+var wanderer = preload("res://actors/NPC/EnemyWanderer.tscn")
+
+func _process(delta):
+	if Input.is_action_just_pressed("continue"):
+		var w = wanderer.instance()
+		w.position = Vector2(randi() % int(OS.window_size.x), randi() % int(OS.window_size.y))
+		$World/Battlefield.add_child(w)
+		
+		
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
