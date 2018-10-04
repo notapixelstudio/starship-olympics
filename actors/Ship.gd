6ff5c28938a09bfe2863b321182a1470640560ac
nitaku <matteo.abrate@gmail.com>
2018-10-04 20:18:35 +0200

    Nonworking attempt at physics-based ship movement

--- actors/Ship.gd
+++ actors/Ship.gd
@@ -1,12 +1,19 @@
 # script ship
-extends Area2D
+extends RigidBody2D
 
 var left
 var right
-export var velocity = Vector2(6,0)
+var max_velocity = 500
+var max_steer_force = 2500
+var thrust = 200
+
 var speed_multiplier = 1
 
-const ROTATION_SPEED = 0.1
+var steer_force = 0
+var rot = 0
+var rotation_dir = 0
+const ROTATION_SPEED = 100000000
+
 var count = 0
 signal died
 var alive = true 
@@ -37,67 +44,94 @@ func _ready():
 	height = get_node('/root/Arena').height
 
 func control(delta):
-	left = Input.is_action_pressed(player+'_left')
-	right = Input.is_action_pressed(player+'_right')
+	rotation_dir = 0
 	
-	if left and not right:
-		steer(-ROTATION_SPEED)
-	elif right and not left:
-		steer(ROTATION_SPEED)
-
+	if Input.is_action_pressed(player+'_left'):
+		rotation_dir -= 1
+		
+	if Input.is_action_pressed(player+'_right'):
+		rotation_dir += 1
+		
 	# dash
 	#if Input.is_action_just_pressed(player+'_dash') and dash_cooldown <= 0:
 	#	speed_multiplier = 3
 	#	$TrailParticles.emitting = true
 	#	dash_cooldown = 1
 	
+func _integrate_forces(state):
+	#steer_force = max_steer_force * rotation_dir
+	
+	set_applied_force(Vector2(thrust,0).rotated(rotation))
+	set_applied_torque(rotation_dir * 25000)
+	
+	# force the physics engine
+	var xform = state.get_transform()
+	
+	# wrap
+	if xform.origin.x > width:
+		xform.origin.x = 0
+	if xform.origin.x < 0:
+		xform.origin.x = width
+	if xform.origin.y > height:
+		xform.origin.y = 0
+	if xform.origin.y < 0:
+		xform.origin.y = height
+	
+	# clamp velocity
+	#state.linear_velocity = state.linear_velocity.clamped(max_velocity)
+	
+	# rotate: match rotation with velocity angle
+	#rot = state.linear_velocity.angle()
+	#rotation = rot
+	
+	state.set_transform(xform)
 
-func _physics_process(delta):
+func _process(delta):
 	if not alive:
 		return
 	control(delta)
 	
-	var actual_velocity = velocity * speed_multiplier
-	
-	position.x += actual_velocity.x
-	position.y += actual_velocity.y
+#func _physics_process(delta):
+#	if not alive:
+#		return
+#	control(delta)
+#	
+#	var actual_velocity = velocity * speed_multiplier
+#	
+#	position.x += actual_velocity.x
+#	position.y += actual_velocity.y
+#
+#	# fire
+#	if Input.is_action_just_pressed(player+'_fire') and fire_cooldown <= 0 and dash_cooldown <= 0:
+#		fire()
+#		fire_cooldown = 0.1
+#	# cooldown
+#	fire_cooldown -= delta
+#	dash_cooldown -= delta
+#	
+#	# wrap
+#	if position.x > width:
+#		position.x -= width
+#	elif position.x <= 0:
+#		position.x += width
+#		
+#	if position.y > height:
+#		position.y -= height
+#	elif position.y <= 0:
+#		position.y += height
+#		
+#	# trail
+#	if speed_multiplier > 1:
+#		var trail = Trail.instance()
+#		trail.position = position
+#		trail.rotation = rotation + PI/2
+#		get_parent().add_child(trail)
+#	else:
+#		$TrailParticles.emitting = false
+#		
+#	# dash recover
+#	#speed_multiplier = max(1, speed_multiplier - 0.1)
 
-	# fire
-	if Input.is_action_just_pressed(player+'_fire') and fire_cooldown <= 0 and dash_cooldown <= 0:
-		fire()
-		fire_cooldown = 0.1
-	# cooldown
-	fire_cooldown -= delta
-	dash_cooldown -= delta
-	
-	# wrap
-	if position.x > width:
-		position.x -= width
-	elif position.x <= 0:
-		position.x += width
-		
-	if position.y > height:
-		position.y -= height
-	elif position.y <= 0:
-		position.y += height
-		
-	# trail
-	if speed_multiplier > 1:
-		var trail = Trail.instance()
-		trail.position = position
-		trail.rotation = rotation + PI/2
-		get_parent().add_child(trail)
-	else:
-		$TrailParticles.emitting = false
-		
-	# dash recover
-	#speed_multiplier = max(1, speed_multiplier - 0.1)
-	
-# Steer the ship _rad_ radians clockwise.
-func steer(rad):
-	velocity = velocity.rotated(rad)
-	rotate(rad)
-	
 # Fire a bomb
 func fire():
 	var bomb = Bomb.instance()
