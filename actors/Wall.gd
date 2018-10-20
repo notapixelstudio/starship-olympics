936bef8a8a9125a617dc3f7d29868d58cf402ec8
nitaku <matteo.abrate@gmail.com>
2018-10-20 15:15:57 +0200

    Hollow walls (not working)

--- actors/Wall.gd
+++ actors/Wall.gd
@@ -4,8 +4,15 @@ extends StaticBody2D
 
 export (PoolVector2Array) var points setget set_points
 
+export (bool) var hollow setget set_hollow
+
 func set_points(pts):
 	points = pts
+	if has_node('CollisionShape2D'):
+		refresh()
+		
+func set_hollow(h):
+	hollow = h
 	if has_node('CollisionShape2D'):
 		refresh()
 		
@@ -13,12 +20,32 @@ func _ready():
 	refresh()
 	
 func refresh():
-	var shape = ConvexPolygonShape2D.new()
+	var shape
+	
 	if points == null or len(points) < 3:
 		points = PoolVector2Array([Vector2(-100,-100),Vector2(100,-100),Vector2(100,100),Vector2(-100,100)]) # clockwise!
-	shape.set_points(points)
+		
+	if hollow:
+		shape = ConcavePolygonShape2D.new()
+		
+		# create segments
+		var segments = PoolVector2Array()
+		for p in points:
+			segments.append(p)
+			segments.append(p)
+			
+		segments.remove(0)
+		segments.append(points[0])
+		
+		shape.set_segments(segments)
+	else:
+		shape = ConvexPolygonShape2D.new()
+		shape.set_points(points)
+		
 	$CollisionShape2D.set_shape(shape)
 	$Polygon2D.set_polygon(points)
+	
+	$Polygon2D.visible = not hollow
 	
 	# close the line with a seamless join
 	var ps = PoolVector2Array(points)
