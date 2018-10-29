66f0aee535ee09c3999568862205fa63d72abae2
salvob <tholalak@yahoo.it>
2018-10-30 00:37:51 +0100

    a bit more of visual feedback when you get the piece

--- screens/gameover_screen/Counter.gd
+++ screens/gameover_screen/Counter.gd
@@ -1,7 +1,15 @@
 extends NinePatchRect
 
+onready var tween = get_node("Tween")
+
 func change_life_texture(new_texture): 
 	get_node("life_texture").texture = new_texture
 	
-func get_lives(lives):
-	get_node("Number").text = str(lives)
+func get_lives(points):
+	if points == 0:
+		return
+	var label = get_node("Number")
+	tween.interpolate_property(label, "rect_scale", label.rect_scale, label.rect_scale *1.5, 0.5,tween.TRANS_BOUNCE, tween.EASE_IN)
+	tween.interpolate_property(label, "rect_scale", label.rect_scale*1.5, label.rect_scale, 0.5,tween.TRANS_BOUNCE, tween.EASE_IN, 0.5)
+	tween.start()
+	label.text = str(points)
