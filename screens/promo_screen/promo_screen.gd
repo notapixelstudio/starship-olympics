ff14008c12453d971b8a61611d32dbf0029d4b6f
salvob <tholalak@yahoo.it>
2018-10-30 18:13:42 +0100

    added more feedback to start game

--- screens/promo_screen/promo_screen.gd
+++ screens/promo_screen/promo_screen.gd
@@ -3,14 +3,20 @@ extends "res://screens/basic_screen.gd"
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
 		set_process_input(false)
+		$AnimationPlayer.play("ready")
+		yield($AnimationPlayer,"animation_finished")
 		change_scene()
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	Input.connect("joy_connection_changed", self, "_on_joy_connection_changed")
+	var joypads = Input.get_connected_joypads()
+	print(joypads)
+	if joypads:
+		print("NOT")
+		$Control.visible = not $Control.visible
 
-#func _process(delta):
+func _on_joy_connection_changed(device_id, connected):
+	$Control.visible = not $Control.visible
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
