196ff0ce172cb15705415bd9f7900622cb3072b7
salvob <tholalak@yahoo.it>
2018-12-19 10:17:32 +0100

    added also next signal. Need a bit of refactoring and clean up

--- selection/SelectionScreen.gd
+++ selection/SelectionScreen.gd
@@ -21,4 +21,6 @@ func get_previous(player_selection):
 	
 func get_next(player_selection):
 	print(player_selection.species)
-	print(ordered_species.find(player_selection.species))
+	var current_index = ordered_species.find(player_selection.species)
+	current_index = global.mod(current_index-1,len(ordered_species))
+	player_selection.change_species(ordered_species[current_index])
