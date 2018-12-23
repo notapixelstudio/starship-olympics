39f2808afa10ba558073c627024f979a339ec105
salvob <tholalak@yahoo.it>
2018-12-23 17:03:46 +0100

    extends Ship in order to address CPU Ships in #51.

--- actors/battlers/Ship.gd
+++ actors/battlers/Ship.gd
@@ -62,43 +62,7 @@ func _ready():
 	skin.initialize()
 	# load battlefield size
 	
-func control(delta):
-	rotation_dir = int(Input.is_action_pressed(controls+'_right')) - int(Input.is_action_pressed(controls+'_left'))
-		
-	# charge
-	if charging:
-		charge = charge+delta
-	else:
-		charge = 0
-		
-	# charge feedback
-	$Graphics/ChargeBar/Charge.set_point_position(1, $Graphics/ChargeBar/ChargeAxis.points[1] * min(charge,MAX_CHARGE)/MAX_CHARGE)
-	
-	# overcharge feedback
-	if charge > MAX_CHARGE + (MAX_OVERCHARGE-MAX_CHARGE)/2:
-		$Graphics/ChargeBar.visible = int(floor(charge * 15)) % 2
-		
-	if not charging and Input.is_action_just_pressed(controls+'_fire') and fire_cooldown <= 0:
-		charging = true
-		$Graphics/ChargeBar.visible = true
-		
-	# fire
-	if charging and Input.is_action_just_released(controls+'_fire'):
-		fire()
-		
-	# overcharge
-	if charge > MAX_OVERCHARGE:
-		fire()
-		
-	# cooldown
-	fire_cooldown -= delta
-		
-	# dash
-	#if Input.is_action_just_pressed(player+'_dash') and dash_cooldown <= 0:
-	#	speed_multiplier = 3
-	#	$TrailParticles.emitting = true
-	#	dash_cooldown = 1
-	
+
 func _integrate_forces(state):
 	steer_force = max_steer_force * rotation_dir
 	
@@ -125,6 +89,9 @@ func _integrate_forces(state):
 	# store velocity as a readable var
 	velocity = state.linear_velocity
 	state.set_transform(xform)
+
+func control(delta):
+	pass
 
 func _process(delta):
 	if not alive:
