69f1f9e7c514f4574f0c8ace4be6eb4b0c42470c
nitaku <matteo.abrate@gmail.com>
2019-01-13 21:35:37 +0100

    Types in CrownMode

--- combat/modes/CrownMode.gd
+++ combat/modes/CrownMode.gd
@@ -1,25 +1,25 @@
 extends Node
 
-const TIME_LEFT = 5*60 # 5 minutes
-var time_left = TIME_LEFT
+const TIME_LEFT:float = 5*60.0 # 5 minutes
+var time_left:float = TIME_LEFT
 
-const TARGET_SCORE = 2*60 # 2 minutes
-var scores = {}
+const TARGET_SCORE:float = 2*60.0 # 2 minutes
+var scores:Dictionary = {}
 var queen = null
 
-var game_over = false
+var game_over:bool = false
 
-func init(players):
+func init(players:Array):
 	for player in players:
 		scores[player] = 0
 		
-func crown_taken(player_id):
+func crown_taken(player_id:String):
 	queen = player_id
 	
 func crown_lost():
 	queen = null
 	
-func _process(delta):
+func _process(delta:float):
 	if game_over:
 		return
 		
