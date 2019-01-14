acf4fca391067ee017673ef92501a73f5b55c847
nitaku <matteo.abrate@gmail.com>
2019-01-14 17:54:19 +0100

    Score bars

---
+++ combat/interface/hud/HUD.gd
@@ -0,0 +1,26 @@
+extends HBoxContainer
+
+var Bar = preload('res://combat/interface/hud/Bar.tscn')
+
+var game_mode
+
+const colors = {
+	'mantiacs': Color(0,1,0,1),
+	'robolords': Color(1,0,0,1),
+	'trixens': Color(1,1,0,1),
+	'anothers': Color(0,0,1,1)
+}
+
+func initialize(_game_mode):
+	game_mode = _game_mode
+	
+	for player in game_mode.players:
+		var bar = Bar.instance()
+		$Bars.add_child(bar)
+		bar.initialize(colors[player.battler_template.species_name], game_mode.TARGET_SCORE)
+		bar.player = player
+		
+func _process(_delta):
+	for bar in $Bars.get_children():
+		bar.set_value(game_mode.scores[bar.player.name])
+		
--- combat/interface/hud_bars/bar/texture_progress.gd
+++
@@ -1,4 +0,0 @@
-extends TextureProgress
-
-func _on_Bar_maximum_changed(maximum):
-	max_value = maximum
