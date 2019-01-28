02ff641ac6419645e94f0dce7bf8ff766f8b9df7
salvob <tholalak@yahoo.it>
2019-01-28 18:51:21 +0100

    fix for references. starting renaming assets, bit perplexed, actually

--- selection/species/SpeciesTemplate.gd
+++ selection/species/SpeciesTemplate.gd
@@ -7,7 +7,6 @@ export (String) var species_name
 export (StreamTexture) var ship
 export (Texture) var character_ok
 export (Texture) var character_beaten
-export (Texture) var select_rect
 
 export var species_id : int
 
@@ -17,5 +16,3 @@ export var color : Color
 # graphic and animations of battlers
 export var ship_anim : PackedScene
 
-func change_rect(color:Color):
-    select_rect.modulate = color
