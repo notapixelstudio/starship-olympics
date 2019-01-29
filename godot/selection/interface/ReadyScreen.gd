586fe3b04e46289dbc1ca264b7ce78f8844406b0
salvob <tholalak@yahoo.it>
2019-01-29 14:50:11 +0100

    clean up import files

--- godot/selection/interface/ReadyScreen.gd
+++ godot/selection/interface/ReadyScreen.gd
@@ -1,19 +1,8 @@
 extends Control
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 onready var buttons = get_node("Choose_container")
 var num_players = 0
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
 func ready_to_fight(n_players):
 	num_players = n_players
 	buttons.get_node("Fight").text = str(n_players) + " players ready to fight!"
