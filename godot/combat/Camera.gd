34bc7ff957d97d2762d0338295a94fc7bc0f4f7e
salvob <tholalak@yahoo.it>
2019-02-10 14:46:54 +0100

    still a bit shaky, but with the right combination we'll be there

--- godot/combat/Camera.gd
+++ godot/combat/Camera.gd
@@ -5,7 +5,7 @@ export var speed = 3.0
 export var zoomspeed = 0.8
 export var zoommargin = 0.1
 
-export var zoomMin = 0.9
+export var zoomMin = 2
 export var marginX = 200.0
 export var marginY = 200.0
 export(float, 0.1, 0.5) var zoom_offset : float = 0.2
@@ -21,7 +21,7 @@ var margin_min = Vector2(0,0)
 var margin_max = Vector2(1280, 720)
 var rect_extents = Vector2(1280,720)
 var arena_size = Vector2()
-var zoomMax = 2.0
+var zoomMax = 1.0
 const PADDING = 200
 var ships
 const FRAME_DELAY = 20
@@ -40,6 +40,7 @@ func initialize(rect_extention:Vector2, _zoom_max:float):
 	set_process(enabled)
 
 func _process(delta: float) -> void:
+	ships = get_tree().get_nodes_in_group("players")
 	rect_extents = Vector2(zoom.x*margin_max.x, zoom.y*margin_max.y)/2
 	camera_rect = Rect2(ships[0].global_position, Vector2())
 	for ship in ships:
@@ -47,19 +48,20 @@ func _process(delta: float) -> void:
 	
 	var offset_to_be = calculate_center(camera_rect)
 	var zoom_to_be = calculate_zoom(camera_rect, viewport_rect.size)
-	
+	if abs(offset_to_be.x) < abs(offset.x) or abs(offset_to_be.x) < abs(offset.x):
+		wait_in_frame = FRAME_DELAY
 	if wait_in_frame < 0:
 		# change position
 		offset.x = lerp(offset.x, offset_to_be.x,speed * delta)
 		offset.y = lerp(offset.y, offset_to_be.y, speed * delta)
 		offset.x = clamp(offset.x, (rect_extents.x*2 - offset.x ), (arena_size.x-rect_extents.x))
-		offset.y = clamp(offset.y, (rect_extents.y*2 - offset.y ), (arena_size.y-rect_extents.y))
-		
+		offset.y = clamp(offset.y, (rect_extents.y*2 - offset.y), (arena_size.y-rect_extents.y))
+		"""
 		zoom.x = lerp(zoom.x, zoom_to_be.x, zoomspeed * delta)
 		zoom.y = lerp(zoom.y, zoom_to_be.y, zoomspeed * delta)
 		zoom.x = clamp(zoom_to_be.x, zoomMin, zoomMax)
 		zoom.y = clamp(zoom_to_be.y, zoomMin, zoomMax)
-		
+		"""
 	else:
 		print("We have to wait ", wait_in_frame, " frames")
 	
