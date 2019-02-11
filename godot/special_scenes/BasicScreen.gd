54d7db01a35b96746ab84e44dd5539612ff173fa
salvob <tholalak@yahoo.it>
2019-02-11 23:43:05 +0100

    starting skeleton for gametitle scene

--- godot/special_scenes/BasicScreen.gd
+++ godot/special_scenes/BasicScreen.gd
@@ -7,13 +7,10 @@ onready var transition = $Overlays/TransitionColor
 
 var transitioning = false
 func _ready():
-	pass
+	switch()
 
 
 func switch():
-	"""
-	
-	"""
 	if transitioning:
 		return
 		
@@ -23,9 +20,6 @@ func switch():
 	yield(transition.fade_from_color(), "completed")
 	transitioning = false
 	emit_signal("started")
-	# Get data from the battlers after the battle ended,
-	# Then copy into the Party node to save earned experience,
-	# items, and currentstats
 	emit_signal("finished")
 	transitioning = true
 	yield(transition.fade_to_color(), "completed")
