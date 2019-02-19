0b8a4670d58995edb4d2abd180198ef620876211
salvob <tholalak@yahoo.it>
2019-02-19 17:20:42 +0100

    added theme and updating the gametitle

--- godot/special_scenes/BasicScreen.gd
+++ godot/special_scenes/BasicScreen.gd
@@ -4,9 +4,13 @@ signal started()
 signal finished()
 
 onready var transition = $Overlays/TransitionColor
-
+onready var buttons = $Buttons
 var transitioning = false
+
 func _ready():
+	for button in buttons.get_children():
+		button.disabled = false
+	buttons.get_child(0).grab_focus()
 	switch()
 
 
