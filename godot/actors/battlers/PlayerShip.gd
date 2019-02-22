db12e9f0ffcb6820231f973450d34a0c5de2854b
SalvoB <salvob41@users.noreply.github.com>
2019-02-22 18:20:44 +0100

    new ways of controls (#143)
    * Experiment with new way of handling ships in combat
    
    * better handling changes of direction
    
    * Fix joypad controls: when nothing has been touched, the normalized vector for the target_velocity was a 0.7, 0.7 resulting in the ships going on the right-down sector of the battlefield. Added deadzone for analogic movement
    
    * charged shot stops the ship
    
    * Tweaks to new movement
    
    * Depending on the input device you're using, it will change from absolute to relative
    
    * reducing the vibration

--- godot/actors/battlers/PlayerShip.gd
+++ godot/actors/battlers/PlayerShip.gd
@@ -1,20 +1,20 @@
 extends Ship
 
 const THRESHOLD = 0.07
-export var absolute_controls : bool= true
+const DEADZONE = 0.5
+
 var device_controller_id : int
 func _ready():
 	device_controller_id = InputMap.get_action_list(controls+"_right")[0].device
-	
 	if "joy" in controls:
 		print("THIS IS A CONTROLLER ")
 	if "kb" in controls:
 		absolute_controls = false
 		
 func die():
-	print("DIE FROM PLAYER")
-	# randomize the vibration
-	Input.start_joy_vibration(device_controller_id,1.0,1.0,2)   
+	if "joy" in controls:
+		# Vibrate if joypad
+		Input.start_joy_vibration(device_controller_id, 0.5, 0.5, 1)   
 	.die()
 	
 func joypad_handling():
@@ -31,25 +31,33 @@ func joypad_handling():
 	# xAxis is a value from +/0 0-1 depending on how hard the stick is being pressed
 	target.x = Input.get_joy_axis(analogic.device, JOY_AXIS_0)  
 	target.y = Input.get_joy_axis(analogic.device, JOY_AXIS_1)
-	
+	if abs(target.x) < DEADZONE:
+		target.x = 0
+	if abs(target.y) < DEADZONE:
+		target.y = 0
 	return target
 			
 func control(delta):
-	if absolute_controls:
-		var target_vel = Vector2()
-		# Check for joypad
-		if "joy" in controls:
-			target_vel = joypad_handling()
-		else:
-			target_vel.y = int(Input.is_action_pressed(controls+'_down')) - int(Input.is_action_pressed(controls+'_up'))
-			target_vel.x = int(Input.is_action_pressed(controls+'_right')) - int(Input.is_action_pressed(controls+'_left'))
-		
+	var target_vel = Vector2()
+	if "joy" in controls:
+		target_vel = joypad_handling()
+			
+	if not target_vel == Vector2():
+		absolute_controls = true
+		# this works for absolute controls in keyboard
+		# target_vel.y = int(Input.is_action_pressed(controls+'_down')) - int(Input.is_action_pressed(controls+'_up'))
+		# target_vel.x = int(Input.is_action_pressed(controls+'_right')) - int(Input.is_action_pressed(controls+'_left'))
 		var front = Vector2(cos(rotation), sin(rotation))
+		
 		if target_vel == Vector2():
-			rotation_dir = 0
-		rotation_dir = find_side(Vector2(0,0), front, target_vel)
+			# this in order to keep the velocity constant
+			pass
+		else:
+			target_velocity = target_vel.normalized()
+		rotation_dir = find_side(Vector2(0,0), front, target_velocity)
 		
 	else:
+		absolute_controls = false
 		rotation_dir = int(Input.is_action_pressed(controls+'_right')) - int(Input.is_action_pressed(controls+'_left'))
 		
 	# charge
