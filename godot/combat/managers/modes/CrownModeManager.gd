b44f1c79515e1f1327a8e2bed3c4b1bea4cb99ca
nitaku <matteo.abrate@gmail.com>
2019-03-21 18:55:13 +0100

    Enable/disable mode managers + default mode: crown

--- godot/combat/managers/modes/CrownModeManager.gd
+++ godot/combat/managers/modes/CrownModeManager.gd
@@ -1,9 +1,12 @@
-extends Manager
+extends ModeManager
 
 # move crown -> royal code here
 
 signal score
 func _process(delta):
+	if not enabled:
+		return
+		
 	for royal in ECM.entities_with('Royal'):
 		assert royal.get_host() is Ship
 		
--- godot/combat/managers/modes/DeathmatchModeManager.gd
+++ godot/combat/managers/modes/DeathmatchModeManager.gd
@@ -1,7 +1,10 @@
-extends Manager
+extends ModeManager
 
 signal score
 func _on_ship_killed(ship : Ship, killer : Ship):
+	if not enabled:
+		return
+		
 	if killer and killer.species != ship.species:
 		emit_signal('score', killer.species, 5)
 	else:
