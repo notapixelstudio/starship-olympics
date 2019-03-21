3d8ca18512d8aa14d75f94cb504575aa5a9252a4
nitaku <matteo.abrate@gmail.com>
2019-03-21 18:30:49 +0100

    Less points per kill

--- godot/combat/managers/modes/DeathmatchModeManager.gd
+++ godot/combat/managers/modes/DeathmatchModeManager.gd
@@ -3,7 +3,7 @@ extends Manager
 signal score
 func _on_ship_killed(ship : Ship, killer : Ship):
 	if killer and killer.species != ship.species:
-		emit_signal('score', killer.species, 10)
+		emit_signal('score', killer.species, 5)
 	else:
-		emit_signal('score', ship.species, -10)
+		emit_signal('score', ship.species, -5)
 		
