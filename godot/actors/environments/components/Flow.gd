c6c0a6d3799473c47b39647d63a0016aea08ccc0
Matteo Abrate <matteo.abrate@gmail.com>
2019-03-25 22:40:41 +0100

    Trixen levels (#230)
    * Flow are becoming Attractors
    
    * Improvements on attractor fields

--- godot/actors/environments/components/Flow.gd
+++ godot/actors/environments/components/Flow.gd
@@ -1,7 +1,16 @@
 extends Component
 
-export var flow_vector : Vector2
+enum TYPE { center, direction }
+export(TYPE) var type = TYPE.center
 
-func get_flow_vector():
-	return flow_vector
+export var vector_or_point : Vector2 = Vector2(0,0)
+export var charge : int = -1 # attractive
+
+func get_flow_vector(pos) -> Vector2:
+	if type == TYPE.center:
+		return (pos-(get_host().position+vector_or_point)).normalized() * charge # mmm a bit of logic here
+	elif type == TYPE.direction:
+		return vector_or_point.normalized() * charge
+	else:
+		return Vector2() # this is to quiet the compiler
 	
