04fdd99676a08f2e4b8f39be575f444466b8f5f3
nitaku <matteo.abrate@gmail.com>
2019-03-28 22:54:56 +0100

    Cargo, collectables and crown separated

--- godot/combat/managers/EnvironmentsManager.gd
+++ godot/combat/managers/EnvironmentsManager.gd
@@ -3,6 +3,7 @@ extends Manager
 # WARNING this implementation works only if a field never changes components while an object is inside it
 # FIXME need to switch to a continuous check + timeout
 
+signal repel_cargo
 func _on_sth_entered(sth, other):
 	var sth_entity = ECM.E(sth)
 	var other_entity = ECM.E(other)
@@ -17,8 +18,8 @@ func _on_sth_entered(sth, other):
 		other_entity.get_node('Flowing').set_flow(sth_entity.get_node('Flow'))
 		other_entity.get_node('Flowing').enable()
 		
-	if sth_entity.has('CrownDropper') and other_entity.get_host() is Ship: # FIXME use Royal
-		other_entity.get_host().drop()
+	if sth_entity.has('CrownDropper') and other_entity.has('Cargo') and other_entity.get('Cargo').what is Crown:
+		emit_signal('repel_cargo', other_entity.get_host())
 
 	if sth_entity.has('Throne') and other_entity.could_have('Royal'):
 		other_entity.get_node('Royal').enable()
@@ -35,9 +36,6 @@ func _on_sth_exited(sth, other):
 		
 	if sth_entity.has('Flow') and other_entity.has('Flowing'):
 		other_entity.get_node('Flowing').disable()
-		
-	if sth_entity.has('CrownDropper') and other_entity.get_host() is Ship: # FIXME use Royal
-		other_entity.get_host().drop()
 		
 	if sth_entity.has('Throne') and other_entity.has('Royal'):
 		other_entity.get_node('Royal').disable()
--- godot/combat/managers/modes/CrownModeManager.gd
+++ godot/combat/managers/modes/CrownModeManager.gd
@@ -1,7 +1,13 @@
 extends ModeManager
 
-# move crown -> royal code here
-
+func _on_sth_collected(collector, collectee):
+	if collectee is Crown and ECM.E(collector).could_have('Royal'):
+		ECM.E(collector).get('Royal').enable()
+		
+func _on_sth_dropped(dropper, droppee):
+	if droppee is Crown and ECM.E(dropper).has('Royal'):
+		ECM.E(dropper).get('Royal').disable()
+		
 signal score
 func _process(delta):
 	if not enabled:
