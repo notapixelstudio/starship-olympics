a7b09896f38d563af321bed8d4e30d1e2c253379
salvob <tholalak@yahoo.it>
2019-03-28 18:49:30 +0100

    bars juice with tween

--- godot/combat/interface/hud/Bar.gd
+++ godot/combat/interface/hud/Bar.gd
@@ -1,7 +1,10 @@
-extends Control
+extends Node2D
+
+class_name Bar
 
 onready var progressbar = $ProgressBar
 var player
+var new_position setget change_position
 
 func initialize(color, max_value):
 	progressbar.modulate = color
@@ -9,4 +12,15 @@ func initialize(color, max_value):
 	
 func set_value(value):
 	progressbar.value = value
+
+func get_value():
+	return progressbar.value
+
+func change_position(new_value):
+	new_position = new_value
+	if position != new_position and not $Tween.is_active():
+		print("We should do something ", position, " ", new_position) 
+		$Tween.interpolate_property(self, "position", self.position, new_position, 0.5, 
+			Tween.TRANS_CUBIC, Tween.EASE_IN, 0)
+		$Tween.start()
 	
