0403fdcea1ae7b02a9269499aa290f794d9df4c1
nitaku <matteo.abrate@gmail.com>
2019-03-29 21:12:17 +0100

    Tweaks to deathmatch scoring, time and some levels

--- godot/combat/managers/modes/DeathmatchModeManager.gd
+++ godot/combat/managers/modes/DeathmatchModeManager.gd
@@ -8,5 +8,5 @@ func _on_ship_killed(ship : Ship, killer : Ship):
 	if killer and killer.species != ship.species:
 		emit_signal('score', killer.species, 8)
 	else:
-		emit_signal('score', ship.species, -5)
+		emit_signal('score', ship.species, -3)
 		
