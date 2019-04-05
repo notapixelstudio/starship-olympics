44f4416fccc3e5f2404179f19b742063eeca11c6
nitaku <matteo.abrate@gmail.com>
2019-04-05 23:49:27 +0200

    Trail disappears when speed is zero

--- godot/actors/battlers/Trail.gd
+++ godot/actors/battlers/Trail.gd
@@ -3,9 +3,11 @@ extends Node2D
 class_name Trail
 
 var ship : Ship
+var ship_e : Entity
 
 func initialize(_ship : Ship):
 	ship = _ship
+	ship_e = ECM.E(ship)
 	ship.connect('spawned', self, '_on_ship_spawned')
 	ship.connect('dead', self, '_on_ship_dead')
 	update()
@@ -16,6 +18,7 @@ func _process(delta):
 func update():
 	position = ship.position
 	rotation = ship.rotation
+	$Particles2D.emitting = ship_e.has('Thrusters') and ship_e.get('Thrusters').get_speed() > 0
 	
 func _on_ship_spawned(ship : Ship):
 	update()
