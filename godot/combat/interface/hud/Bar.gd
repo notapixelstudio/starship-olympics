ba75cc24cf7717ed91da96734ea679ed2a757606
SalvoB <salvob41@users.noreply.github.com>
2019-04-07 20:35:16 +0200

    Combat hud juice (#253)
    * skeleton for sprite next to bar
    
    * added ship plain and darkened as tomb
    
    * HUD bars cleanup and focus when someone is scoring
    
    * Mini-ships in bars

--- godot/combat/interface/hud/Bar.gd
+++ godot/combat/interface/hud/Bar.gd
@@ -3,16 +3,30 @@ extends Node2D
 class_name Bar
 
 onready var progressbar = $ProgressBar
+onready var sprite = $Ship/Sprite
+
+var sprite_off
+var sprite_on
 var player
 var new_position setget change_position
 
-func initialize(color, max_value):
-	progressbar.modulate = color
+func initialize(species_template: SpeciesTemplate, max_value):
+	progressbar.modulate = species_template.color
 	progressbar.max_value = max_value
+	sprite_on = species_template.ship
+	sprite_off = species_template.ship_off
+	sprite.texture = sprite_off
+	
 	
 func set_value(value):
+	# round to 2 decimal digit
+	if stepify(value, 0.01) > progressbar.value:
+		sprite.texture = sprite_on
+	else:
+		sprite.texture = sprite_off
 	progressbar.value = value
-
+	$Ship.position.x = progressbar.value*progressbar.rect_size.x/progressbar.max_value
+	
 func get_value():
 	return progressbar.value
 
