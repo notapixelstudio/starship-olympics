e9cbfed68170b4cc20ec3b0bd4e8c500aeb2c534
nitaku <matteo.abrate@gmail.com>
2019-04-09 10:55:49 +0200

    Skeleton for HUD graphics

--- godot/combat/interface/hud/HUD.gd
+++ godot/combat/interface/hud/HUD.gd
@@ -1,29 +1,35 @@
-extends HBoxContainer
+extends Control
 
 var Bar = preload('res://combat/interface/hud/Bar.tscn')
 
 var game_mode
 
+onready var Bars = $Content/Bars
+onready var TimeLeft = $Content/ModePanel/TimeLeft
+
 func initialize(_game_mode):
 	game_mode = _game_mode
 	print("initializing HUD")
-	$TimeLeft.text = str(int(floor(game_mode.time_left)))
+	TimeLeft.text = str(int(floor(game_mode.time_left)))
 	var i = 0
 
 	for player in game_mode.scores:
 		var bar = Bar.instance()
 		bar.position += Vector2(0, 25)*i
-		$Bars.add_child(bar)
+		Bars.add_child(bar)
 		bar.initialize(player.species_template, game_mode.TARGET_SCORE)
 		bar.player = player
 		i+=1
+		
+	# adjust background
+	$Content/BarsBackground.rect_size.y = 15 + 25 * len(game_mode.scores)
 
 func _process(_delta):
 	# update time left
-	$TimeLeft.text = str(int(floor(game_mode.time_left)))
+	TimeLeft.text = str(int(floor(game_mode.time_left)))
 
 	# update scores
-	var bars = $Bars.get_children()
+	var bars = Bars.get_children()
 	for bar in bars:
 		var player : InfoPlayer = game_mode.scores_index[bar.player.species]
 		bar.set_value(player.score)
