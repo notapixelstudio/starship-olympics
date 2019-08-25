b97bcf54d79b75b14dbaca2aac0bf081803e1afc
salvob <tholalak@yahoo.it>
2019-08-25 20:22:40 +0200

    Enabling and refactoring main and Title screen

--- godot/special_scenes/title_screen/TitleScreen.gd
+++ godot/special_scenes/title_screen/TitleScreen.gd
@@ -13,16 +13,16 @@ func initialize():
 	animation.stop(true)
 	animation.play("fade_in")
 	yield(animation, "animation_finished")
+	enable_buttons()
 	for button in buttons.get_children():
 		button.disabled = false
 	buttons.get_child(0).grab_focus()
 	emit_signal("entered")
 	can_press = true
 	
+	
 func _ready():
-	# disable all buttons at first
-	for button in buttons.get_children():
-		button.disabled = true
+	disable_buttons()
 	Soundtrack.play("Lobby5", true)
 	initialize()
 
@@ -31,17 +31,10 @@ func _on_Options_pressed():
 	yield(animation, "animation_finished")
 	emit_signal("option_selected")
 
-func _input(event):
-	if can_press:
-		if event is InputEventKey or event is InputEventJoypadButton: 
-			enable_buttons()
-
 	
 func enable_buttons():
 	can_press = false
 	$Buttons.visible = true
-	$Description2.visible = false
-	$Description2/AnimationPlayer.stop()
 	yield(get_tree().create_timer(5), "timeout")
 	can_press = true
 	for button in $Buttons.get_children():
@@ -56,3 +49,12 @@ func _on_StartHuman_pressed():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func disable_buttons():
+	buttons.visible = false
+	# disable all buttons at first
+	for button in buttons.get_children():
+		button.disabled = true
+
+func _exit_tree():
+	disable_buttons()
