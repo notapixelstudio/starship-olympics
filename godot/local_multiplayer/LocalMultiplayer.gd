926d3b6c0e90cfa1dc478d841e5afe308ad9cbe2
salvob <tholalak@yahoo.it>
2019-10-31 01:15:39 +0100

    disabled keyboards when in demo mode

--- godot/local_multiplayer/LocalMultiplayer.gd
+++ godot/local_multiplayer/LocalMultiplayer.gd
@@ -28,6 +28,10 @@ var players : Dictionary
 
 signal updated
 
+func _unhandled_input(event):
+	if event.is_action_pressed("force_pause"):
+		get_tree().reload_current_scene()
+
 func from_species_to_info_player(selection_species: Species) -> InfoPlayer:
 	var info_player = InfoPlayer.new()
 	info_player.id = selection_species.name
