2c561c132df7d24c561aee1172193ee18bcc2fcc
salvob <tholalak@yahoo.it>
2019-11-12 17:10:52 +0100

    small fix for the CPU, everything except their cells are valuable

--- godot/combat/managers/ConquestManager.gd
+++ godot/combat/managers/ConquestManager.gd
@@ -12,6 +12,10 @@ func _on_ship_collided(other : CollisionObject2D, ship : Ship):
 	if ECM.E(ship).has('Conqueror') and entity.has('Conquerable'):
 		var species = ECM.E(ship).get('Conqueror').get_species()
 		
+		# AI
+		if ship.cpu:
+			entity.get('Valuable').disable()
+		
 		if entity.get('Conquerable').get_species() != null and entity.get('Conquerable').get_species() != species:
 		#	# converting back a cell is delayed
 			yield(get_tree().create_timer(0.5),'timeout')
@@ -22,8 +26,7 @@ func _on_ship_collided(other : CollisionObject2D, ship : Ship):
 			entity.get('Conquerable').set_species(species)
 			emit_signal('conquered', species, entity.get_host())
 			
-			# AI
-			entity.get('Valuable').disable()
+
 		
 	if entity.has('Fillable') and entity.could_have('Fluid'):
 		entity.get_node('Fluid').enable()
