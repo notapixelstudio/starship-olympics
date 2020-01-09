ce30c545f60a34034811831bb15fddc3d4533f62
salvob <tholalak@yahoo.it>
2020-01-09 14:27:32 +0100

    self bombs not triggered, but re-enabled self-kill

--- godot/combat/managers/CombatManager.gd
+++ godot/combat/managers/CombatManager.gd
@@ -11,8 +11,8 @@ func _on_ship_collided(other : CollisionObject2D, ship : Ship):
 	if entity.has('Deadly'):
 		if entity.has('Owned') and entity.get('Owned').get_owned_by() != ship:
 			ship.die(entity.get('Owned').get_owned_by())
-		# else:
-		#	ship.die(null)
+		else:
+			ship.die(null)
 			
 func bomb_near_area_entered(other : CollisionObject2D, bomb : Bomb):
 	var entity = ECM.E(other)
