cd4ce4fc274a78bc6cf1f05657e995036ef96c37
nitaku <matteo.abrate@gmail.com>
2020-02-23 02:58:51 +0100

    first attempt

---
+++ godot/actors/environments/Rock.gd
@@ -0,0 +1,40 @@
+extends RigidBody2D
+
+class_name Rock
+var RockScene = load('res://actors/environments/Rock.tscn')
+var DiamondScene = load('res://combat/collectables/Diamond.tscn')
+
+export var order : int = 4
+
+var gshape
+var breakable = false
+
+func _ready():
+	gshape = GRegularPolygon.new()
+	gshape.sides = 4
+	gshape.radius = 50*pow(2,order)
+	
+	$Polygon2D.polygon = gshape.to_PoolVector2Array()
+	$Line2D.points = gshape.to_closed_PoolVector2Array()
+	$CollisionShape2D.shape = gshape.to_Shape2D()
+	$Area2D/CollisionShape2D.shape = gshape.to_Shape2D()
+	
+func _on_Area2D_area_entered(area):
+	if breakable and area is Explosion:
+		queue_free()
+		for i in range(4):
+			var child
+			if order > 1:
+				child = RockScene.instance()
+				child.mass = mass/4
+				child.order = order - 1
+			else:
+				child = DiamondScene.instance()
+			child.position = position + Vector2(gshape.radius*0.4,0).rotated(2*PI/4*i)
+			child.linear_velocity = 0.25*linear_velocity + Vector2(50*order,0).rotated(2*PI/4*i)
+			get_parent().call_deferred("add_child", child)
+		
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == 'Forming':
+		breakable = true
+		
