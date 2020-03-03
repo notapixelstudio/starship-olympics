1c62ed7813499475a508d3582954c7844a23ae47
nitaku <matteo.abrate@gmail.com>
2020-03-03 22:25:05 +0100

    background in headshot

--- godot/special_scenes/combat_UI/session_points/Headshot.gd
+++ godot/special_scenes/combat_UI/session_points/Headshot.gd
@@ -12,5 +12,10 @@ func set_species(value: Species):
 		return
 	if species:
 		$Sprite.texture = species.character_ok
+		$Polygon2D.modulate = species.color
+		$Line2D.visible = true
 	else:
 		$Sprite.texture = null
+		$Polygon2D.modulate = Color(1,1,1,0)
+		$Line2D.visible = false
+		
