6b21cf74278ac09f7b8ed6b1b3cf04cd860a7ed5
nitaku <matteo.abrate@gmail.com>
2020-03-08 03:42:58 +0100

    functional 2 players mining

--- godot/actors/environments/Rock.gd
+++ godot/actors/environments/Rock.gd
@@ -3,16 +3,16 @@ extends RigidBody2D
 class_name Rock
 var RockScene = load('res://actors/environments/Rock.tscn')
 var DiamondScene = load('res://combat/collectables/Diamond.tscn')
+var BigDiamondScene = load('res://combat/collectables/BigDiamond.tscn')
 
 export var order : int = 4
 
 var gshape
-var breakable = false
 
 func _ready():
 	gshape = GRegularPolygon.new()
 	gshape.sides = 4
-	gshape.radius = 50*pow(2,order)
+	gshape.radius = 45*pow(2,order)
 	
 	$Polygon2D.polygon = gshape.to_PoolVector2Array()
 	$Line2D.points = gshape.to_closed_PoolVector2Array()
@@ -23,27 +23,31 @@ func _ready():
 	$Line2D.texture_mode = Line2D.LINE_TEXTURE_TILE
 	
 func _on_Area2D_body_entered(body):
-	if body is Bomb or  body is Ship and not body.invincible:
-		try_break()
+	if body is Bomb or body is Ship and not body.invincible:
+		queue_free()
 		
-func try_break():
-	if not breakable:
-		return
+		if order < 1:
+			return
+		
+		for i in range(4):
+			var child
+			if order > 1:
+				child = new_child_rock()
+			else:
+				var chance = randf()
+				if chance < 0.025:
+					child = BigDiamondScene.instance()
+				elif chance < 0.33:
+					child = new_child_rock()
+				else:
+					child = DiamondScene.instance()
+			child.position = position + Vector2(gshape.radius*0.4,0).rotated(2*PI/4*i)
+			child.linear_velocity = 0.25*linear_velocity + Vector2(50*order,0).rotated(2*PI/4*i)
+			get_parent().call_deferred("add_child", child)
+			
+func new_child_rock():
+	var child = RockScene.instance()
+	child.mass = mass/4
+	child.order = order - 1
+	return child
 	
-	queue_free()
-	for i in range(4):
-		var child
-		if order > 1:
-			child = RockScene.instance()
-			child.mass = mass/4
-			child.order = order - 1
-		else:
-			child = DiamondScene.instance()
-		child.position = position + Vector2(gshape.radius*0.4,0).rotated(2*PI/4*i)
-		child.linear_velocity = 0.25*linear_velocity + Vector2(50*order,0).rotated(2*PI/4*i)
-		get_parent().call_deferred("add_child", child)
-		
-func _on_AnimationPlayer_animation_finished(anim_name):
-	if anim_name == 'Forming':
-		breakable = true
-		
--- godot/combat/Arena.gd
+++ godot/combat/Arena.gd
@@ -364,6 +364,10 @@ func ship_just_died(ship, killer):
 	
 	$Battlefield.call_deferred("remove_child", ship)
 	$Battlefield.call_deferred("add_child", ship.dead_ship_instance)
+	
+	ship.dead_ship_instance.apply_central_impulse(-ship.linear_velocity)
+	ship.dead_ship_instance.apply_torque_impulse(ship.linear_velocity.length()*20)
+	
 	var respawn_timeout = 1.5
 	if crown_mode.enabled:
 		if len(ECM.entities_with('Royal')) > 0:
@@ -496,8 +500,9 @@ func _on_sth_collected(collector, collectee):
 	if collectee.get_parent().is_in_group("spawner_group"):
 		collectee.get_parent().call_deferred('remove', collectee)
 	else:
-		$Battlefield.call_deferred('remove_child', collectee) # collisions do not work as expected without defer
-
+		#$Battlefield.call_deferred('remove_child', collectee) # collisions do not work as expected without defer
+		collectee.call_deferred('queue_free')
+		
 func _on_sth_dropped(dropper, droppee):
 	$Battlefield.add_child(droppee)
 	droppee.position = dropper.position
