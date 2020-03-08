7a01e4e3b5d120021eb6935e278cca8748f6dbdf
nitaku <matteo.abrate@gmail.com>
2020-03-08 03:56:43 +0100

    tweaks

--- godot/actors/environments/Rock.gd
+++ godot/actors/environments/Rock.gd
@@ -8,11 +8,12 @@ var BigDiamondScene = load('res://combat/collectables/BigDiamond.tscn')
 export var order : int = 4
 
 var gshape
+var breakable = false
 
 func _ready():
 	gshape = GRegularPolygon.new()
 	gshape.sides = 4
-	gshape.radius = 45*pow(2,order)
+	gshape.radius = 50*pow(2,order)
 	
 	$Polygon2D.polygon = gshape.to_PoolVector2Array()
 	$Line2D.points = gshape.to_closed_PoolVector2Array()
@@ -24,30 +25,39 @@ func _ready():
 	
 func _on_Area2D_body_entered(body):
 	if body is Bomb or body is Ship and not body.invincible:
-		queue_free()
+		try_break()
 		
-		if order < 1:
-			return
-		
-		for i in range(4):
-			var child
-			if order > 1:
+func try_break():
+	if not breakable:
+		return
+	
+	queue_free()
+	
+	if order < 1:
+		return
+	
+	for i in range(4):
+		var child
+		if order > 1:
+			child = new_child_rock()
+		else:
+			var chance = randf()
+			if chance < 0.025:
+				child = BigDiamondScene.instance()
+			elif chance < 0.33:
 				child = new_child_rock()
 			else:
-				var chance = randf()
-				if chance < 0.025:
-					child = BigDiamondScene.instance()
-				elif chance < 0.33:
-					child = new_child_rock()
-				else:
-					child = DiamondScene.instance()
-			child.position = position + Vector2(gshape.radius*0.4,0).rotated(2*PI/4*i)
-			child.linear_velocity = 0.25*linear_velocity + Vector2(50*order,0).rotated(2*PI/4*i)
-			get_parent().call_deferred("add_child", child)
-			
+				child = DiamondScene.instance()
+		child.position = position + Vector2(gshape.radius*0.4,0).rotated(2*PI/4*i)
+		child.linear_velocity = 0.25*linear_velocity + Vector2(50*order,0).rotated(2*PI/4*i)
+		get_parent().call_deferred("add_child", child)
+		
 func new_child_rock():
 	var child = RockScene.instance()
 	child.mass = mass/4
 	child.order = order - 1
 	return child
+	
+func become_breakable():
+	breakable = true
 	
