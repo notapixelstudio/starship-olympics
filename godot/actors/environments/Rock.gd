42a921d65d0056de5abed041536ffebcfb07dd6c
nitaku <matteo.abrate@gmail.com>
2020-03-09 01:53:03 +0100

    tweaks and graphics

--- godot/actors/environments/Rock.gd
+++ godot/actors/environments/Rock.gd
@@ -11,14 +11,25 @@ var gshape
 var breakable = false
 
 func _ready():
-	gshape = GRegularPolygon.new()
-	gshape.sides = 4
-	gshape.radius = 50*pow(2,order)
-	
+	gshape = GBeveledRect.new()
+	gshape.width = 2*51/sqrt(2)*pow(2,order)
+	gshape.height = gshape.width
+	gshape.bevel = gshape.width*0.2
 	$Polygon2D.polygon = gshape.to_PoolVector2Array()
 	$Line2D.points = gshape.to_closed_PoolVector2Array()
+	var epoints = gshape.to_PoolVector2Array()
+	var ipoints = gshape.to_PoolVector2Array_offset(Vector2(0,0), 0.7)
+	$LightLine2D.points = PoolVector2Array([ipoints[0],ipoints[2]])
+	$LightLine2D2.points = PoolVector2Array([ipoints[2],ipoints[4]])
+	$LightLine2D3.points = PoolVector2Array([ipoints[4],ipoints[6]])
+	$LightLine2D4.points = PoolVector2Array([ipoints[6],ipoints[0]])
+	$LightLine2DE.points = PoolVector2Array([epoints[0], ipoints[0],epoints[1]])
+	$LightLine2DE2.points = PoolVector2Array([epoints[2], ipoints[2],epoints[3]])
+	$LightLine2DE3.points = PoolVector2Array([epoints[4], ipoints[4],epoints[5]])
+	$LightLine2DE4.points = PoolVector2Array([epoints[6], ipoints[6],epoints[7]])
 	$CollisionShape2D.shape = gshape.to_Shape2D()
 	$Area2D/CollisionShape2D.shape = gshape.to_Shape2D()
+	rotation_degrees = 45
 	
 	# workaround
 	$Line2D.texture_mode = Line2D.LINE_TEXTURE_TILE
@@ -31,6 +42,7 @@ func try_break():
 	if not breakable:
 		return
 	
+	breakable = false
 	queue_free()
 	
 	if order < 1:
@@ -48,7 +60,7 @@ func try_break():
 				child = new_child_rock()
 			else:
 				child = DiamondScene.instance()
-		child.position = position + Vector2(gshape.radius*0.4,0).rotated(2*PI/4*i)
+		child.position = position + Vector2(gshape.width/2*sqrt(2)*0.4,0).rotated(2*PI/4*i)
 		child.linear_velocity = 0.25*linear_velocity + Vector2(50*order,0).rotated(2*PI/4*i)
 		get_parent().call_deferred("add_child", child)
 		
