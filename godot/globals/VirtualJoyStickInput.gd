022c3e58458d36f301e2cf7bcf91ee86bd1b1d34
Nguyen Tien Loc <locnt8@vinfast.vn>
2020-03-19 23:24:31 +0700

    Avoid crash when use VirtualJoysStickInput

--- godot/globals/VirtualJoyStickInput.gd
+++ godot/globals/VirtualJoyStickInput.gd
@@ -2,27 +2,31 @@ extends Node
 
 
 onready var joystick_scene = preload("res://special_scenes/virtual_joystick/VirtualJoystick.tscn")
+# TODO free virtual joystick
 var virtual_joystick
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	virtual_joystick = joystick_scene.instance()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
 
-func add_joy_stick():
+func add_joy_stick(show_wheel = true, show_buttons = true):
 	# TODO add virtual joystick to keyboard
 	var canvas_layer = CanvasLayer.new()
+	virtual_joystick = joystick_scene.instance()
+	
 	canvas_layer.add_child(virtual_joystick)
 	get_tree().get_current_scene().add_child(canvas_layer)
+	virtual_joystick.show(show_wheel, show_buttons)
 
 func is_action_pressed(action_name: String) -> bool:
-	return virtual_joystick.is_action_pressed(action_name)
+	return virtual_joystick != null and virtual_joystick.is_action_pressed(action_name)
 
 func is_action_just_press(action_name: String) -> bool:
-	return virtual_joystick.is_action_pressed(action_name)
+	return virtual_joystick != null and virtual_joystick.is_action_pressed(action_name)
 
 func is_action_just_release(action_name: String) -> bool:
-	return virtual_joystick.is_action_pressed(action_name)
+	return virtual_joystick != null and virtual_joystick.is_action_pressed(action_name)
--- godot/special_scenes/virtual_joystick/VirtualJoystick.gd
+++ godot/special_scenes/virtual_joystick/VirtualJoystick.gd
@@ -6,8 +6,8 @@ onready var wheel: JoyStickWheel = get_node("Wheel/TouchScreenButton")
 
 
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Wheel.visible = false
+	$Buttons.visible = true
 
 #func _process(delta: float) -> void:
 #	pass
@@ -15,21 +15,25 @@ func _ready() -> void:
 func is_action_pressed(action_name: String) -> bool:
 	match action_name:
 		"fire":
-			return fire_btn.is_pressed()
+			return fire_btn != null and fire_btn.is_pressed()
 		_:
-			return wheel.is_action_pressed(action_name)
+			return wheel != null and wheel.is_action_pressed(action_name)
 
 # TODO Implement these functions
 func is_action_just_press(action_name: String) -> bool:
 	match action_name:
 		"fire":
-			return fire_btn.is_pressed()
+			return fire_btn != null and fire_btn.is_pressed()
 		_:
-			return wheel.is_action_pressed(action_name)
+			return wheel != null and wheel.is_action_pressed(action_name)
 
 func is_action_just_release(action_name: String) -> bool:
 	match action_name:
 		"fire":
-			return fire_btn.is_pressed()
+			return fire_btn != null and fire_btn.is_pressed()
 		_:
-			return wheel.is_action_pressed(action_name)
+			return wheel != null and wheel.is_action_pressed(action_name)
+
+func show(show_wheel = true, show_buttons = true):
+	$Wheel.visible = show_wheel
+	$Buttons.visible = show_buttons
