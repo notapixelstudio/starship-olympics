29b291eb3984eed0431c34e35db61afc60c5e0f2
nitaku <matteo.abrate@gmail.com>
2020-08-07 12:21:08 +0200

    streaks arrows in bars

--- godot/combat/interface/hud/Bar.gd
+++ godot/combat/interface/hud/Bar.gd
@@ -7,10 +7,11 @@ const bar_height = 20
 const ministar_width = 20
 const margin_left = 20
 const margin_top = 10
+const streak_arrow_width = 8
+var max_score
 
 const Star = preload('res://special_scenes/combat_UI/session_points/Star.tscn')
 
-onready var progressbar = $ProgressBar
 onready var sprite = $Ship/Sprite
 
 onready var ministar_margin = ministar_width * global.win
@@ -22,25 +23,32 @@ var new_position setget change_position
 var current_value = 0
 var previous_value = 0
 
+var streaking = false
+var current_streak_bar
+var streak_start
+
 func initialize(p: PlayerStats, matchscore: MatchScores):
 	player = p
 	var species = player.species
-	progressbar.modulate = species.color
-	progressbar.max_value = matchscore.target_score
+	max_score = matchscore.target_score
 	
-	progressbar.rect_size.x = max_bar_width - ministar_margin
 	sprite_on = species.ship
 	sprite_off = species.ship_off
 	sprite.texture = sprite_off
 	
+	# background
+	$Background.rect_position = Vector2(margin_left, margin_top)
+	$Background.rect_size = Vector2(max_bar_width - ministar_margin, bar_height)
+	$Background.color = species.color
+	
 	# ticks
-	if matchscore.target_score != 100: # FIXME workaround for star sports
-		for i in range(1, int(matchscore.target_score)):
+	if max_score != 100: # FIXME workaround for star sports
+		for i in range(1, int(max_score)):
 			var tick = Line2D.new()
-			var opacity = 1 if matchscore.target_score <= 10 or i%10 == 0 else 0.2
+			var opacity = 0.8 if max_score <= 10 or i%10 == 0 else 0.2
 			tick.default_color = Color(0,0,0,opacity)
 			tick.width = 3
-			var x = round((max_bar_width - ministar_margin) / matchscore.target_score * i)
+			var x = round((max_bar_width - ministar_margin) / max_score * i)
 			tick.points = PoolVector2Array([Vector2(margin_left+x, margin_top), Vector2(margin_left+x, margin_top+bar_height)])
 			$Ticks.add_child(tick)
 	
@@ -71,8 +79,7 @@ func set_value(value):
 	if current_value > previous_value:
 		streak_on()
 		
-	progressbar.value = value
-	$Ship.position.x = margin_left + current_value*progressbar.rect_size.x/progressbar.max_value
+	$Ship.position.x = margin_left + (max_bar_width - ministar_margin) / max_score * current_value
 	
 func get_value():
 	return current_value
@@ -85,11 +92,33 @@ func change_position(new_value):
 		$Tween.start()
 	
 func streak_on():
-	sprite.texture = sprite_on
+	if not streaking:
+		streaking = true
+		sprite.texture = sprite_on
+		add_streak_bar()
 	$StreakTimer.start(0.5)
+	update_current_streak_bar()
 	
 func streak_off():
+	streaking = false
 	sprite.texture = sprite_off
 
 func _on_StreakTimer_timeout():
 	streak_off()
+
+func add_streak_bar():
+	current_streak_bar = Polygon2D.new()
+	current_streak_bar.color = player.species.color
+	streak_start = previous_value
+	$Streaks.add_child(current_streak_bar)
+	
+func update_current_streak_bar():
+	var left = margin_left + (max_bar_width - ministar_margin) / max_score * streak_start
+	var right = margin_left + (max_bar_width - ministar_margin) / max_score * current_value
+	current_streak_bar.polygon = PoolVector2Array([
+		Vector2(left, margin_top+bar_height),
+		Vector2(left, margin_top),
+		Vector2(right-streak_arrow_width, margin_top),
+		Vector2(right, margin_top+bar_height/2),
+		Vector2(right-streak_arrow_width, margin_top+bar_height)
+	])
