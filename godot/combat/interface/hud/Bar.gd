0b53724f4b3976e4b95f86efd362f9d8f71552bc
Matteo Abrate <matteo.abrate@gmail.com>
2020-08-07 13:23:44 +0200

    feat(ui): in Combat bars will have Ticks and streaks (#343)
    * tick marks
    
    * workaround for star sports
    
    * wip streaks
    
    * streak timing in bars
    
    * streaks arrows in bars
    
    * more subtle streak arrow

--- godot/combat/interface/hud/Bar.gd
+++ godot/combat/interface/hud/Bar.gd
@@ -3,12 +3,15 @@ extends Node2D
 class_name Bar
 
 const max_bar_width = 940
+const bar_height = 20
 const ministar_width = 20
 const margin_left = 20
+const margin_top = 10
+const streak_arrow_width = 8
+var max_score
 
 const Star = preload('res://special_scenes/combat_UI/session_points/Star.tscn')
 
-onready var progressbar = $ProgressBar
 onready var sprite = $Ship/Sprite
 
 onready var ministar_margin = ministar_width * global.win
@@ -17,18 +20,42 @@ var sprite_off
 var sprite_on
 var player
 var new_position setget change_position
+var current_value = 0
+var previous_value = 0
+
+var streaking = false
+var current_streak_bar
+var streak_start
 
 func initialize(p: PlayerStats, matchscore: MatchScores):
 	player = p
 	var species = player.species
-	progressbar.modulate = species.color
-	progressbar.max_value = matchscore.target_score
+	max_score = matchscore.target_score
 	
-	progressbar.rect_size.x = max_bar_width - ministar_margin
 	sprite_on = species.ship
 	sprite_off = species.ship_off
 	sprite.texture = sprite_off
 	
+	# background
+	$Background.rect_position = Vector2(margin_left, margin_top)
+	$Background.rect_size = Vector2(max_bar_width - ministar_margin, bar_height)
+	$Background.color = species.color
+	
+	# megabar
+	$MegaBar.color = species.color
+	
+	# ticks
+	if max_score != 100: # FIXME workaround for star sports
+		for i in range(1, int(max_score)):
+			var tick = Line2D.new()
+			var opacity = 0.8 if max_score <= 10 or i%10 == 0 else 0.2
+			tick.default_color = Color(0,0,0,opacity)
+			tick.width = 3
+			var x = round((max_bar_width - ministar_margin) / max_score * i)
+			tick.points = PoolVector2Array([Vector2(margin_left+x, margin_top), Vector2(margin_left+x, margin_top+bar_height)])
+			$Ticks.add_child(tick)
+	
+	# mini stars
 	for i in range(global.win):
 		var star = Star.instance()
 		star.scale = Vector2(0.18,0.18)
@@ -49,16 +76,16 @@ func update_stars():
 			stars[i].won = true
 	
 func set_value(value):
-	# round to 2 decimal digit
-	if stepify(value, 0.01) > progressbar.value:
-		sprite.texture = sprite_on
-	else:
-		sprite.texture = sprite_off
-	progressbar.value = value
-	$Ship.position.x = margin_left + progressbar.value*progressbar.rect_size.x/progressbar.max_value
+	previous_value = current_value
+	current_value = value
+	
+	if current_value > previous_value:
+		streak_on()
+		
+	$Ship.position.x = margin_left + (max_bar_width - ministar_margin) / max_score * current_value
 	
 func get_value():
-	return progressbar.value
+	return current_value
 
 func change_position(new_value):
 	new_position = new_value
@@ -67,3 +94,41 @@ func change_position(new_value):
 			Tween.TRANS_CUBIC, Tween.EASE_IN, 0)
 		$Tween.start()
 	
+func streak_on():
+	if not streaking:
+		streaking = true
+		sprite.texture = sprite_on
+		add_streak_bar()
+	$StreakTimer.start(0.5)
+	update_current_streak_bar()
+	
+func streak_off():
+	streaking = false
+	sprite.texture = sprite_off
+
+func _on_StreakTimer_timeout():
+	streak_off()
+
+func add_streak_bar():
+	current_streak_bar = Polygon2D.new()
+	current_streak_bar.color = player.species.color
+	streak_start = previous_value
+	$Streaks.add_child(current_streak_bar)
+	
+func update_current_streak_bar():
+	var left = margin_left + (max_bar_width - ministar_margin) / max_score * streak_start
+	var right = margin_left + (max_bar_width - ministar_margin) / max_score * current_value
+	current_streak_bar.polygon = PoolVector2Array([
+		Vector2(left, margin_top+bar_height),
+		Vector2(left, margin_top),
+		Vector2(max(left,right-streak_arrow_width), margin_top),
+		Vector2(right, margin_top+bar_height/2),
+		Vector2(max(left,right-streak_arrow_width), margin_top+bar_height)
+	])
+	$MegaBar.polygon = PoolVector2Array([
+		Vector2(margin_left, margin_top+bar_height),
+		Vector2(margin_left, margin_top),
+		Vector2(max(margin_left,right-streak_arrow_width), margin_top),
+		Vector2(right, margin_top+bar_height/2),
+		Vector2(max(margin_left,right-streak_arrow_width), margin_top+bar_height)
+	])
