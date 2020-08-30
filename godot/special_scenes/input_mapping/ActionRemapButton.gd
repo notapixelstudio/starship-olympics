7cd9f06ed20b00cf78b555d255e044ac8346b44f
salvob <tholalak@yahoo.it>
2020-08-30 20:07:01 +0200

    fix(ui): remapping are now for ui as well

--- godot/special_scenes/input_mapping/ActionRemapButton.gd
+++ godot/special_scenes/input_mapping/ActionRemapButton.gd
@@ -1,12 +1,11 @@
 extends Button
 
 export var action: String = "ui_up" setget _set_action
-export var input_type_str: String = "key"
 
-func check_type(type_str: String, event:InputEvent):
-	if type_str == "key":
+func check_input_event(action_: String, event:InputEvent):
+	if "kb" in action_:
 		return event is InputEventKey
-	elif type_str == "joy":
+	elif "joy" in action_:
 		return event is InputEventJoypadButton 
 
 func _set_action(value_):
@@ -25,26 +24,24 @@ func _toggled(button_pressed):
 		text = "..."
 	else:
 		display_current_key()
-
-
+		
+		
 func _input(event):
 	# Note that you can use the _input callback instead, especially if
 	# you want to work with gamepads.
-	remap_action_to(event)
-	pressed = false
+	if event is InputEventKey:
+		remap_action_to(self.action, event)
 
 
-func remap_action_to(event):
-	InputMap.action_erase_events(action)
-	InputMap.action_add_event(action, event)
-	print(InputMap.get_action_list(action)[0].as_text())
-	text = "%s " % event.as_text().to_upper()
+func remap_action_to(action, event, ui_flag=true):
+	var current_key = global.remap_action_to(action, event)
+	text = "%s " % current_key.to_upper()
 
 
 func display_current_key():
 	var current_key = "..."
-	for event in InputMap.get_action_list(action):
-		if check_type(input_type_str, event):
+	for event in InputMap.get_action_list(self.action):
+		if check_input_event(self.action, event):
 			current_key = event.as_text()
 			break
 	text = "%s " % current_key.to_upper()
