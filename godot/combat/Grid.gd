51458317799c39f4e5374b72de745cb7a89d615e
nitaku <matteo.abrate@gmail.com>
2020-09-03 01:34:16 +0200

    vector3 instead of colors

--- godot/combat/Grid.gd
+++ godot/combat/Grid.gd
@@ -2,15 +2,21 @@ extends Polygon2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _process(delta):
-	update_wells([Color(-500,-500,5.0), Color(500,0,10.0)])
+func _ready():
+	update_wells([
+		Vector3(-500,-500,5.0),
+		Vector3(500,0,10.0),
+		Vector3(300,100,10.25),
+		Vector3(0,0,11.0),
+		Vector3(50,50,12.0)
+	])
 	
 func update_wells(array):
 	var wells_img = Image.new()
 	wells_img.create(len(array), 1, false, Image.FORMAT_RGBF)
 	wells_img.lock()
 	for i in len(array):
-		wells_img.set_pixel(i, 0, array[i])
+		wells_img.set_pixel(i, 0, Color(array[i].x, array[i].y, array[i].z))
 	wells_img.unlock()
 	var wells_texture = ImageTexture.new()
 	wells_texture.create_from_image(wells_img, 0)
