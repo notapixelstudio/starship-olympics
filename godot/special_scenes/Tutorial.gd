64641621832e906c66a3fe5fe9d944b3e90d5fda
salvob <tholalak@yahoo.it>
2020-09-11 21:44:22 +0200

    fix: skip sluggish tutorial. Close #381

--- godot/special_scenes/Tutorial.gd
+++ godot/special_scenes/Tutorial.gd
@@ -5,9 +5,9 @@ onready var timer = $Timer
 signal over
 
 func _input(event):
-	if event.is_action_pressed("ui_fire"):
+	if event.is_action_pressed("ui_accept"):
 		set_process_input(false)
-		yield(get_tree().create_timer(max(0, 0.5 - (timer.wait_time - timer.time_left))), "timeout")
+		yield(get_tree().create_timer(max(0, 1 - (timer.wait_time - timer.time_left))), "timeout")
 		delete_and_quit()
 		
 func _on_Timer_timeout():
