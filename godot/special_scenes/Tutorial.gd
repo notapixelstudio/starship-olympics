304b8770f49181622b730f275e6c344dd1ab9c15
nitaku <matteo.abrate@gmail.com>
2020-09-11 16:58:32 +0200

    addresses #381

--- godot/special_scenes/Tutorial.gd
+++ godot/special_scenes/Tutorial.gd
@@ -7,7 +7,7 @@ signal over
 func _input(event):
 	if event.is_action_pressed("ui_fire"):
 		set_process_input(false)
-		yield(get_tree().create_timer(max(0, 2 - (timer.wait_time - timer.time_left))), "timeout")
+		yield(get_tree().create_timer(max(0, 0.5 - (timer.wait_time - timer.time_left))), "timeout")
 		delete_and_quit()
 		
 func _on_Timer_timeout():
