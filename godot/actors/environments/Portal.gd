59e37897c49f652c09bb7be9fdc9925bc6c45aaf
ekorina <alnitak.nik@gmail.com>
2020-09-17 15:32:52 +0200

    Portal headaches... still wrestling with them.

--- godot/actors/environments/Portal.gd
+++ godot/actors/environments/Portal.gd
@@ -1,64 +1,40 @@
-tool
-
 extends Node2D
 
 class_name Portal
 
 export var linked_to_path : NodePath
-var linked_to : Portal
+export var offset : float = 200
 
-export var thickness : float = 10 setget set_thickness
-export var width : float = 100 setget set_width
-export var offset : float = 30 setget set_offset
-
-func set_thickness(value):
-	thickness = value
-	refresh()
-	
-func set_width(value):
-	width = value
-	refresh()
-	
-func set_offset(value):
-	offset = value
-	refresh()
-	
-func _ready():
-	refresh()
-	
-func refresh():
-	linked_to = get_node(linked_to_path)
-	
-	$Area2D/CollisionShape2D.shape.extents = Vector2(width, thickness)
-	$StaticBody2D/CollisionShape2D.shape.extents = Vector2(width, thickness)
-	
-	$Line2D.points = PoolVector2Array([
-		Vector2(-width, 0),
-		Vector2(width, 0)
-	])
-	$Line2D.width = thickness*10
-	
-	# workaround for losing texture mode
-	$Line2D.texture_mode = Line2D.LINE_TEXTURE_TILE
-	
-func _on_Area2D_body_entered(body):
+func _on_Area2D_body_entered(body : PhysicsBody2D):
+	if abs(wrapf(body.global_rotation - global_rotation, -PI, PI)) >= PI/2:
+		return
+		
 	var entity = ECM.E(body)
 	
 	if not entity:
 		return
+		
 	
 	if entity.has('Teleportable'):
+		print(rad2deg(wrapf(body.global_rotation - global_rotation, -PI, PI)))
+		
 		var teleportable = entity.get('Teleportable')
 		var vector = body.position - position
 		#offset = offset.rotated(-rotation)
 		#offset.x = -offset.x
 		#offset = offset.rotated(rotation)
 		teleportable.disable()
-		teleportable.set_destination(linked_to.position + vector - offset*Vector2(0,1).rotated(rotation))
+		var linked_to = get_node(linked_to_path)
+		teleportable.set_destination(linked_to.position + vector.project(Vector2(0,-1)) + offset*Vector2(-1,0).rotated(rotation+PI))
 		if entity.has('Thrusters'):
 			entity.get('Thrusters').disable()
 			
-		yield(get_tree().create_timer(0.1), 'timeout')
+		# temporarily disable all collisions for body
+		body.get_node('CollisionShape2D').disabled = true
+		
+		yield(get_tree().create_timer(2), 'timeout')
+		
+		body.get_node('CollisionShape2D').disabled = false
 		
 		if teleportable:
 			teleportable.enable()
