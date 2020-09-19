d515ac8c1fcf879d7d3227622b920da860155a87
ekorina <alnitak.nik@gmail.com>
2020-09-19 22:24:59 +0200

    Good enough portals.

--- godot/actors/environments/Portal.gd
+++ godot/actors/environments/Portal.gd
@@ -1,10 +1,42 @@
+tool
 extends Node2D
 
 class_name Portal
 
 export var linked_to_path : NodePath
-export var offset : float = 250
+export var offset : float = 80
+export var color : Color = Color(1, 0, 1, 1) setget set_color
+export var inverted : bool = false setget set_inverted
 
+onready var wall = $StaticBody2D
+
+func set_color(v):
+	color = v
+	refresh()
+	
+func set_inverted(v):
+	inverted = v
+	refresh()
+
+func _ready():
+	refresh()
+	
+func refresh():
+	if is_inside_tree():
+		$Line2D.modulate = color
+		$Particles2D.modulate = color
+		$SpikeParticles2D.modulate = color
+		$Particles2D.scale.x = -1 if inverted else 1
+		$Particles2D2.scale.x = -1 if inverted else 1
+		
+func enable():
+	$Area2D/CollisionShape2D.disabled = false
+	$StaticBody2D/CollisionShape2D.disabled = false
+	
+func disable():
+	$Area2D/CollisionShape2D.disabled = true
+	$StaticBody2D/CollisionShape2D.disabled = true
+	
 func _on_Area2D_body_entered(body : PhysicsBody2D):
 	var entity = ECM.E(body)
 	
@@ -19,16 +51,23 @@ func _on_Area2D_body_entered(body : PhysicsBody2D):
 		#offset = offset.rotated(rotation)
 		teleportable.disable()
 		var linked_to = get_node(linked_to_path)
-		teleportable.set_destination(linked_to.position  + vector.project(Vector2(0,-1).rotated(rotation+PI)) + offset*Vector2(-1,0).rotated(rotation+PI))
-		#teleportable.set_destination(linked_to.position + Vector2(-100,100))
+		body.add_collision_exception_with(linked_to.wall)
+		
+		teleportable.set_destination(linked_to.position + vector.project(Vector2(0,-1).rotated(rotation+PI)) + offset*Vector2(-1,0).rotated(rotation+PI))
+		
+		var had_thrusters = false
 		if entity.has('Thrusters'):
+			had_thrusters = true
 			entity.get('Thrusters').disable()
 			
 		yield(get_tree().create_timer(0.01), 'timeout')
 		
-		if teleportable:
-			teleportable.enable()
-		if entity.could_have('Thrusters'):
+		if had_thrusters:
 			entity.get('Thrusters').enable()
 			
+		yield(get_tree().create_timer(0.1), 'timeout')
+		
+		body.remove_collision_exception_with(linked_to.wall)
+		if teleportable:
+			teleportable.enable()
 		
