e27ff20e67dd98e52b43de51e3318fbe306c2c81
salvob <tholalak@yahoo.it>
2020-09-20 18:39:16 +0200

    fix(combat): camera tweaks

--- godot/actors/battlers/TargetDest.gd
+++ godot/actors/battlers/TargetDest.gd
@@ -9,6 +9,8 @@ func _ready():
 	while not node_owner:
 		node_owner = get_node(node_owner_path)
 	
-	
 func get_master_ship():
 	return node_owner
+
+func _process(delta):
+	position.x = (node_owner.velocity).length()/2
