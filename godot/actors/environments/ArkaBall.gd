cab3b9d56968dc776cea0b7c9dc8e4d8ed768d15
ekorina <alnitak.nik@gmail.com>
2020-09-21 10:19:19 +0200

    Pong prototype improved.

--- godot/actors/environments/ArkaBall.gd
+++ godot/actors/environments/ArkaBall.gd
@@ -1,1 +1,17 @@
 extends RigidBody2D
+
+export var impulse : float = 30.0
+var active : bool = false
+
+func start():
+	active = true
+
+func _physics_process(delta):
+	if active:
+		apply_central_impulse(impulse*Vector2(1,0).rotated(linear_velocity.angle()))
+	
+
+func _on_ArkaBall_body_entered(body):
+	if body is Paddle:
+		apply_central_impulse(linear_velocity.normalized()*2000)
+	
