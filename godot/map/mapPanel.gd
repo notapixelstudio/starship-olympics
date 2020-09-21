02acc3dd0cb4110853578a961188950ab7d92d62
ekorina <alnitak.nik@gmail.com>
2020-09-21 16:25:47 +0200

    Improved map graphics.

--- godot/map/mapPanel.gd
+++ godot/map/mapPanel.gd
@@ -1,4 +1,4 @@
-extends ColorRect
+extends Control
 
 var species setget set_species
 onready var sprite = $Sprite
@@ -7,17 +7,28 @@ onready var desc = $Label
 var planet setget set_planet
 var rest_text = "choose a planet"
 var chosen = false setget set_chosen
-onready var rectangle = $character_selection_rect
+onready var background = $Background
+const deselected_modulate = Color(1,1,1,0.6)
+
+func _ready():
+	background.self_modulate = deselected_modulate
 
 func set_chosen(chosen_):
 	if not is_inside_tree():
 		yield(self, "ready")
 	chosen = chosen_
 	if chosen:
-		rectangle.modulate = species.color
+		background.self_modulate = Color(1.1,1.1,1.1,1)
+		$Tween.interpolate_property(self, 'rect_position',
+			rect_position, Vector2(rect_position.x,-30), 0.5,
+			Tween.TRANS_CUBIC, Tween.EASE_OUT)
+		$Tween.start()
 	else:
-		rectangle.modulate = Color(1,1,1)
-	
+		background.self_modulate = deselected_modulate
+		$Tween.interpolate_property(self, 'rect_position',
+			rect_position, Vector2(rect_position.x,0), 0.5,
+			Tween.TRANS_CUBIC, Tween.EASE_OUT)
+		$Tween.start()
 	
 func set_species(species_):
 	
@@ -27,6 +38,7 @@ func set_species(species_):
 		species = species_
 		sprite.texture = species.ship_off
 		desc.text = rest_text
+		background.modulate = species.color
 	
 func set_planet(planet_):
 	if not is_inside_tree():
