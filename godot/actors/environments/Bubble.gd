5b15a4889d6ba96450cb1c24697dbb23b9988c6b
nitaku <matteo.abrate@gmail.com>
2020-10-25 23:46:49 +0100

    colors and accessibility letters

--- godot/actors/environments/Bubble.gd
+++ godot/actors/environments/Bubble.gd
@@ -1,7 +1,18 @@
+tool
 extends RigidBody2D
 
 onready var radius = $CollisionShape2D.shape.radius
 
+export var species : Resource setget set_species
+
+func set_species(v):
+	species = v
+	$Sprite.modulate = species.color
+	$Sprite/Label.text = species.species_name.left(1).to_upper()# + species.species_name.substr(1,1)
+
 func _process(delta):
 	$Sprite.rotation = -rotation
+	
+func get_color():
+	return $Sprite.modulate
 	
