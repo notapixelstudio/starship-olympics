e226895f48e8732ac525de9d72abc75d8228a675
nitaku <matteo.abrate@gmail.com>
2020-11-28 02:42:43 +0100

    death sfx

--- godot/actors/battlers/DeathFlash.gd
+++ godot/actors/battlers/DeathFlash.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 export var species : Resource
-export var timeout : float = 2.0
+export var timeout : float = 4.0
 
 func _ready():
 	$Particles2D.modulate = species.color
 	$Particles2D.lifetime = timeout
 	$Particles2D.emitting = true
+	$RandomAudioStreamPlayer2D.play()
+	$RandomAudioStreamPlayer2D2.play()
 	yield(get_tree().create_timer(timeout), "timeout")
 	queue_free()
 	
