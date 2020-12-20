553e04a4fee7409006853cf192c7fd7037a7f551
nitaku <matteo.abrate@gmail.com>
2020-12-20 04:14:12 +0100

    drop shadows

---
+++ godot/utils/Shadow.gd
@@ -0,0 +1,15 @@
+extends Sprite
+
+export var sprite : NodePath setget set_sprite
+export var dy = 32
+
+func set_sprite(v):
+	sprite = v
+	
+func _ready():
+	yield(get_tree(), "idle_frame") # wait for e.g., ships to prepare
+	texture = get_node(sprite).texture
+	
+func _process(_delta):
+	position = Vector2(0, dy).rotated(-global_rotation)
+	
