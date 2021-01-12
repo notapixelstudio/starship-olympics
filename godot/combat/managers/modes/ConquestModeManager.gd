e9e421fcf6e913bb5b57cccc4c8f6946a575834b
nitaku <matteo.abrate@gmail.com>
2021-01-12 23:43:33 +0100

    negative scores reintroduced in conquest games

--- godot/combat/managers/modes/ConquestModeManager.gd
+++ godot/combat/managers/modes/ConquestModeManager.gd
@@ -10,6 +10,6 @@ func _on_sth_conquered(conquered_by, what, points=1, show_msg=true):
 		
 func _on_sth_lost(lost_by, what, points=1, show_msg=true):
 	if enabled:
-		emit_signal('score', lost_by.get_id(), -points*score_multiplier)
+		.score(lost_by.get_id(), -points*score_multiplier)
 		if show_msg:
 			emit_signal('show_msg', lost_by.species, -points*score_multiplier, what.position)
