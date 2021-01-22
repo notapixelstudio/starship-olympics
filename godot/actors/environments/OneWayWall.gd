6cc09628e0d2979f0dc519f8648d3c517f477cf8
nitaku <matteo.abrate@gmail.com>
2021-01-22 12:57:10 +0100

    one way wall style

--- godot/actors/environments/OneWayWall.gd
+++ godot/actors/environments/OneWayWall.gd
@@ -6,5 +6,19 @@ export var width = 600 setget set_width
 func set_width(v):
 	width = v
 	$CollisionShape2D.shape.extents.x = width/2
-	$Line2D.width = width
+	$Polygon2D.polygon = PoolVector2Array([
+		Vector2(-width/2, -100),
+		Vector2(width/2, -100),
+		Vector2(width/2, 0),
+		Vector2(-width/2, 0)
+	])
+	$ShadowPolygon2D.polygon = PoolVector2Array([
+		Vector2(-width/2, -100),
+		Vector2(width/2, -100),
+		Vector2(width/2, 0),
+		Vector2(-width/2, 0)
+	])
+	
+func _ready():
+	$ShadowPolygon2D.position = Vector2(0,32).rotated(-global_rotation)
 	
