1eb9af9352a8a483d02570a44666b342b334c2b1
nitaku <matteo.abrate@gmail.com>
2021-01-22 11:46:15 +0100

    rotated numbers in tiles

--- godot/actors/environments/Tile.gd
+++ godot/actors/environments/Tile.gd
@@ -55,6 +55,7 @@ func _ready():
 	refresh_polygon()
 	
 	$Graphics.position = Vector2(0,32).rotated(-global_rotation)
+	$Graphics/Wrapper.rotation = -global_rotation
 	$Graphics/Wrapper/Label.text = '' if points == 1 else str(points)
 	yield(get_tree(), "idle_frame") # wait for all tiles to be ready
 	
