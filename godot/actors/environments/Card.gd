728c7db27e98624998879deb73bfbcf55b81d2e4
nitaku <matteo.abrate@gmail.com>
2021-01-26 02:15:17 +0100

    animals for memory

--- godot/actors/environments/Card.gd
+++ godot/actors/environments/Card.gd
@@ -2,6 +2,14 @@ extends Area2D
 
 onready var anim = $AnimationPlayer
 onready var outline = $Ground/Outline
+onready var figure = $Ground/Front/Figure
+
+func _ready():
+	var special = randf() > 0.7
+	if special:
+		figure.texture = load('res://assets/sprites/animals/b0' + str(randi() % 10) + '.png')
+	else:
+		figure.texture = load('res://assets/sprites/animals/a0' + str(randi() % 5) + '.png')
 
 func _on_tap(author):
 	anim.play("Reveal")
