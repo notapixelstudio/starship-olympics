9509bdd40435b48d8559b60c8ab9f55d71695fd2
nitaku <matteo.abrate@gmail.com>
2021-05-22 22:53:03 +0200

    tweaks

--- godot/actors/enemies/Bullet.gd
+++ godot/actors/enemies/Bullet.gd
@@ -3,8 +3,7 @@ extends Area2D
 var velocity = Vector2(100,0)
 
 func _on_Bullet_body_entered(body):
-	if not ECM.E(body).has('Phasing'):
-		queue_free()
+	queue_free()
 
 func _physics_process(delta):
 	position += velocity*delta
