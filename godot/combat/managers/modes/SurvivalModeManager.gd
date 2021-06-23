7cbc8d3ac20c214db80321140665896c1a0d27c2
nitaku <matteo.abrate@gmail.com>
2021-06-23 11:00:28 +0200

    closes #699

--- godot/combat/managers/modes/SurvivalModeManager.gd
+++ godot/combat/managers/modes/SurvivalModeManager.gd
@@ -1,9 +1,14 @@
 extends ModeManager
 
+var active = false
+
+func start():
+	active = true
+
 func _process(delta):
-	if not enabled:
+	if not enabled or not active:
 		return
 		
 	for ship in get_tree().get_nodes_in_group('players'):
 		if ship.alive:
-			.score(ship.get_id(), delta)
+			global.the_match.add_score(ship.get_id(), delta)
