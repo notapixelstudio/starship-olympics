97ca3d630c8e6ac4ada73b271ccc0a197a0cb68f
nitaku <matteo.abrate@gmail.com>
2021-06-24 19:50:55 +0200

    dash multiplier toned down + bugfix in tennis

--- godot/combat/managers/TennisManager.gd
+++ godot/combat/managers/TennisManager.gd
@@ -12,7 +12,7 @@ func start():
 	ball.connect('body_entered', self, '_on_ball_body_entered')
 	
 func _on_ball_body_entered(body):
-	if body is WallGoal:
-		var player =  ball.owner_ship.get_player()
+	if body is WallGoal and ball.owner_ship:
+		var player = ball.owner_ship.get_player()
 		if player != null and player.team != body.get_player().team:
 			body.do_goal(player, ball.position)
