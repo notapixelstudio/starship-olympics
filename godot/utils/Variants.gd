3fabf3ed75e1a9c35e89a1268ef9d27f4513909b
ekorina <alnitak.nik@gmail.com>
2021-06-27 20:56:57 +0200

    Better deathmatch variants.

--- godot/utils/Variants.gd
+++ godot/utils/Variants.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
+export var debug = false
+
 func _ready():
-	# destroy all children but one at random
 	var children = get_children()
 	var index = randi() % len(children)
 	var i = 0
 	for child in children:
-		if index != i:
+		if debug and child.visible == false:
+			# destroy invisible children
 			child.queue_free()
 		else:
-			# make sure it is visible
-			child.visible = true
+			# destroy all children but one at random
+			if index != i:
+				child.queue_free()
+			else:
+				# make sure it is visible
+				child.visible = true
 		i += 1
 		
