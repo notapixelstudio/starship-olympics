50527160f53acbe107372394e7a08630a342f147
ekorina <alnitak.nik@gmail.com>
2021-08-08 20:52:58 +0200

    Lovers manager now spawns aliens.

--- godot/actors/environments/Heart.gd
+++ godot/actors/environments/Heart.gd
@@ -1,8 +1,33 @@
 extends Ball
 
+const BASIC_FIGURES = [
+	'animals/a00',
+	'animals/a01',
+	'animals/a02',
+	'animals/a03',
+	'animals/a04'
+]
+const ADVANCED_FIGURES = [
+	'animals/b00',
+	'animals/b01',
+	'animals/b02',
+	'animals/b03',
+	'animals/b04',
+	'animals/b05',
+	'animals/b06',
+	'animals/b07',
+	'animals/b08',
+	'animals/b09',
+	'animals/b10'
+]
+var alien : String
+
 func _ready():
 	._ready()
 	remove_from_group('in_camera')
+	
+	alien = BASIC_FIGURES[randi() % len(BASIC_FIGURES)]
+	$Sprite/Alien.texture = load('res://assets/sprites/' + alien + '.png')
 
 func start() -> void:
 	$Sprite/AnimationPlayer.play("Wobble")
--- godot/combat/managers/LoversManager.gd
+++ godot/combat/managers/LoversManager.gd
@@ -1,16 +1,14 @@
 extends Node
 
+export var HeartScene: PackedScene
+export var y := 1800.0
+export var width := 4000.0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func start():
+	$AnimationPlayer.play("Spawn")
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func spawn_alien():
+	var alien = HeartScene.instance()
+	alien.position = Vector2(width*(randf()-0.5), y)
+	global.arena.battlefield.add_child(alien)
+	alien.start()
