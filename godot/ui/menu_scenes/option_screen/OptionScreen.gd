29d883644a8dee986c0a55831227d102f45326ac
salvob <tholalak@yahoo.it>
2021-10-25 22:58:02 +0200

    fix: continuing refactoring

---
+++ godot/ui/menu_scenes/option_screen/OptionScreen.gd
@@ -0,0 +1,56 @@
+extends ColorRect
+
+signal back_at_you # signal that gives the line back to whoever instantiated the option
+
+class_name UIOptions
+
+export var title: String = "Options"
+export var start_scene: PackedScene
+onready var banner_info = $CanvasLayer/BannerInfo
+
+var focus_index = 0
+
+
+func _ready():
+	Events.connect("ui_back_menu", self,"back")
+	Events.connect("ui_nav_to", self, "nav_to")
+	assert( start_scene is PackedScene)
+	var instanced_scene = start_scene.instance()
+	self.set_content(instanced_scene)
+	
+
+
+var separator = " > "
+onready var navbar = [title] # Navbar of title string screen
+onready var navbar_scene = []
+
+func back_to_menu():
+	emit_signal("back_at_you")
+	queue_free()
+
+func _exit_tree():
+	# Let's save the changes
+	persistance.save_game()
+
+func nav_to(title, menu_scene: UIOptionPanel):
+	"""
+	will update the navbar and instance the new scene inside the tree
+	"""
+	var current = navbar_scene[len(navbar_scene)-1]
+	remove_child(current)
+	self.set_content(menu_scene)
+	
+func back():
+	var current = navbar_scene.pop_back()
+	current.queue_free()
+	if len(navbar_scene) >= 1:
+		self.set_content(navbar_scene[len(navbar_scene)-1], false)
+	else:
+		back_to_menu()
+
+func set_content(instanced_scene: UIOptionPanel, nav_forward: bool = true):
+	add_child(instanced_scene)
+	instanced_scene.get_focus()
+	if nav_forward:
+		navbar_scene.append(instanced_scene)
+	
