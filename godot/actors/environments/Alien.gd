f37ef05b4af8af173c97c567313e72fc6839964c
nitaku <matteo.abrate@gmail.com>
2021-10-27 00:19:09 +0200

    experiments with foundlings

--- godot/actors/environments/Alien.gd
+++ godot/actors/environments/Alien.gd
@@ -23,9 +23,6 @@ const ADVANCED_FIGURES = [
 ]
 var kind : String
 
-var direction := Vector2.UP
-var step := 3
-
 func _ready():
 	._ready()
 	remove_from_group('in_camera')
@@ -37,21 +34,7 @@ func start() -> void:
 	$Graphics/AnimationPlayer.play("Wobble")
 
 func dive() -> void:
-	var strength = 200
-	if direction == Vector2.UP:
-		strength = 400
-		
-	apply_central_impulse(direction*strength)
-	match step:
-		0:
-			direction = Vector2.UP
-		1,2,3,4:
-			direction = Vector2.LEFT
-		5:
-			direction = Vector2.UP
-		6,7,8,9:
-			direction = Vector2.RIGHT
-	step = (step+1) % 10
+	apply_central_impulse(Vector2.UP*400)
 	
 func get_texture():
 	return $Graphics/Alien.texture
