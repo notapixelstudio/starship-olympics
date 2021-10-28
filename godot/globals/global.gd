db82afe5d2fad23b406690134e0069dd26852ba4
nitaku <matteo.abrate@gmail.com>
2021-10-28 22:52:42 +0200

    fix: typo

--- godot/globals/global.gd
+++ godot/globals/global.gd
@@ -608,7 +608,7 @@ func new_session() -> TheSession:
 	return session
 	
 func safe_destroy_game() -> void:
-	if is_game_running():<
+	if is_game_running():
 		# also delete the session
 		safe_destroy_session()
 		
