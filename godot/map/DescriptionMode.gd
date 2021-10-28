81a3dd4df278c6165f49160da5b8f95658628bb1
nitaku <matteo.abrate@gmail.com>
2021-10-28 19:50:58 +0200

    fix: get rid of rules and restore a single line description for minigames (some files still linger) - closes #768

--- godot/map/DescriptionMode.gd
+++ godot/map/DescriptionMode.gd
@@ -11,30 +11,29 @@ signal ready_to_fight
 
 func _ready():
 	refresh()
-	$Description2.modulate *= Color(1,1,1,0)
+	$Continue.modulate *= Color(1,1,1,0)
 
 func refresh():
-	if $Sprite and gamemode:
-		#$Sprite.texture = gamemode.logo
-		"""
-		sport_name.text = tr(gamemode.description).format({
-			"score": str(gamemode.max_score),
-			"time": str(gamemode.max_timeout)
-		})
-		"""
-		$Sprite/Label.text = tr(gamemode.name)
+	if not is_inside_tree():
+		yield(self, 'ready')
 		
-		var i = 1
-		for rule in gamemode.rules:
-			get_node("Rule"+str(i)).apply_rule(rule)
-			i+=1
-		
-		"""
-		if "shoot_bombs" in gamemode and not gamemode["shoot_bombs"]:
-			$Description3.text = 'No bombs!'
-		elif "starting_ammo" in gamemode and gamemode["starting_ammo"] != -1:
-			$Description3.text = 'Limited ammo!'
-		"""
+	#$Sprite.texture = gamemode.logo
+	"""
+	sport_name.text = tr(gamemode.description).format({
+		"score": str(gamemode.max_score),
+		"time": str(gamemode.max_timeout)
+	})
+	"""
+	$Label.text = tr(gamemode.name)
+	$Description.text = tr(gamemode.description)
+	
+	"""
+	if "shoot_bombs" in gamemode and not gamemode["shoot_bombs"]:
+		$Description3.text = 'No bombs!'
+	elif "starting_ammo" in gamemode and gamemode["starting_ammo"] != -1:
+		$Description3.text = 'Limited ammo!'
+	"""
+	
 func set_gamemode(value: GameMode):
 	gamemode = value
 	refresh()
@@ -56,10 +55,10 @@ func appears():
 	
 func disappears():
 	animator.play("getout")
-	$Description2.queue_free()
+	$Continue.queue_free()
 	yield(animator, "animation_finished")
 	emit_signal("ready_to_fight")
 	queue_free()
 
 func demomode(demo = false):
-	$Description2.visible = not demo
+	$Continue.visible = not demo
