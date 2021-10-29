229f1b2879230237a390ccccd28b3aae0eb07b30
nitaku <matteo.abrate@gmail.com>
2021-10-29 12:48:22 +0200

    foundling minigame basics

--- godot/actors/environments/Homeworld.gd
+++ godot/actors/environments/Homeworld.gd
@@ -2,15 +2,15 @@ tool
 extends "res://actors/environments/Planet.gd"
 class_name Homeworld
 
-export var kind := 0 setget set_kind, get_kind
+export (String, 'animals/a00', 'animals/a01', 'animals/a02', 'animals/a03', 'animals/a04') var kind := 'animals/a00' setget set_kind, get_kind
 
-const COLORS := [
-	Color.orangered,
-	Color.yellow,
-	Color.mediumvioletred,
-	Color.cornflower,
-	Color.mediumseagreen
-]
+const COLORS := {
+	'animals/a00': Color.orangered,
+	'animals/a01': Color.yellow,
+	'animals/a02': Color.mediumvioletred,
+	'animals/a03': Color.cornflower,
+	'animals/a04': Color.mediumseagreen
+}
 
 func set_ground_radius(v):
 	.set_ground_radius(v)
@@ -22,7 +22,7 @@ func set_kind(v):
 	kind = v
 	if not is_inside_tree():
 		yield(self, 'ready')
-	$Alien.texture = load('res://assets/sprites/animals/a0' + str(kind) + '.png')
+	$Alien.texture = load('res://assets/sprites/' + kind + '.png')
 	$Ground.modulate = COLORS[kind]
 
 func get_kind():
--- godot/combat/levels/singles/foundlings/FoundlingsManager.gd
+++ godot/combat/levels/singles/foundlings/FoundlingsManager.gd
@@ -11,5 +11,7 @@ func _on_planet_reached(planet, sth):
 	
 	if cargo.has_holdable():
 		var holdable = cargo.get_holdable()
-		if holdable is Alien:# and holdable.kind == planet.get_kind()
+		if holdable is Alien and holdable.get_kind() == planet.get_kind():
 			cargo.empty()
+			global.the_match.add_score(sth.get_player().id, 1)
+			global.arena.show_msg(sth.get_player().species, 1, sth.position)
