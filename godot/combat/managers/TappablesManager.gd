7bc1a9959de8e30e40fddc7597c3824d47228efc
ekorina <alnitak.nik@gmail.com>
2021-11-03 09:40:34 +0100

    fix: Tappables preview is now in sync with actual state.

--- godot/combat/managers/TappablesManager.gd
+++ godot/combat/managers/TappablesManager.gd
@@ -8,15 +8,28 @@ func _ready():
 	Events.connect("tap", self, '_on_tap')
 	
 func _on_tappable_entered(tappable, ship : Ship):
-	# activate another tappable only if there are no older ones
+	# activate another tappable only if there isn't an older one for this ship
 	if not active_tappables.has(ship):
 		active_tappables[ship] = tappable
-		tappable.show_tap_preview(ship)
+		tappable.show_tap_preview(ship) # WARNING the new color overwrites the old
 	
 func _on_tappable_exited(tappable, ship : Ship):
 	if active_tappables.has(ship):
-		active_tappables[ship].hide_tap_preview()
+		var old_tappable = active_tappables[ship]
 		active_tappables.erase(ship)
+		
+		# search if there are ships left holding the old tappable
+		var found := false
+		for s in active_tappables.keys():
+			if active_tappables[s] == old_tappable:
+				found = true
+				# a ship was found, change the preview to its color
+				active_tappables[s].show_tap_preview(s)
+				break
+				
+		# if there are no more ships holding the old tappable, hide the preview
+		if not found:
+			old_tappable.hide_tap_preview()
 		
 		# check if we will be in another tappable
 		yield(get_tree(), "idle_frame")
