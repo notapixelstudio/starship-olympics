da8d2529ba9c1ddc66230c49a3c1166f1e9cbbb9
nitaku <matteo.abrate@gmail.com>
2021-11-23 21:29:29 +0100

    nine lives with two turrets (experimental, 2 players only)

--- godot/actors/enemies/Turret.gd
+++ godot/actors/enemies/Turret.gd
@@ -1,5 +1,4 @@
-tool
-extends StaticBody2D
+extends RigidBody2D
 
 export var weapon : PackedScene
 export var speed = 400
@@ -10,11 +9,16 @@ export var spread = 90
 export var offset = 45
 export var radius = 125
 export var active := true setget set_active
+export var sleeping_modulate := Color(1,1,1,1) setget set_sleeping_modulate
+export var autoplay := true
 
 var ccw = +1
 var direction = Vector2(1,0)
 
 func fire():
+	if not active:
+		return
+		
 	$AnimationPlayer.stop()
 	$AnimationPlayer.play("Fire")
 	for r in range(rays):
@@ -23,14 +27,14 @@ func fire():
 func fire_angled(angle):
 	var bullet = weapon.instance()
 	
-	bullet.position = radius*direction.rotated(angle)
+	bullet.global_position = global_position + radius*direction.rotated(angle)
 	bullet.linear_velocity = direction.rotated(angle)*speed
 	
 	# ugly
 	get_parent().add_child(bullet)
 	
 func _process(delta):
-	if active and not Engine.editor_hint:
+	if active:
 		direction = direction.rotated(rotation_speed*delta*ccw)
 		set_rotation_and_stuff(rotation + rotation_speed*delta*ccw)
 
@@ -45,16 +49,23 @@ func set_active(v: bool):
 	$SpriteOff.visible = not active
 	$Entity/Deadly.set_enabled(active)
 	
+func set_sleeping_modulate(v: Color):
+	sleeping_modulate = v
+	$SpriteOff.modulate= sleeping_modulate
+	
 func _on_Timer_timeout():
 	fire()
 	
 func _ready():
-	if active:
+	Events.connect("sth_collided_with_ship", self, '_on_sth_collided_with_ship')
+	
+	if autoplay:
 		ccw = +1 if randf() > 0.5 else -1
 		var random_angle = randf()*2*PI
 		direction = direction.rotated(random_angle)
 		set_rotation_and_stuff(random_angle)
 		set_process(false)
+		$Routine.play('Default')
 	
 func start():
 	set_process(true)
@@ -64,3 +75,7 @@ func start():
 func set_rotation_and_stuff(angle):
 	rotation = angle
 	$SpriteOff/Eye.rotation = -rotation
+
+func _on_Turret_body_entered(body):
+	if not active and body is Bomb and self.has_node('Routine'):
+		$Routine.play('Default')
