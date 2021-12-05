51532ac72cf79c401379b490a7f5a98584c8d210
salvob <tholalak@yahoo.it>
2021-12-05 01:58:21 +0100

    fix: varous experiments with rescaling ui

--- godot/map/CanvasLayerTop.gd
+++ godot/map/CanvasLayerTop.gd
@@ -1,6 +1,5 @@
 extends CanvasLayer
 
 func _ready():
-	scale = global.get_graphics_scale()
-	$PanelContainer.rect_size = Vector2(1280, 180)
-	$PanelContainer.margin_top = -180
+	$PanelContainer.rect_scale = global.get_graphics_scale()
+	
