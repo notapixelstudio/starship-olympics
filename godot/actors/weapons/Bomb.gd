1a87f131bba4d31a9b0216b50f9796c06e738098
nitaku <matteo.abrate@gmail.com>
2022-01-02 17:59:38 +0100

    neon weapons

--- godot/actors/weapons/Bomb.gd
+++ godot/actors/weapons/Bomb.gd
@@ -68,7 +68,6 @@ func initialize(bomb_type, pos : Vector2, impulse, ship, size = 1):
 		$NearArea/CollisionShape2D.shape.radius = size*80
 		$Sprite.texture = bullet_texture
 		$Sprite.scale = Vector2(size*0.6, size*0.6)
-		$Sprite.modulate = $Sprite.modulate.darkened(0.3)
 		mode = MODE_CHARACTER
 		
 	elif type == GameMode.BOMB_TYPE.bubble:
@@ -89,7 +88,6 @@ func initialize(bomb_type, pos : Vector2, impulse, ship, size = 1):
 		$NearArea/CollisionShape2D.shape.radius = size*80
 		$Sprite.texture = ice_texture
 		$Sprite.scale = Vector2(size*1.1, size*1.1)
-		$Sprite.modulate = $Sprite.modulate.darkened(0.3)
 		mode = MODE_CHARACTER
 		
 	else:
