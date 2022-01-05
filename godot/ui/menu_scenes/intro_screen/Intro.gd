20938dad9bbbd0f4aea2250f4c2e3fd599a17463
salvob <tholalak@yahoo.it>
2022-01-05 01:03:36 +0100

    fix: new intro wip

--- godot/ui/menu_scenes/intro_screen/Intro.gd
+++ godot/ui/menu_scenes/intro_screen/Intro.gd
@@ -3,24 +3,23 @@ extends Control
 onready var disclaimer = $Disclaimer
 onready var anim = $AnimationPlayer
 
-onready var line1 = $Line1
-onready var line2 = $Line2
-onready var line3 = $Line3
+var line1 = tr("...what? ...has a [i]century[/i] passed already?")
+var line2 = tr("a-hem... welcome mortals!")
+var line3 = tr("welcome back to another edition of...")
 
+onready var typewriter = $Typewriter
 
 export var main_screen : PackedScene
 
 func _ready():
 	global.start_execution()
 	
-	line1.bbcode_text = tr("[center]MILLENNIA OF INTERGALACTIC WARS[/center]")
-	line2.bbcode_text = tr("[center]FINALLY CAME TO AN END[/center]")
-	line3.bbcode_text = tr("[center]WHEN [i]THE GAMES[/i] WERE CREATED[/center]")
 	set_process_input(false)
 	if global.first_time:
 		disclaimer.start()
 		yield(disclaimer, "okay")
 	anim.play("Appear")
+	typewriter.text = line1
 
 func go_ahead():
 	get_tree().change_scene_to(main_screen)
