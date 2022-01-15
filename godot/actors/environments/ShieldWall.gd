b9bd816e1f7ae3140fe7b4a949450b6b436aa7d1
nitaku <matteo.abrate@gmail.com>
2022-01-15 11:34:36 +0100

    shield walls (unused)

---
+++ godot/actors/environments/ShieldWall.gd
@@ -0,0 +1,44 @@
+extends Area2D
+
+export (String, 'shield', 'plate', 'skin') var type = 'plate'
+
+func _ready():
+	$Polygon2D.polygon = $CollisionPolygon2D.polygon
+	$Shadow.polygon = $CollisionPolygon2D.polygon
+	up(type)
+
+func up(new_type):
+	type = new_type
+	match type:
+		'shield':
+			$Polygon2D.modulate = Color('#008bff')
+		'plate':
+			$Polygon2D.modulate = Color.white
+		'skin':
+			$Polygon2D.modulate = Color.green
+	enable_collisions()
+	$AnimationPlayer.play("reset")
+
+func down():
+	# collisions will be disabled near the end of the animation
+	$AnimationPlayer.stop() # this would make the sector flash again
+	if type == 'plate':
+		$AnimationPlayer.play("IndestructibleHit")
+	else:
+		$AnimationPlayer.play("Disappear")
+	
+func enable_collisions():
+	$CollisionPolygon2D.call_deferred('set_disabled', false)
+	
+func disable_collisions():
+	$CollisionPolygon2D.call_deferred('set_disabled', true)
+	if type == 'skin':
+		yield(get_tree().create_timer(5), "timeout")
+		if type == 'skin': # shield type could have changed (e.g., if switched off)
+			up('skin')
+
+func _on_ShieldWall_body_entered(body):
+	if body is Bomb or body is Bullet:
+		body.queue_free()
+		
+		self.down()
