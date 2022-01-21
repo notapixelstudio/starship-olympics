787aedb845eca1d3cd750aa4652e3e7a85fa55dd
salvob <tholalak@yahoo.it>
2022-01-21 13:26:11 +0100

    feat(UI): Added voice to referee and updated font

--- godot/ui/menu_scenes/intro_screen/Intro.gd
+++ godot/ui/menu_scenes/intro_screen/Intro.gd
@@ -3,12 +3,13 @@ extends Control
 onready var disclaimer = $Disclaimer
 onready var anim = $AnimationPlayer
 
-var line1 = tr("...what? ...has a [i]century[/i] passed already?")
-var line2 = tr("a-hem... welcome mortals!")
-var line3 = tr("welcome back to another edition of...")
+var line1 = tr("...WHAT?! Has a [i]century[/i] passed already?")
+var line2 = tr("A-hem... Welcome mortals!")
+var line3 = tr("Welcome back to another edition of...")
 
 var step = 0
 var lines := [line1, line2, line3]
+var voice_lines = ["res://assets/audio/voice/ref0.wav", "res://assets/audio/voice/ref1.wav", "res://assets/audio/voice/ref2.wav", "res://assets/audio/voice/ref4.wav", "res://assets/audio/voice/ref5.wav", "res://assets/audio/voice/ref6.wav"]
 onready var typewriter = $Typewriter
 
 export var main_screen : PackedScene
@@ -19,7 +20,6 @@ func _ready():
 	if global.first_time:
 		disclaimer.start()
 		yield(disclaimer, "okay")
-	anim.play("Appear")
 	typewriter.type(line1)
 
 func go_ahead():
@@ -34,7 +34,17 @@ func _on_Typewriter_done():
 	if step == 1:
 		anim.play("Referee")
 	elif step >= len(lines):
-		go_ahead()
+		anim.play("Appear")
+		go_ahead() 
 		set_process_input(false)
 		return 
 	typewriter.type(lines[step])
+	$AudioStreamPlayer2D2.stream = load(voice_lines[step])
+	yield(get_tree().create_timer(1), "timeout")
+	$AudioStreamPlayer2D2.play()
+
+
+func _on_Control_completed():
+	# SKIP the intro
+	go_ahead()
+	set_process_input(false)
