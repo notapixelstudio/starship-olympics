3cfbc247d0a774684c2c354eb689b830f4353425
nitaku <matteo.abrate@gmail.com>
2022-01-21 01:15:38 +0100

    first steps towards team color

--- godot/actors/battlers/Ship.gd
+++ godot/actors/battlers/Ship.gd
@@ -210,6 +210,12 @@ func _ready():
 	$DashParticles.process_material = dash_process_material
 	$Graphics/ChargeBar/Crosshair.modulate = species.color
 	
+	# if we are on a proper team, switch on the outline
+	if info_player.team != info_player.id:
+		$Graphics/Sprite.material.set_shader_param('active', true)
+		var color = Color('#ff2e2e') if info_player.team == 'A' else Color('#1a59ff')
+		$Graphics/Sprite.material.set_shader_param('outline_color', color)
+	
 func change_engine(value: bool):
 	responsive = value
 	set_physics_process(responsive)
