048cebf649c7268e00d0456c44dad3e798694e6d
nitaku <matteo.abrate@gmail.com>
2022-01-24 23:07:32 +0100

    variant in 2 players crown

--- godot/actors/environments/NoCrownZone.gd
+++ godot/actors/environments/NoCrownZone.gd
@@ -5,13 +5,17 @@ export var visible_decorations := true
 export var symbol_scale := 2.0
 export var decoration_scale := 1.0
 
+var gshape : GShape
+
 func _ready():
-	var gshape = self.get_gshape()
+	gshape = self.get_gshape()
 	
-	# connect feedback signal 
+	# connect feedback signal
 	self.connect("entered", self, "_on_Field_entered")
 	
+	gshape.connect("changed", self, "_on_gshape_changed")
 	$FeedbackLine.points = gshape.to_closed_PoolVector2Array()
+	
 	$Decoration.visible = visible_decorations
 	$NoCrownSign.scale = Vector2(symbol_scale, symbol_scale)
 	$Decoration.scale = Vector2(decoration_scale, decoration_scale)
@@ -27,3 +31,6 @@ func _on_Field_entered(field, body):
 		$AnimationPlayer.play("Feedback")
 		$AudioStreamPlayer2D.play()
 		
+func _on_gshape_changed():
+	$FeedbackLine.points = gshape.to_closed_PoolVector2Array()
+	
