781ddd4d90ed879644fa1b92a6ad7f12406f466e
salvob <tholalak@yahoo.it>
2022-06-19 17:22:34 +0200

    fix(ui): wait all players to be ready!

--- godot/map_with_sets/DescriptionMode.gd
+++ godot/map_with_sets/DescriptionMode.gd
@@ -4,6 +4,8 @@ export var gamemode : Resource setget set_gamemode
 
 onready var animator = $AnimationPlayer
 
+var array_players = [] # Array of InfoPlayers
+
 ##  signal to emit when we are ready to fight
 signal ready_to_fight
 
@@ -11,6 +13,7 @@ func _ready():
 	set_process_input(false)
 	refresh()
 	$Continue.modulate *= Color(1,1,1,0)
+
 
 func refresh():
 	if not is_inside_tree():
@@ -27,12 +30,21 @@ func set_gamemode(value: GameMode):
 
 signal letsfight
 
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		set_process_input(false)
-		disappears()
-
 func appears():
+	if global.is_game_running():
+		array_players = global.the_game.get_players()
+	for p_node in $PlayersReady.get_children():
+		p_node.hide()
+	for player_info in array_players:
+		var player_ready = $PlayersReady.get_node((player_info as InfoPlayer).id)
+		$PlayersReady.get_node((player_info as InfoPlayer).id).visible= true
+		$PlayersReady.get_node((player_info as InfoPlayer).id).set_player_info((player_info as InfoPlayer))
+	for p_node in $PlayersReady.get_children():
+		# this is just because if not, we will receive multiple signals from player_node that are 
+		# not playing. TODO: might need some love
+		if not p_node.visible: 
+			p_node.queue_free()
+	Events.connect("player_ready", self, "a_player_is_ready")
 	visible = true
 	$AudioStreamPlayer.play()
 	$Description.type(tr(gamemode.description))
@@ -52,3 +64,14 @@ func demomode(demo = false):
 
 func _on_Description_done():
 	set_process_input(true)
+
+var players_ready = []
+func a_player_is_ready(player_info: InfoPlayer):
+	print(player_info.id + "is ready")
+	players_ready.append(player_info)
+	for p_info in array_players:
+		if not p_info in players_ready:
+			return false
+	disappears()
+
+	
