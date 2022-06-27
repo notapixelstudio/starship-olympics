eea5d65370422cec1a70fb05327ba71c86781d2b
salvob <tholalak@yahoo.it>
2022-06-27 18:17:55 +0200

    fix: disappear ship when tapping it

--- godot/map/draft/DraftCard.gd
+++ godot/map/draft/DraftCard.gd
@@ -14,5 +14,8 @@ func tap(author):
 	if author is Ship:
 		ship = author
 		set_player(author.get_player())
+		# remove author
+		author.get_parent().remove_child(author)
+		
 
 	print("{minigame} tapped by {author_name}".format({"minigame": card_content.id, "author_name":author.info_player.id}))
