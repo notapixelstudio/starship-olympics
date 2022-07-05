529ed7da1e5a230c79633779320bdc2d0a2ebc8a
nitaku <matteo.abrate@gmail.com>
2022-07-05 20:25:01 +0200

    more refined brick tennis

--- godot/actors/environments/PlayerBricks.gd
+++ godot/actors/environments/PlayerBricks.gd
@@ -5,18 +5,23 @@ var player
 
 signal goal_done
 
+var bricks := []
+
 func _ready():
-	var player_spawner = get_node(goal_owner)
-	if player_spawner:
-		yield(player_spawner, "player_assigned")
-		set_player(player_spawner.get_player())
-		
 	for brick in get_children():
 		if not brick is Brick:
 			continue
 			
+		bricks.append(brick)
 		brick.connect('killed', self, '_on_brick_destroyed')
-
+		
+	var player_spawner = get_node(goal_owner)
+	if player_spawner:
+		yield(player_spawner, "player_assigned")
+		set_player(player_spawner.get_player())
+		for brick in bricks:
+			brick.set_color(player.species.color)
+		
 func get_score():
 	return -1
 	
@@ -28,7 +33,6 @@ func _on_brick_destroyed(brick, breaker):
 
 func set_player(v : InfoPlayer):
 	player = v
-	modulate = player.species.color
 	
 func get_player():
 	return player
