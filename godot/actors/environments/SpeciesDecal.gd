620a0c1c979e74e809a026487f82634062c6a037
nitaku <matteo.abrate@gmail.com>
2022-07-08 01:27:30 +0200

    species decals converted into player IDs

--- godot/actors/environments/SpeciesDecal.gd
+++ godot/actors/environments/SpeciesDecal.gd
@@ -1,22 +1,12 @@
 tool
 extends Node2D
 
-export var size : float = 8 setget set_size
 export var goal_owner : NodePath
-var species
-
-func set_size(v):
-	size = v
-	yield(self, 'ready')
-	$Sprite.scale = Vector2(size, size)
 
 func _ready():
 	yield(get_tree(), 'idle_frame')
 	
-	species = get_node(goal_owner).species
-	modulate = species.color
-	$Sprite.texture = species.ship_w
-	
-	# decal is on the floor
-	$Sprite.position = global.isometric_offset.rotated(-global_rotation)
+	var player = get_node(goal_owner).get_player()
+	$Label.text = player.get_id()
+	$Label.modulate = player.get_color()
 	
