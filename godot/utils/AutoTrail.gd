555f06ae353a163401e18840f815ce9c3c8b6fe3
ekorina <alnitak.nik@gmail.com>
2022-07-13 18:01:00 +0200

    Fun with portals: they still don't work.

--- godot/utils/AutoTrail.gd
+++ godot/utils/AutoTrail.gd
@@ -4,11 +4,19 @@ export var starting_color := Color(1,1,1,0.1)
 export var ending_color := Color(1,1,1,0)
 export var length := 30
 export var width := 90
+export var max_segment_length := 1000
 
 var trail
 
 # create a new trail each time this node (hence its parent) enters the tree
 func _enter_tree():
+	self.create_trail()
+	
+# drop the trail onto the parent's parent on exiting the tree
+func _exit_tree():
+	self.drop_trail()
+	
+func create_trail():
 	trail = Trail2D.new()
 	trail.gradient = Gradient.new()
 	trail.gradient.set_color(0, ending_color)
@@ -16,9 +24,18 @@ func _enter_tree():
 	trail.trail_length = length
 	trail.width = width
 	add_child(trail)
+	trail.connect('point_added', self, '_on_trail_point_added')
 	
-# drop the trail onto the parent's parent on exiting the tree
-func _exit_tree():
+func drop_trail():
 	remove_child(trail)
 	get_parent().get_parent().add_child(trail)
 	trail.disappear()
+
+func _on_trail_point_added(point: Vector2, last_point):
+	if last_point == null:
+		return
+		
+	if point.distance_to(last_point) > max_segment_length:
+		trail.remove_last_point()
+		self.drop_trail()
+		self.create_trail()
