b7fd8a4371452a9d76a9864fdfe28c7d4b40ff23
ekorina <alnitak.nik@gmail.com>
2022-07-20 12:48:39 +0200

    Player counts in minigame list.

--- godot/ui/minigame_list/MinigameListItem.gd
+++ godot/ui/minigame_list/MinigameListItem.gd
@@ -8,7 +8,7 @@ var minigame: Minigame
 func set_minigame(v: Minigame, pool: CardPool) -> void:
 	minigame = v
 	$HBoxContainer/LogoContainer/Logo.texture = minigame.get_icon()
-	$HBoxContainer/Texts/Title.text = minigame.get_name()
+	$HBoxContainer/Texts/HBoxContainer/Title.text = minigame.get_name()
 	$HBoxContainer/Texts/Description.text = minigame.get_description()
 	$HBoxContainer/Unlocks/Id.text = minigame.get_id()
 	
@@ -17,3 +17,24 @@ func set_minigame(v: Minigame, pool: CardPool) -> void:
 		var minilogo = MinigameMinilogoScene.instance()
 		minilogo.set_minigame(successor)
 		$HBoxContainer/Unlocks/List.add_child(minilogo)
+	
+	var player_counts = ''
+	if minigame.level_1players:
+		player_counts += '1'
+	else:
+		player_counts += '[color=#555555]1[/color]'
+	if minigame.level_2players:
+		player_counts += '2'
+	else:
+		player_counts += '[color=#555555]2[/color]'
+	if minigame.level_3players:
+		player_counts += '3'
+	else:
+		player_counts += '[color=#555555]3[/color]'
+	if minigame.level_4players:
+		player_counts += '4'
+	else:
+		player_counts += '[color=#555555]4[/color]'
+		
+	$HBoxContainer/Texts/HBoxContainer/PlayerCount.bbcode_text = '[center]'+player_counts+'[/center]'
+	
