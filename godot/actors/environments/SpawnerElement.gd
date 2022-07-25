31e84b6bfd1bfe41b58dec2997dadf6c7c5b0143
nitaku <matteo.abrate@gmail.com>
2022-07-25 17:37:42 +0200

    SpawnerElement class documented

--- godot/actors/environments/SpawnerElement.gd
+++ godot/actors/environments/SpawnerElement.gd
@@ -1,30 +1,22 @@
-extends Node2D
-
 tool
 
+extends Node2D
 class_name SpawnerElement
-export var element_scene: PackedScene
-export var wave: int
 
-var children_ = []
+export var element_scene: PackedScene
+export var preview_sprite_name := "Sprite"
 
 func _ready():
+	# in editor, just use the element's sprite texture instead of the actual element
 	var element = element_scene.instance()
 	if Engine.is_editor_hint():
-		$Sprite.texture = element.get_node("Sprite").texture
+		$PreviewSprite.texture = element.get_node(preview_sprite_name).texture
 	else:
-		$Sprite.queue_free()
+		$PreviewSprite.queue_free()
 		
-		
-	
 func spawn():
 	var element = element_scene.instance()
 	add_child(element)
-		#var diamond = diamond_scene.instance()
-		# offset of the spawner
-		#diamond.position = element.position + position
-		#collectables.append(diamond)
-	#return collectables
 	
 func remove_child(element):
 	.remove_child(element)
