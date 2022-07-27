4abc2249fe39b8ae02c9217c3a0b252b24931b3d
ekorina <alnitak.nik@gmail.com>
2022-07-27 15:24:05 +0200

    Gates as Rings, detection is still unreliable.

--- godot/actors/environments/Gate.gd
+++ godot/actors/environments/Gate.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-export var aperture := PI
+export var aperture := PI*0.95
 signal goal_done
 
 func _physics_process(delta):
@@ -8,7 +8,7 @@ func _physics_process(delta):
 		if body is Ship:
 			var relative_position : Vector2 = body.global_position - global_position
 			var relative_future_position : Vector2 = relative_position + body.velocity*delta
-			var will_cross : bool = abs(relative_position.angle() - global_rotation) <= PI/2 and abs(relative_future_position.angle() - global_rotation) > PI/2
+			var will_cross : bool = abs(relative_position.angle() - global_rotation) <= PI/2 and abs(relative_future_position.angle() - global_rotation) >= PI/2
 			var relative_angle_of_incidence : float = abs(body.velocity.angle() - global_rotation)
 			
 			if will_cross and relative_angle_of_incidence > PI/2 + (PI-aperture)/2:
