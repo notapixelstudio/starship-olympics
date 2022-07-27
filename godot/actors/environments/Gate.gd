934aeff9475111c6448111853245a27307d47743
ekorina <alnitak.nik@gmail.com>
2022-07-27 19:35:53 +0200

    WIP Gate enhancements.

--- godot/actors/environments/Gate.gd
+++ godot/actors/environments/Gate.gd
@@ -8,6 +8,7 @@ func _physics_process(delta):
 		if body is Ship:
 			var relative_position : Vector2 = body.global_position - global_position
 			var relative_future_position : Vector2 = relative_position + body.velocity*delta
+			var gate_top_end
 			var will_cross : bool = abs(relative_position.rotated(-global_rotation).angle()) <= PI/2 and abs(relative_future_position.rotated(-global_rotation).angle()) >= PI/2
 			var relative_angle_of_incidence : float = body.velocity.rotated(-global_rotation).angle()
 			
