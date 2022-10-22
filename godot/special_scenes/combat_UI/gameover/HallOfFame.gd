55243d4a7859c46d83ac7e06bb821f7f0366b70b
salvob <tholalak@yahoo.it>
2022-10-22 20:42:38 +0200

    fix(hall-of-fame): mockup

--- godot/special_scenes/combat_UI/gameover/HallOfFame.gd
+++ godot/special_scenes/combat_UI/gameover/HallOfFame.gd
@@ -6,9 +6,17 @@ func cleanup_datetime_str(datetime_str: String):
 	return datetime_str.replace("T", " ")
 
 func _ready():
-	$"%Label".queue_free() 
-	$"%WinnerBanner".queue_free()
+	
 	var data = global.read_file_by_line(InfoChampion.PATH_FILE_CHAMPIONS)
+	if data.empty():
+		for i in range(30):
+			var l = $"%Label".duplicate()
+			var champ_scene = $"%WinnerBanner".duplicate()
+			$"%SessionWon".add_child(l)
+			$"%SessionWon".add_child(champ_scene)
+	else:
+		$"%Label".queue_free() 
+		$"%WinnerBanner".queue_free()
 	data.invert()
 	for champion in data:
 		var champ_info := InfoChampion.new()
@@ -22,7 +30,7 @@ func _ready():
 		$"%SessionWon".add_child(l)
 		$"%SessionWon".add_child(champ_scene)
 	yield(get_tree(), "idle_frame")
-	$"%ScrollContainer".scroll_vertical = 1000
+	$"%ScrollContainer".scroll_vertical = pow(10, 4)
 	var tween := create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
 	tween.chain().tween_property($"%ScrollContainer", 'scroll_vertical', 0, 4)
 
