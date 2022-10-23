d18fc89b807adf8d829ebdf953fe58313cd44802
salvob <tholalak@yahoo.it>
2022-10-23 13:29:58 +0200

    fix(session-winners): almost ready. need to link

--- godot/special_scenes/combat_UI/gameover/HallOfFame.gd
+++ godot/special_scenes/combat_UI/gameover/HallOfFame.gd
@@ -6,17 +6,23 @@ func cleanup_datetime_str(datetime_str: String):
 	return datetime_str.replace("T", " ")
 
 func _ready():
-	
+	set_process_input(false)
 	var data = global.read_file_by_line(InfoChampion.PATH_FILE_CHAMPIONS)
 	if data.empty():
 		for i in range(30):
+			var info_player := InfoPlayer.new()
+			var champ = InfoChampion.new()
+			info_player.set_species(global.get_species(TheUnlocker.unlocked_elements["species"].keys()[randi()%4]))
+			champ.player = info_player.to_dict()
+			champ.session_info = {"timestamp": global.datetime_to_str(OS.get_datetime(true))}
 			var l = $"%Label".duplicate()
 			var champ_scene = $"%WinnerBanner".duplicate()
+			champ_scene.set_player(champ)
 			$"%SessionWon".add_child(l)
 			$"%SessionWon".add_child(champ_scene)
-	else:
-		$"%Label".queue_free() 
-		$"%WinnerBanner".queue_free()
+	
+	$"%Label".queue_free() 
+	$"%WinnerBanner".queue_free()
 	data.invert()
 	for champion in data:
 		var champ_info := InfoChampion.new()
@@ -33,7 +39,17 @@ func _ready():
 	$"%ScrollContainer".scroll_vertical = pow(10, 4)
 	var tween := create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
 	tween.chain().tween_property($"%ScrollContainer", 'scroll_vertical', 0, 4)
-
+	yield(tween, "finished")
+	# you can write your name now
+	var this_champion = $"%SessionWon".get_child(1)
+	this_champion.insert_name()
+	this_champion.connect("champion_has_a_name", self, "naming_champions" )
+	
+func naming_champions():
+	print("Everything is set")
+	set_process_input(true)
+	
+	
 func _input(event):
 	if event.is_action_pressed("confirm"):
 		Events.emit_signal("nav_to_map")
--- godot/special_scenes/combat_UI/gameover/WinnerBanner.gd
+++ godot/special_scenes/combat_UI/gameover/WinnerBanner.gd
@@ -1,10 +1,20 @@
 extends Control
 
+signal champion_has_a_name 
+
+var this_champion : InfoChampion # InfoChampion
+
+func _ready():
+	$"%PlayerName".visible = true
+	$"%HBoxContainer".visible = false
+	this_champion = InfoChampion.new()
+	
 func set_player_name(player_name: String):
 	$"%PlayerName".text = player_name
 
 
 func set_player(champion: InfoChampion):
+	this_champion = champion.duplicate()
 	$"%PlayerName".text = champion.player.username if champion.player.username else champion.player.id
 	$"%Headshot".set_species(global.get_species(champion.player.species))
 	var matches = champion.session_info.get("matches", [])
@@ -12,3 +22,25 @@ func set_player(champion: InfoChampion):
 		var l = Label.new()
 		l.text = a_match["card_id"]
 		$"%StarsContainer".add_child(l)
+
+func insert_name():
+	$"%InsertName".connect("name_inserted", self, "_on_InsertName_name_inserted")
+	$"%PlayerName".visible = false
+	$"%HBoxContainer".visible = true
+	$"%InsertName".grab_focus()
+	$"%InsertName".set_process_input(true)
+
+
+func _on_InsertName_name_inserted(player_name: String):
+	set_player_name(player_name)
+	$"%PlayerName".visible = true
+	$"%HBoxContainer".queue_free()
+	$"%PlayerName".text = player_name
+	this_champion.player.username = player_name
+	this_champion.store()
+	var this_game : TheGame = global.get("the_game")
+	if this_game:
+		var players = this_game.get_players()
+		for player in players:
+			if player.id == this_champion.player.id:
+				player.username = player_name
