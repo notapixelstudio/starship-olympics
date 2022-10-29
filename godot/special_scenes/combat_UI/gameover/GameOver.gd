75e47e1c4db3769d4c8c72562ed4b644a26adba3
nitaku <matteo.abrate@gmail.com>
2022-10-29 16:09:29 +0200

    tweak(ui): better handling of continue and quit

--- godot/special_scenes/combat_UI/gameover/GameOver.gd
+++ godot/special_scenes/combat_UI/gameover/GameOver.gd
@@ -20,9 +20,44 @@ func _ready():
 func _on_Continue_pressed():
 	get_tree().paused = false
 	Events.emit_signal("continue_after_game_over", global.session.is_over())
+	
+func _on_Cards_pressed():
+	var confirm = sure_scene.instance()
+	for button in buttons.get_children():
+		button.focus_mode = Control.FOCUS_NONE
+	add_child(confirm)
+	confirm.setup("cards")
+	yield(confirm, "choice_selected")
+	if confirm.choice:
+		get_tree().paused = false
+		Events.emit_signal("nav_to_map")
+	else:
+		for button in buttons.get_children():
+			if not button is Button:
+				continue
+			button.focus_mode = Control.FOCUS_ALL
+		buttons.get_child(0).grab_focus()
+		
+	confirm.queue_free()
 
 func _on_Quit_pressed():
-	global.end_execution()
+	var confirm = sure_scene.instance()
+	for button in buttons.get_children():
+		button.focus_mode = Control.FOCUS_NONE
+	add_child(confirm)
+	confirm.setup("quit_current_game")
+	yield(confirm, "choice_selected")
+	if confirm.choice:
+		get_tree().paused = false
+		Events.emit_signal("nav_to_menu")
+	else:
+		for button in buttons.get_children():
+			if not button is Button:
+				continue
+			button.focus_mode = Control.FOCUS_ALL
+		buttons.get_child(0).grab_focus()
+		
+	confirm.queue_free()
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_accept"):
@@ -41,24 +76,6 @@ func _show_arena():
 	set_process_unhandled_input(true)
 	self.visible = false
 	emit_signal("show_arena")
-
-
-func _on_Map_pressed():
-	var confirm = sure_scene.instance()
-	for button in buttons.get_children():
-		button.focus_mode = Control.FOCUS_NONE
-	add_child(confirm)
-	confirm.setup("map")
-	yield(confirm, "choice_selected")
-	if confirm.choice:
-		get_tree().paused = false
-		Events.emit_signal("nav_to_map")
-	else:
-		for button in buttons.get_children():
-			button.focus_mode = Control.FOCUS_ALL
-	confirm.queue_free()
-	
-
 
 func _on_LeaderBoard_animation_over():
 	if global.demo:
