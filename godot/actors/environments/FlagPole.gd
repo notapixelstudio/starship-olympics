0482eeaa3ef4fea8a231fb5f83340fdc459967af
Matteo Abrate <matteo.abrate@gmail.com>
2022-11-26 20:10:08 +0100

    feat(minigames): capture the flag (all player counts)

--- godot/actors/environments/FlagPole.gd
+++ godot/actors/environments/FlagPole.gd
@@ -3,8 +3,14 @@ class_name FlagPole
 
 var flags := []
 
+export var circuit_rotation_degrees := 0 setget set_circuit_rotation_degrees
+
 export var goal_owner : NodePath
 var player
+
+func set_circuit_rotation_degrees(v):
+	circuit_rotation_degrees = v
+	$"%Circuit".rotation_degrees = circuit_rotation_degrees
 
 func _ready():
 	var player_spawner = get_node(goal_owner)
@@ -17,6 +23,7 @@ func set_player(v : InfoPlayer):
 	$'%PlayerLabel'.text = player.get_username().to_upper()
 	$'%PlayerLabel'.modulate = player.species.color
 	$Sprite.modulate = player.species.color
+	$"%Circuit".modulate = player.species.color
 	
 func get_player():
 	return player
@@ -58,3 +65,6 @@ func _on_FlagPole_body_entered(body):
 		if body.get_player() != get_player() and not body.has_cargo() and len(flags) > 0:
 			var flag = pop_flag()
 			body.get_cargo().load_holdable(flag)
+	elif body is Ball and body.has_type('flag') and body.get_player() == get_player() and len(flags) < 3:
+		body.get_parent().remove_child(body)
+		place_flag(body)
