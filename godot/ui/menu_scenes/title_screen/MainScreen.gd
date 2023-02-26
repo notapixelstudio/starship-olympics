9a212d0ae7c6d01d1234bbdd4a8d34bd3ebb83eb
salvob <tholalak@yahoo.it>
2023-02-26 17:17:53 +0100

    fix(unlock): more clean and clear

--- godot/ui/menu_scenes/title_screen/MainScreen.gd
+++ godot/ui/menu_scenes/title_screen/MainScreen.gd
@@ -5,13 +5,12 @@ func _ready():
 	# TranslationServer.set_locale("es")
 	# Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	$Label.text = tr("DEMO BUILD - v"+ str(global.version))
-	
-	$"%Info".text = "Sessions played: {sessions_played}. Press SHIFT+R to reset".format({"sessions_played": global.sessions_played})
+	$"%Info".text = "Sessions played: {sessions_played}. Decks unlocked: {unlocked} \n Press SHIFT+R to reset".format({"sessions_played": global.sessions_played, "unlocked": TheUnlocker.get_unlocked_list("starting_decks")})
 
 func _process(delta):
 	if Input.is_key_pressed(KEY_SHIFT) and Input.is_key_pressed(KEY_R):
 		global.sessions_played = 0
 		# TheUnlocker.unlock_element("starting_decks", "winter", TheUnlocker.LOCKED)
 		TheUnlocker.reset_unlocks()
-		$"%Info".text = "Sessions played: {sessions_played}. Press SHIFT+R to reset".format({"sessions_played": global.sessions_played})
+		$"%Info".text = "Sessions played: {sessions_played}. Decks unlocked: {unlocked} \n Press SHIFT+R to reset".format({"sessions_played": global.sessions_played, "unlocked": TheUnlocker.get_unlocked_list("starting_decks")})
 		persistance.save_game()
