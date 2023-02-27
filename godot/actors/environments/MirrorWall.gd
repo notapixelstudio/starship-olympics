a2ef79a1d962f94a5b747e3c2f6dbb6b12f721cb
Matteo Abrate <matteo.abrate@gmail.com>
2023-02-27 08:03:46 +0100

    feat(environments): Reflector Walls

--- godot/actors/environments/MirrorWall.gd
+++ godot/actors/environments/MirrorWall.gd
@@ -3,6 +3,12 @@ extends Wall
 
 class_name MirrorWall
 
+const texture = preload('res://assets/patterns/rhombus.png')
+
+func _ready():
+	$InnerPolygon2D.texture = texture
+	$InnerPolygon2D.texture_scale = Vector2(1,1)
+
 func damage(hazard, damager):
 	$AnimationPlayer.advance(10) # should be enough to end the current animation and make the sector flash again
 	$AnimationPlayer.play("Hit")
