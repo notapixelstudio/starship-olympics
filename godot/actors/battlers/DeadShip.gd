433f6e82155e9fd9c40e07dd11a8283ec17039e2
nitaku <matteo.abrate@gmail.com>
2023-03-04 22:39:50 +0100

    tweak(usability): deadship flashes on damage received

--- godot/actors/battlers/DeadShip.gd
+++ godot/actors/battlers/DeadShip.gd
@@ -45,3 +45,7 @@ func _integrate_forces(state):
 const CAMERA_RECT_SIZE := 800.0
 func get_camera_rect() -> Rect2:
 	return Rect2(global_position - Vector2(CAMERA_RECT_SIZE,CAMERA_RECT_SIZE)/2, Vector2(CAMERA_RECT_SIZE,CAMERA_RECT_SIZE))
+
+func damage(hazard, damager) -> void:
+	$HitAnimationPlayer.play("hit")
+	
