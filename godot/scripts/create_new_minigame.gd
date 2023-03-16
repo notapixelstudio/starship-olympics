fc547d490d759c1c9d1e7738c6625d8764fd8bd9
ekorina <alnitak.nik@gmail.com>
2023-03-16 21:34:41 +0100

    WIP

--- godot/scripts/create_new_minigame.gd
+++ godot/scripts/create_new_minigame.gd
@@ -1,28 +1,28 @@
 tool
 extends EditorScript
 
-const NAME := "ClaimTheBanner"
-const TEMPLATE := "TakeTheCrown"
-const SUIT_TOP := 'red'
-const SUIT_BOTTOM := 'red'
+const NAME := "Claim The Banner"
+const TEMPLATE := "Diamondsnatch"
 
 const LEVELS_BASE_DIR := "res://combat/levels/singles/"
 const MINIGAMES_BASE_DIR := "res://combat/minigames/"
 
 func _run():
+	var dir = Directory.new()
+	
 	var camelcase_name = NAME.capitalize().replace(' ','')
 	var snakecase_name = NAME.capitalize().to_lower().replace(' ','_')
-	print(camelcase_name)
-	print(snakecase_name)
 	
 	var template = load(MINIGAMES_BASE_DIR+TEMPLATE+".tres")
-	print(template.suit_top)
+	dir.make_dir(LEVELS_BASE_DIR+snakecase_name)
+	dir.copy(template.arenas_dir+"/2players.tscn",LEVELS_BASE_DIR+snakecase_name+"/2players.tscn")
 	
-#	var dir = Directory.new()
-#	dir.make_dir(LEVELS_BASE_DIR+snakecase_name)
-#
-#	var minigame = Minigame.new()
-#	minigame.arenas_dir = LEVELS_BASE_DIR+snakecase_name
-#	minigame.suit_top = SUIT_TOP
-#	minigame.suit_bottom = SUIT_BOTTOM
-#	ResourceSaver.save("res://combat/minigames/"+camelcase_name+".tres", minigame)
+	var game_mode = GameMode.new()
+	game_mode.id = camelcase_name
+	game_mode.name = NAME
+	
+	var minigame = Minigame.new()
+	minigame.arenas_dir = LEVELS_BASE_DIR+snakecase_name
+	minigame.suit_top = template.suit_top
+	minigame.suit_bottom = template.suit_bottom
+	ResourceSaver.save("res://combat/minigames/"+camelcase_name+".tres", minigame)
