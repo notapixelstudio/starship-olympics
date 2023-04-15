3e81e9adae319499e676da32c24d48d112159885
Matteo Abrate <matteo.abrate@gmail.com>
2023-04-15 13:14:58 +0200

    feat(cpu): diamond warfare brain

--- godot/actors/brains/DiamondWarfareBrain.gd
+++ godot/actors/brains/DiamondWarfareBrain.gd
@@ -1,6 +1,7 @@
 extends CPUBrain
 
-export var go_to_center_p := 0.1
+export var back_attack_distance := 1200
+export var back_attack_probability := 1.0
 
 var random_preference : int
 
@@ -14,14 +15,46 @@ func think():
 	set_stance('aggressive') # we CAN shoot in this minigame
 	log_strategy('')
 	
+	# avoiding dangers takes priority over the rest
+	var danger_points = get_danger_points_ahead()
+	if len(danger_points) > 0:
+		var escape_vector = global_position - compute_average_position(danger_points)
+		go_to(global_position + escape_vector)
+		log_strategy('avoid danger')
+		return
+	
+	# chasing diamonds takes priority over attacking ships
 	targets = get_tree().get_nodes_in_group('Diamond')
 	if len(targets) > 0:
 		go_to(targets[random_preference%len(targets)].global_position)
 		log_strategy('chase diamond')
 		return
+		
+	# if we have nothing else to do, attack ships
+	targets = get_tree().get_nodes_in_group('Ship')
+	var valid_targets = []
 	
-	if randf() < go_to_center_p:
-		go_to(Vector2(0,0))
-		log_strategy('go to center')
-	else:
-		forget_current_target_location()
+	for target in targets:
+		if target != controllee:
+			valid_targets.append(target)
+			
+	if len(valid_targets) > 0:
+		# choose a preferred target ship
+		var target = valid_targets[random_preference%len(valid_targets)]
+		# backwards weapon
+		if $NavigationAgent2D.distance_to_target() > back_attack_distance:
+			set_stance('quiet')
+			go_to(target.get_target_destination())
+			log_strategy('chase ship')
+		else:
+			set_stance('aggressive')
+			var escape_vector = global_position - target.get_target_destination()
+			go_to(global_position + escape_vector)
+			log_strategy('back attack')
+			if randf() <= back_attack_probability:
+				start_charging_to_dash(escape_vector.length()*0.2+randf()*350) # try to charge more if target is far
+		return
+	
+	set_stance('quiet')
+	go_home()
+	
