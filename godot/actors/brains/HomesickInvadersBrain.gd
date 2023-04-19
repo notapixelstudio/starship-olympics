90a5e47995d47f4fcc52091abe8954d3efb80394
Matteo Abrate <matteo.abrate@gmail.com>
2023-04-20 00:31:44 +0200

    feat(cpu): homesick invaders cpu

--- godot/actors/brains/HomesickInvadersBrain.gd
+++ godot/actors/brains/HomesickInvadersBrain.gd
@@ -1,5 +1,6 @@
 extends CPUBrain
 
+export var extents : Rect2 = Rect2(Vector2(-3200, 200), Vector2(6400, 1400))
 export var go_to_center_p := 0.1
 
 var random_preference : int
@@ -23,13 +24,16 @@ func think():
 				my_targets.append(target)
 		if len(targets) > 0:
 			go_to(my_targets[random_preference%len(my_targets)].global_position)
-			log_strategy('attempt bring back Alien')
+			log_strategy('bring back alien')
 			return
 			
-	targets = get_tree().get_nodes_in_group('Alien')
+	targets = []
+	for a in get_tree().get_nodes_in_group('Alien'):
+		if extents.has_point(a.global_position): # don't consider aliens outside the battlefield
+			targets.append(a)
 	if len(targets) > 0:
 		go_to(targets[random_preference%len(targets)].global_position)
-		log_strategy('chase diamond')
+		log_strategy('take alien')
 		return
 	
 	if randf() < go_to_center_p:
