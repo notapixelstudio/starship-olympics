507a363e8db25b373b965784c29eef25f8e19ab8
salvob <tholalak@yahoo.it>
2023-05-12 21:35:43 +0200

    fix(analytics): completely remove GameAnalytics setup

--- godot/addons/Godot-GameAnalytics/GodotGameAnalyticsPlugin.gd
+++
@@ -1,8 +0,0 @@
-extends EditorPlugin
-tool
-
-func _enter_tree():
-	add_autoload_singleton("GameAnalytics", "res://addons/Godot-GameAnalytics/GameAnalytics.gd")
-
-func _exit_tree():
-	remove_autoload_singleton("GameAnalytics")
