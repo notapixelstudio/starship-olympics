5395ea80ea7ef44ba01099404439d86098917138
ekorina <alnitak.nik@gmail.com>
2023-06-27 19:34:15 +0200

    feat(ui): Animations in fancy buttons.

--- godot/ui/fancy_menus/FancyButton.gd
+++ godot/ui/fancy_menus/FancyButton.gd
@@ -1,14 +1,16 @@
 extends TextureButton
 
 func _ready():
+	rect_pivot_offset = rect_size / 2.0
 	blur()
 	
 func focus():
-	self_modulate = Color(1.1, 1.1, 1.1)
+	self_modulate = Color(1.2, 1.2, 1.2)
+	$AnimationPlayer.play("Grow")
 
 func blur():
 	self_modulate = Color(0.4, 0.4, 0.4)
-	
+	$AnimationPlayer.play("Shrink")
 	
 func _on_FancyButton_focus_entered():
 	focus()
