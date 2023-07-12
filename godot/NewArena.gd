0467f70b84d81984efb350f2dd038efb8c29be3b
salvob <tholalak@yahoo.it>
2023-07-12 18:14:31 +0200

    fix(brains): test updated for swapping brains

--- godot/NewArena.gd
+++ godot/NewArena.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
-var brains:= []
+
 var ships := []
 func _ready():
-	brains = [$NewShip/PlayerBrain, $NewShip2/PlayerBrain]
 	ships = [$NewShip, $NewShip2]
 func _process(delta):
 	$Label2.text = str(Input.get_action_strength("ui_right"))
@@ -11,7 +10,12 @@ func _process(delta):
 var i = 0
 func _on_timer_timeout():
 	var labels = ["normal", "swap"]
-	for b in get_tree().get_nodes_in_group('brains'):
+	var brains := []
+	for s in ships:
+		for b in s.get_children():
+			if b is Brain:
+				brains.append(b)
+	for b in brains:
 		var index_ship = ships.find(b.get_parent())
 		b.reparent(ships[(index_ship+1)%len(ships)], false)
 	i+=1
