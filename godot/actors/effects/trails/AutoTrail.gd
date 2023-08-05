ccc02765b38216c3dfdd7a173018dbc54fcec8f8
Matteo Abrate <matteo.abrate@gmail.com>
2023-08-05 11:16:37 +0200

    feat(vfx): Trail and AutoTrail class hierarchy done - FlameTrail and MotionAutoTrail too

--- godot/actors/effects/trails/AutoTrail.gd
+++ godot/actors/effects/trails/AutoTrail.gd
@@ -1,22 +1,25 @@
 extends Trail
 class_name AutoTrail
 
+## A special kind of [Trail] that has the ability to be [method cut]. Whenever an AutoTrail is cut,
+## a disappearing copy is dropped onto the host's parent, and the current trail is started anew.
+## AutoTrails are also automatically cut when there's a sudden jump (configured via [member
+## long_segment_min_length]) or the AutoTrail exits the tree.
+
+## When the trail is cut, the copy that's leaft behind will disappear by shrinking at this rate (px/s).
 @export_range (0.0, 100000.0, 1.0, 'suffix:px/s') var disappear_speed := 100.0
+## The trail is automatically cut when there's a sudden jump that's longer than this value (px).
 @export_range(0, 100000, 1, 'suffix:px') var long_segment_min_length := 1000
 
-var host_parent : Node
-var detached := false
-
-func _ready():
-	super._ready()
-	host_parent = host.get_parent()
+var _detached := false
 
 func _process(delta):
-	if not detached and will_next_segment_be_too_long():
-		drop_duplicate()
+	# cut the trail if there's a sudden jump (e.g., portals)
+	if not _detached and _will_next_segment_be_too_long():
+		cut()
 		
 	# reduce the width of the trail if detached, until it reaches zero and then free it
-	if detached:
+	if _detached:
 		if width > 0:
 			width = max(0, width-delta*disappear_speed)
 		else:
@@ -25,28 +28,26 @@ func _process(delta):
 		
 	super._process(delta)
 	
-# drop the trail on exiting the tree
+# cut the trail on exiting the tree (e.g., the host is freed)
 func _exit_tree():
-	if not detached:
-		drop_duplicate.call_deferred()
+	if not _detached:
+		cut()
 		
-func will_next_segment_be_too_long() -> bool:
-	if curve.get_point_count() < 1:
+# test whether there will be a sudden jump in the trail
+func _will_next_segment_be_too_long() -> bool:
+	if _curve.get_point_count() < 1:
 		return false
-	var p1 = curve.get_point_position(curve.get_point_count()-1)
+	var p1 = _curve.get_point_position(_curve.get_point_count()-1)
 	var p2 = compute_new_point()
 	return p1.distance_to(p2) >= long_segment_min_length
 
-## drop this trail onto the host's parent
-func drop():
-	reparent.call_deferred(host.get_parent())
-	detached = true
-
-## drop a duplicate of this trail, then continue with a cleared state
-func drop_duplicate():
+# drop a duplicate of this trail
+func _drop_duplicate():
 	var dup = duplicate()
-	dup.curve = curve.duplicate()
-	dup.timestamps = timestamps.duplicate()
-	host_parent.add_child(dup)
-	dup.detached = true
+	host.get_parent().add_child.call_deferred(dup)
+	dup._detached = true
+
+## Cut this trail, leaving a disappearing copy behind.
+func cut() -> void:
+	_drop_duplicate()
 	clear()
--- godot/actors/effects/trails/Trail.gd
+++ godot/actors/effects/trails/Trail.gd
@@ -1,58 +1,76 @@
 extends Line2D
 class_name Trail
 
+## A special type of [Line2D] that automatically follows a [member host] [Node2D], drawing a trail.
+
+## The host this trail will follow. You must set this in order for the trail to work.
 @export var host : Node2D
+## An optional offset vector (in px) to change where the trail is attached to the host.
 @export var offset := Vector2(0, 0)
-@export var growing := true : set = set_growing
+## If true, the trail will reduce its length until it reaches zero. Works only if [member length_criterion] is true.
+@export var shrinking := false : set = set_shrinking
+## The rate (px/s) at which the trail reduces its length while shrinking.
 @export_range (0.0, 100000.0, 10.0, 'suffix:px/s') var shrink_speed := 500.0
 
 @export_group('Trail size')
+## If true, this trail is kept with a maximum of [member max_point_count] points in it.
 @export var count_criterion := true
+## If true, the maximum length for this trail will be enforced to be [member max_length] pixels.
 @export var length_criterion := false
+## If true, points older than [member max_age] secs will be removed from the trail.
 @export var age_criterion := false
+## How many points max are enforced to be in the trail, if [member count_criterion] is true.
 @export_range(0, 100000, 1) var max_point_count := 100
+## The maximum pixel length enforced for the trail, if [member length_criterion] is true.
 @export_range(0, 100000, 1, 'suffix:px') var max_length := 150
+## The maximum age in seconds for a point in the trail, if [member age_criterion] is true.
 @export_range(0.0, 100000.0, 0.1, 'suffix:s') var max_age := 1.0
 
-var curve := Curve2D.new()
-var timestamps := []
-var length : float
+var _curve : Curve2D
+var _timestamps : Array
+var _length : float
 
-func set_growing(v: bool) -> void:
-	growing = v
+## Sets whether this trail will be shrinking (i.e., it will reduce its length).
+## Works only if [member length_criterion] is true.
+func set_shrinking(v: bool) -> void:
+	shrinking = v
 	
 func _ready():
-	length = max_length
+	_curve = Curve2D.new()
+	_timestamps = []
+	_length = max_length
 	
 func _process(delta):
-	assert(curve.get_point_count() == timestamps.size())
+	assert(_curve.get_point_count() == _timestamps.size())
 	
 	# reduce the trail length if not growing
-	if not growing:
-		length = max(0, length - shrink_speed*delta)
+	if shrinking:
+		_length = max(0, _length - shrink_speed*delta)
 	else:
-		length = max_length
+		_length = max_length
 		
 	var new_point := compute_new_point()
 	
 	# add the new point only if it is far enough from the last one
-	if curve.get_point_count() == 0 or new_point.distance_to(curve.get_point_position(curve.get_point_count()-1)) > 0.1:
-		curve.add_point(new_point)
-		timestamps.append(Time.get_ticks_msec())
+	if _curve.get_point_count() == 0 or new_point.distance_to(_curve.get_point_position(_curve.get_point_count()-1)) > 0.1:
+		_curve.add_point(new_point)
+		_timestamps.append(Time.get_ticks_msec())
 	
-	while (count_criterion and curve.get_point_count() > max_point_count) or (length_criterion and curve.get_baked_length() > length) or (age_criterion and timestamps.size() > 0 and Time.get_ticks_msec()-timestamps[0] > max_age*1000.0):
-		curve.remove_point(0)
-		timestamps.pop_front()
+	while (count_criterion and _curve.get_point_count() > max_point_count) or (length_criterion and _curve.get_baked_length() > _length) or (age_criterion and _timestamps.size() > 0 and Time.get_ticks_msec()-_timestamps[0] > max_age*1000.0):
+		_curve.remove_point(0)
+		_timestamps.pop_front()
 		
-	redraw()
+	_redraw()
 	
+func _redraw() -> void:
+	set_points(_curve.get_baked_points())
+	
+## Returns the new point that this trail has added (or will add) in the current frame.
 func compute_new_point() -> Vector2:
 	return host.global_position + offset.rotated(host.global_rotation)
 	
-func redraw() -> void:
-	set_points(curve.get_baked_points())
-
+## Delete all points from this trail.
 func clear():
-	curve.clear_points()
-	timestamps.clear()
+	_curve.clear_points()
+	_timestamps.clear()
 	clear_points()
