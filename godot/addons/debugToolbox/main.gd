46e2977afea528943a581b00e7aa85f9c79b7aad
ekorina <alnitak.nik@gmail.com>
2023-09-12 14:49:11 +0200

    feat(debug): Starting to merge old debug nodes into the new system (ship debug vectors).

--- godot/addons/debugToolbox/main.gd
+++ godot/addons/debugToolbox/main.gd
@@ -4,11 +4,15 @@ extends EditorPlugin
 func _enter_tree():
 	# Initialization of the plugin goes here
 	add_custom_type("DebugNode", "Node", preload("DebugNode.gd"), preload("icons/DebugNode.svg"))
+	add_custom_type("DebugNode2D", "Node2D", preload("DebugNode.gd"), preload("icons/DebugNode2D.svg"))
+	add_custom_type("DebugNode3D", "Node3D", preload("DebugNode.gd"), preload("icons/DebugNode3D.svg"))
 	add_custom_type("FPSCounter", "Node2D", preload("FPSCounter.gd"), preload("icons/FPSCounter.svg"))
 	add_custom_type("TestGrid", "Node2D", preload("TestGrid.gd"), preload("icons/TestGrid.svg"))
 	
 func _exit_tree():
 	# Clean-up of the plugin goes here
 	remove_custom_type("DebugNode")
+	remove_custom_type("DebugNode2D")
+	remove_custom_type("DebugNode3D")
 	remove_custom_type("FPSCounter")
 	remove_custom_type("TestGrid")
