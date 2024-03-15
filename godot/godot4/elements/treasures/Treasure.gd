d9bbd1d87532f61f1ee06e13670679d9f066b1f8
Matteo Abrate <matteo.abrate@gmail.com>
2024-03-15 20:04:42 +0100

    phasing diamonds collect effect displacement fixed

--- godot/godot4/elements/treasures/Treasure.gd
+++ godot/godot4/elements/treasures/Treasure.gd
@@ -1,15 +1,15 @@
-@tool
 class_name Treasure
 extends RigidBody2D
 
 @export var collectable := true
 @export var points := 1
-@export var texture : Texture : set = set_texture
 @export var treasure_picked_scene : PackedScene
 
 func set_texture(v: Texture) -> void:
-	texture = v
 	%Sprite2D.texture = v
+	
+func get_texture() -> Texture:
+	return %Sprite2D.texture
 	
 func touched_by(toucher : Ship):
 	if not collectable:
@@ -19,7 +19,7 @@ func touched_by(toucher : Ship):
 	
 	# drop a treasure picked effect on parent
 	var picked_effect = treasure_picked_scene.instantiate()
-	picked_effect.set_texture(texture)
+	picked_effect.set_texture(get_texture())
 	picked_effect.global_position = global_position
 	get_parent().add_child(picked_effect)
 	
