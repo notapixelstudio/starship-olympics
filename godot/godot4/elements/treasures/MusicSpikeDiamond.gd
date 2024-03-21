e0057b1c89efc5d0ced0d7b09be9fbcce38efe04
Matteo Abrate <matteo.abrate@gmail.com>
2024-03-21 23:45:12 +0100

    improved feedback

---
+++ godot/godot4/elements/treasures/MusicSpikeDiamond.gd
@@ -0,0 +1,58 @@
+extends Treasure
+
+@export var good_texture : Texture
+@export var bad_texture : Texture
+@export var starting_beats := 0
+var _beats := 0
+var _even := true
+var _spikey := false
+
+func _ready():
+	Events.beat.connect(_on_beat)
+	
+	_beats = starting_beats
+	
+	if not collectable:
+		put_out_spikes()
+	
+func _on_beat(period:int) -> void:
+	%AnimationPlayer.stop()
+	if _even:
+		%AnimationPlayer.play('Pulse')
+	else:
+		%AnimationPlayer.play('PulseBack')
+	_beats = (_beats + 1) % period
+	_even = not _even
+	if _beats == 0:
+		switch()
+	
+func switch():
+	if collectable:
+		collectable = false
+		put_out_spikes()
+	else:
+		collectable = true
+		put_spikes_back_in()
+
+func put_out_spikes():
+	_spikey = true
+	solid = false
+	update_solid()
+	%Sprite2D.texture = bad_texture
+	%CollisionShape2D.shape.radius = 55
+	shine()
+	
+func put_spikes_back_in():
+	_spikey = false
+	solid = true
+	update_solid()
+	%Sprite2D.texture = good_texture
+	%CollisionShape2D.shape.radius = 70
+	shine()
+
+func hurt(sth):
+	if _spikey and sth.has_method('suffer_damage'):
+		sth.suffer_damage(1)
+		
+		%AnimationPlayer2.stop()
+		%AnimationPlayer2.play('KillFeedback')
--- godot/godot4/elements/treasures/SpikeDiamond.gd
+++ godot/godot4/elements/treasures/SpikeDiamond.gd
@@ -4,7 +4,6 @@ extends Treasure
 func hurt(sth):
 	if sth.has_method('suffer_damage'):
 		sth.suffer_damage(1)
-		shine()
 		
 		%AnimationPlayer.stop()
 		%AnimationPlayer.play('KillFeedback')
