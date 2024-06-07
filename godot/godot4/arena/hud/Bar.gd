34cdbfe529aa58a7b1ebd52688496573e35f05dc
ekorina <alnitak.nik@gmail.com>
2024-06-07 16:30:09 +0200

    Ticks and clamping for score bars.

--- godot/godot4/arena/hud/Bar.gd
+++ godot/godot4/arena/hud/Bar.gd
@@ -2,15 +2,44 @@ extends Control
 
 @export var max_value := 100.0
 @export var value := 0.0 : set = set_value
+@export var ticks_thickness := 3.0
 
 func set_value(v: float) -> void:
 	value = v
-	%Fill.size.y = size.y / max_value * value
+	%Fill.size.y = max(0, min(_get_max_size() / max_value * value, _get_max_size()))
 
 func set_species(species:Species) -> void:
 	modulate = species.get_color()
 	
+func _get_max_size() -> float:
+	return %Background.size.y
+	
+func _get_width() -> float:
+	return %Background.size.x
+	
+	
 func _ready() -> void:
-	%Label.size.x = size.x
+	%Label.size.x = _get_width()
 	%Label.position.x = 0
-	%Label.position.y = size.y
+	%Label.position.y = _get_max_size()
+	
+	# _draw() is automatically called once
+	# this assumes that ticks should never be redrawn
+
+func _draw() -> void:
+	_draw_ticks()
+	
+func _draw_ticks() -> void:
+	for i in range(1, int(max_value)):
+		# skip some ticks according to max score's order of magnitude
+		if max_value > 99 and i%10 != 0:
+			continue
+		
+		var d = _get_max_size() - round(i * _get_max_size()/max_value)
+		var color = Color.BLACK
+		if max_value <= 10 or (i%10 == 0 and max_value < 100) or i%100 == 0:
+			color.a = 0.9
+		else:
+			color.a = 0.3
+		draw_line(Vector2(0,d),Vector2(_get_width(),d), color, ticks_thickness)
+		
