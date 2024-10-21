7c78f5301ae7a3c8a95bcab158227daa82098fca
ekorina <alnitak.nik@gmail.com>
2024-10-21 22:56:55 +0200

    WIP

--- godot/special_scenes/combat_UI/session_points/Leaderboard.gd
+++ godot/special_scenes/combat_UI/session_points/Leaderboard.gd
@@ -3,55 +3,64 @@ extends MarginContainer
 const pad = Vector2(0, 140)
 
 @export var pilot_stats_scene : PackedScene 
+@export var players : Array[Player]
 
 signal animation_over
-@onready var container = $Chart
 
-func setup():
+func _ready() -> void:
+	#var player_index = global.the_game.get_player_index()
+	#var match_played = global.the_match
+	#global.session.update_scores(match_played)
 	
-	var player_index = global.the_game.get_player_index()
-	var match_played = global.the_match
-	global.session.update_scores(match_played)
+	#var winners = match_played.winners
+	#var previous_leaderboard = global.session.get_previous_leaderboard()
 	
-	var winners = match_played.winners
-	var previous_leaderboard = global.session.get_previous_leaderboard()
-	
-	var max_points = global.win # Points of the session
+	#var max_points = global.win # Points of the session
 	# sorted before and sorted after
-	var i = 0
-	for player_dict in previous_leaderboard:
-		var player : InfoPlayer = player_index[player_dict["id"]]
-		var pilot_stats = pilot_stats_scene.instantiate()
-		pilot_stats.max_points = max_points
-		pilot_stats.position = pad*i
-		container.add_child(pilot_stats)
+	#var i = 0
+	#for player_dict in previous_leaderboard:
+	#	var player : InfoPlayer = player_index[player_dict["id"]]
+	#	var pilot_stats = pilot_stats_scene.instantiate()
+	#	pilot_stats.max_points = max_points
+	#	pilot_stats.position = pad*i
+	#	container.add_child(pilot_stats)
 		
-		if player in winners:
-			pilot_stats.just_won = true
-		pilot_stats.set_info(player)
+	#	if player in winners:
+	#		pilot_stats.just_won = true
+	#	pilot_stats.set_info(player)
+	#	i+=1
+		
+	var i := 0
+	for player in players:
+		var pilot_stats = pilot_stats_scene.instantiate()
+		pilot_stats.set_player(player)
+		pilot_stats.set_session_score(1)
+		pilot_stats.position = pad*i
+		%Container.add_child(pilot_stats)
 		i+=1
 		
-	$"%AnimationPlayer".play("entrance")
+	%AnimationPlayer.play("entrance")
 	
 	
 func reorder():
-	var current_leaderboard = global.session.get_current_leaderboard()
-	var tween := create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
-	for pilot_stat in container.get_children():
-		var i = 0
-		for player_dict in current_leaderboard:
-			if player_dict["id"] == pilot_stat.info.get_id():
-				tween.parallel().tween_property(pilot_stat, 'position:y', pad.y*i, 1.0)
-			i += 1
+	pass
+	#var current_leaderboard = global.session.get_current_leaderboard()
+	#var tween := create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_IN_OUT)
+	#for pilot_stat in %Container.get_children():
+	#	var i = 0
+	#	for player_dict in current_leaderboard:
+	#		if player_dict["id"] == pilot_stat.info.get_id():
+	#			tween.parallel().tween_property(pilot_stat, 'position:y', pad.y*i, 1.0)
+	#		i += 1
 			
-	await tween.finished
-	emit_signal("animation_over")
-	if global.session and global.session.is_over():
-		celebrate()
+	#await tween.finished
+	#emit_signal("animation_over")
+	#if global.session and global.session.is_over():
+	#	celebrate()
 	
 func celebrate():
 	var session_winners = global.the_game.get_last_winners()
-	for pilot in container.get_children():
+	for pilot in %Container.get_children():
 		if pilot.info in session_winners:
 			# this is a session winner
 			pilot.celebrate()
--- godot/special_scenes/combat_UI/session_points/PilotStats.gd
+++ godot/special_scenes/combat_UI/session_points/PilotStats.gd
@@ -3,34 +3,40 @@ extends Node2D
 
 class_name PilotStats
 
-var info: InfoPlayer
+@export var player : Player : get = get_player, set = set_player
 
 var new_position : set = change_position
-var max_points = 0
+var max_session_score := 1 # default: standalone
 
-var just_won: bool = false
+func set_player(v: Player):
+	player = v
+	if not is_inside_tree():
+		await self.ready
+	%Headshot.set_player(player)
+	#for stats in $"%StatsContainer".get_children():
+	#	stats.set_stats_value(str(player_stats.get(stats.key)))
+	%Background.modulate = player.get_color()
+	%WinnerOutline.modulate = player.get_color()
+	%PlayerID.text = player.get_username()
+	%PlayerID.modulate = player.get_color()
+	
+func get_player() -> Player:
+	return player
 
-func set_info(value: InfoPlayer):
-	info = value
-	var player_stats: PlayerStats = info.stats
-	$"%Headshot".set_player(info)
-	for stats in $"%StatsContainer".get_children():
-		stats.set_stats_value(str(player_stats.get(stats.key)))
-	$"%StarsContainer".initialize(info.session_score, max_points, just_won)
-	$Background.modulate = info.get_color()
-	$WinnerOutline.modulate = info.get_color()
-	$"%PlayerID".text = info.get_username()
-	$"%PlayerID".modulate = info.get_color()
-
+func set_session_score(session_score: int, just_won:= false):
+	if not is_inside_tree():
+		await self.ready
+	%StarsContainer.initialize(session_score, max_session_score, just_won)
+	
 func change_position(new_value):
 	new_position = new_value
-	if position != new_position and not $Tween.is_active():
-		$Tween.interpolate_property(self, "position", self.position, new_position, 0.5, 
-			Tween.TRANS_CUBIC, Tween.EASE_IN, 0)
-		$Tween.start()
-
-func get_player_info():
-	return info
+	if position != new_position:# and not $Tween.is_active():
+		#$Tween.interpolate_property(self, "position", self.position, new_position, 0.5, 
+		#	Tween.TRANS_CUBIC, Tween.EASE_IN, 0)
+		#$Tween.start()
+		var tween := create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+		tween.tween_property(self, 'position', new_position, 0.5)
+		#await tween.finished
 
 func celebrate():
 	$AnimationPlayer.play("Victory")
