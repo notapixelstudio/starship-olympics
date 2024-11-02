9cc862f37c4ad05374a652f42e9fbc9bc3b46666
Matteo Abrate <matteo.abrate@gmail.com>
2024-11-03 00:28:56 +0100

    functional and decent diamondsnatch 2p with bubbles

--- godot/godot4/arena/managers/SpawnerWaveManager.gd
+++ godot/godot4/arena/managers/SpawnerWaveManager.gd
@@ -1,17 +1,18 @@
 extends Node
 
-@export var base_time := 2.5
-@export var waves : Array[SpawnerWave]
+@export var base_time := 2.0
+@export var waves_container : Node2D
 @export var battlefield: Node2D
 
 const WAVES_GROUP = "spawn_waves"
-const COLLECTABLE = "coin"
+#const COLLECTABLE = "collectable"
 const WAVE_DELAY = 0.0
 var to_next_wave = 2
 var current_wave = 0
 
 
 signal spawn_next
+var waves : Array
 var spawners_per_wave : Dictionary
 var how_many_spawners: int
 var current_spawners = 0
@@ -23,7 +24,9 @@ func _ready():
 	
 	Events.connect("spawned", Callable(self, "spawned"))
 	
-	Events.connect("sth_collected", Callable(self, "_on_sth_collected"))
+	#Events.connect("sth_collected", Callable(self, "_on_sth_collected"))
+	
+	waves = waves_container.get_children()
 	
 	setup()
 	
@@ -45,36 +48,48 @@ func spawned(element_spawned: ElementSpawnerGroup):
 	
 	
 func _handle_waves():
-	var no_spawners_left : bool = len(spawners_per_wave[current_wave]) == 0
-	var max_repeats_reached : bool = waves[current_wave].max_repeats != -1 and waves[current_wave].times_spawned >= waves[current_wave].max_repeats
-	if no_spawners_left or max_repeats_reached:
-		current_wave += 1
-	var last_wave : bool = current_wave >= len(spawners_per_wave)
-	if last_wave:
-		current_wave -= 1 # keep spawning the last wave
-		setup(current_wave)
-		
+	while true: # keep looking for a non-empty, non zero repeats wave
+		var over_last_wave : bool = current_wave >= len(spawners_per_wave)
+		if over_last_wave:
+			return
+		#	current_wave -= 1 # keep spawning the last wave
+		#	setup(current_wave)
+		print('wave ', current_wave)
+		var no_spawners_left : bool = len(spawners_per_wave[current_wave]) == 0
+		var max_repeats_reached : bool = waves[current_wave].max_repeats != -1 and waves[current_wave].times_spawned >= waves[current_wave].max_repeats
+		if no_spawners_left or max_repeats_reached:
+			current_wave += 1
+		else:
+			break
+	
+	var over_last_wave : bool = current_wave >= len(spawners_per_wave)
+	if over_last_wave:
+		return
+	#	current_wave -= 1 # keep spawning the last wave
+	#	setup(current_wave)
+	
 	var spawner: ElementSpawnerGroup = spawners_per_wave[current_wave].pop_back()
+	print('spawning from ', waves[current_wave].name)
 	#Events.emit_signal("ask_to_spawn", spawner, WAVE_DELAY + waves[current_wave].extra_delay)
 	spawner.spawn(battlefield)
 	waves[current_wave].times_spawned += 1
-	%Timer.wait_time = base_time + WAVE_DELAY + waves[current_wave].extra_delay
-	self.reset_wave_timer()
+	print('times spawned: ', waves[current_wave].times_spawned)
 	
-func reset_wave_timer():
+	var max_repeats_reached : bool = waves[current_wave].max_repeats != -1 and waves[current_wave].times_spawned >= waves[current_wave].max_repeats
 	%Timer.stop()
+	%Timer.wait_time = base_time + WAVE_DELAY + waves[min(current_wave+1,len(waves)-1) if max_repeats_reached else current_wave].extra_delay
 	%Timer.start()
 	
 func _on_Timer_timeout():
 	print("asking to spawn because timer of {timer_wait_time} has expired".format({"timer_wait_time": %Timer.wait_time}))
 	_handle_waves()
 
-func _on_sth_collected(_collector, collectee):
-	# if there are no collectables to be collected anymore. We can move on with spawning
-	var all = len(get_tree().get_nodes_in_group(COLLECTABLE))
-	if all == 1:
-		print("asking to spawn because there are no collectable anymore")
-		_handle_waves()
-	
+#func _on_sth_collected(_collector, collectee):
+#	# if there are no collectables to be collected anymore. We can move on with spawning
+#	var all = len(get_tree().get_nodes_in_group(COLLECTABLE))
+#	if all <= 1:
+#		print("asking to spawn because there are no collectable anymore")
+#		_handle_waves()
+
 func _on_match_over(data:Dictionary) -> void:
 	%Timer.stop()
--- godot/godot4/elements/bubbles/Bubble.gd
+++ godot/godot4/elements/bubbles/Bubble.gd
@@ -10,7 +10,7 @@ func set_content_scene(v:PackedScene) -> void:
 	_content = content_scene.instantiate()
 	%ContentSprite2D.texture = _content.get_texture() # FIXME strong assumption
 
-func _on_body_entered(body:RigidBody2D):
+func _on_area_2d_body_entered(body):
 	if body is Ship:
 		if body.is_dashing():
 			_content.global_position = global_position
@@ -20,3 +20,4 @@ func _on_body_entered(body:RigidBody2D):
 		else:
 			_bumps += 1
 			%Label.visible = _bumps >= 3
+			apply_central_impulse(max(8,body.linear_velocity.length()/100.0)*(global_position-body.global_position))
