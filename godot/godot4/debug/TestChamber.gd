ae3f3cfe5c4323677cf730e31eee797dc437c097
Matteo Abrate <matteo.abrate@gmail.com>
2024-11-06 01:17:06 +0100

    sfxs for bubbles

--- godot/godot4/debug/TestChamber.gd
+++ godot/godot4/debug/TestChamber.gd
@@ -1,6 +1,8 @@
 class_name TestChamber
 extends Node2D
 
+@export var test_player : Player
+@export var player_brain_scene : PackedScene
 var test_subject : Node2D : set = set_test_subject
 
 func set_test_subject(v : Node2D) -> void:
@@ -12,20 +14,15 @@ func _ready():
 	# default test subject is a node named Ship, accessible via local unique names
 	if get_node_or_null('%Ship'):
 		set_test_subject(%Ship)
-		var fake_player = Player.new()
-		%Ship.set_player(fake_player)
+		%Ship.set_player(test_player)
+		var brain = player_brain_scene.instantiate()
+		brain.set_controls(test_player.get_controls())
+		%Ship.add_child(brain)
 
 func _process(delta):
 	if not test_subject:
 		return
 		
-func _on_control_selector_item_selected(index):
-	if not test_subject or not test_subject is Ship or not test_subject.has_node('PlayerBrain'):
-		return
-		
-	test_subject.get_node('PlayerBrain').set_controls('kb1' if index == 0 else 'joy1')
-	%ControlSelector.release_focus()
-
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("debug_restart_scene"):
 		get_tree().reload_current_scene()
--- godot/godot4/elements/bubbles/Bubble.gd
+++ godot/godot4/elements/bubbles/Bubble.gd
@@ -1,4 +1,5 @@
 extends RigidBody2D
+class_name Bubble
 
 @export var content_scene : PackedScene : set = set_content_scene
 
@@ -13,11 +14,23 @@ func set_content_scene(v:PackedScene) -> void:
 func _on_area_2d_body_entered(body):
 	if body is Ship:
 		if body.is_dashing():
-			_content.global_position = global_position
-			get_parent().add_child.call_deferred(_content)
-			_content.touched_by.call_deferred(body) # FIXME strong assumption
+			if _content:
+				_content.global_position = global_position
+				get_parent().add_child.call_deferred(_content)
+				_content.touched_by.call_deferred(body) # FIXME strong assumption
+			SoundEffects.play(%PopSFX)
 			queue_free()
 		else:
 			_bumps += 1
 			%Label.visible = _bumps >= 3
+			SoundEffects.play(%BounceSFX)
 			apply_central_impulse(max(6,body.linear_velocity.length()/120.0)*(global_position-body.global_position))
+	
+func _on_body_entered(body):
+	if body is Ship:
+		return
+		
+	if body is Bubble and get_rid() <= body.get_rid():
+		return
+		
+	SoundEffects.play(%BounceSFX)
