817e230f49239d7aa7bdc48f79a15098b3f6e457
Matteo Abrate <matteo.abrate@gmail.com>
2024-11-10 23:31:00 +0100

    more experiments with meshes

--- godot/godot4/test/mesh.gd
+++ godot/godot4/test/mesh.gd
@@ -1,7 +1,5 @@
 extends MeshInstance2D
 
-var t := 0.0
-
 func _process(delta) -> void:
 	mesh.clear_surfaces()
 	
@@ -16,22 +14,20 @@ func _process(delta) -> void:
 		Vector2(200,200),
 		Vector2(400,200),
 		Vector2(200,400),
-		Vector2(300+100*sin(t),400)
+		Vector2(400,400)
 	])
 	surface_array[Mesh.ARRAY_TEX_UV] = PackedVector2Array([
 		Vector2(0,0),
-		Vector2(1,0),
-		Vector2(0,1),
-		Vector2(1,1),
+		Vector2(200,0),
+		Vector2(0,200),
+		Vector2(200,200),
 		Vector2(0,0),
-		Vector2(1,0),
-		Vector2(0,1),
-		Vector2(1,1),
+		Vector2(200,0),
+		Vector2(0,200),
+		Vector2(200,200),
 	])
 	surface_array[Mesh.ARRAY_INDEX] = PackedInt32Array([
 		0,1,2,1,2,3,4,5,6,5,6,7
 	])
 	
 	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface_array)
-	
-	t += delta
