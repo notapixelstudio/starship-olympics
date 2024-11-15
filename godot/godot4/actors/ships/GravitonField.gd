431de17c648366324a771014797a9bfb4d764d18
Matteo Abrate <matteo.abrate@gmail.com>
2024-11-15 14:40:59 +0100

    charge-based graviton field while charging

--- godot/godot4/actors/ships/GravitonField.gd
+++ godot/godot4/actors/ships/GravitonField.gd
@@ -3,6 +3,8 @@ extends Node2D
 @export var enabled : bool = false : set = set_enabled, get = get_enabled
 @export var gravity : float = 1024.0
 @export var influence_radius : float = 500.0
+@export var source : Node
+@export var source_strength_method_name : String = 'get_strength'
 
 func set_enabled(v):
 	enabled = v
@@ -24,5 +26,8 @@ func get_influence_radius():
 	return influence_radius
 	
 func get_gravity():
-	return gravity
+	if not source:
+		return gravity
+		
+	return gravity * source.get(source_strength_method_name).call()
 	
