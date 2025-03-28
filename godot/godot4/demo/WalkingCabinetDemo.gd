87f4d06a949ec26d31f47ccec418b2c208dca15a
ekorina <alnitak.nik@gmail.com>
2025-03-28 18:50:45 +0100

    coop support in walking cabinet demo

--- godot/godot4/demo/WalkingCabinetDemo.gd
+++ godot/godot4/demo/WalkingCabinetDemo.gd
@@ -3,6 +3,13 @@ extends Node2D
 @export var game_scene_2p : PackedScene
 @export var game_scene_3p: PackedScene
 @export var game_scene_4p: PackedScene
+@export var game_scene_1pve: PackedScene
+@export var game_scene_2pve: PackedScene
+@export var game_scene_3pve: PackedScene
+@export var game_scene_4pve: PackedScene
+
+var mode = 'vs'
+var player_count = 2
 
 var _current_game
 
@@ -15,7 +22,12 @@ func new_game(game_scene: PackedScene):
 		_current_game.queue_free()
 		
 	_current_game = game_scene.instantiate()
-	_current_game.session = SingleMatchSession.new()
+	var sesh 
+	if mode == 'vs':
+		sesh = SingleMatchSession.new()
+	else:
+		sesh = SinglePveMatchSession.new()
+	_current_game.session = sesh
 	add_child(_current_game)
 	
 	# hackish, I know
@@ -24,9 +36,32 @@ func new_game(game_scene: PackedScene):
 	
 func _input(event):
 	# reset the game2
-	if event.is_action_pressed("force_reset_2p"):
-		new_game(game_scene_2p)
+	if event.is_action_pressed("change_mode_vs"):
+		mode = 'vs'
+	elif event.is_action_pressed("change_mode_coop"):
+		mode = 'coop'
+	elif event.is_action_pressed("force_reset_1p"):
+		player_count = 1
+	elif event.is_action_pressed("force_reset_2p"):
+		player_count = 2
 	elif event.is_action_pressed("force_reset_3p"):
-		new_game(game_scene_3p)
+		player_count = 3
 	elif event.is_action_pressed("force_reset_4p"):
-		new_game(game_scene_4p)
+		player_count = 4
+	
+	if mode == 'vs':
+		if player_count == 2:
+			new_game(game_scene_2p)
+		elif player_count == 3:
+			new_game(game_scene_3p)
+		elif player_count == 4:
+			new_game(game_scene_4p)
+	else:
+		if player_count == 1:
+			new_game(game_scene_1pve)
+		elif player_count == 2:
+			new_game(game_scene_2pve)
+		elif player_count == 3:
+			new_game(game_scene_3pve)
+		elif player_count == 4:
+			new_game(game_scene_4pve)
