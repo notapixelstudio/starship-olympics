341e59b05c5177d4731c8a3d8e73f5957bbafd3b
ekorina <alnitak.nik@gmail.com>
2025-06-10 19:09:29 +0200

    Cleanup of sound volume settings.

--- godot/godot4/global/Settings.gd
+++ godot/godot4/global/Settings.gd
@@ -11,6 +11,9 @@ const LANGUAGES = {
 }
 var language : String = 'english' : set = _set_language
 var fullscreen : bool = true : set = _set_fullscreen
+var master : int = 50 : set = _set_master
+var music : int = 50 : set = _set_music
+var sfx : int = 50 : set = _set_sfx
 
 func _set_language(value: String):
 	language = value
@@ -26,3 +29,17 @@ func _set_fullscreen(value: bool):
 	else:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
+func _set_master(value: int):
+	master = value
+	var db_volume = linear_to_db(float(value)/100)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index('Master'), db_volume)
+
+func _set_music(value: int):
+	music = value
+	var db_volume = linear_to_db(float(value)/100)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index('Music'), db_volume)
+
+func _set_sfx(value: int):
+	sfx = value
+	var db_volume = linear_to_db(float(value)/100)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index('SFX'), db_volume)
---
+++ godot/godot4/ui/settings_controls/SettingSlider.gd
@@ -0,0 +1,26 @@
+extends Panel
+
+@export var setting_name : String
+
+@onready var sfx_effect  = $AudioStreamPlayer
+@onready var label = $VBoxContainer/Volume
+@onready var slider =$VBoxContainer/HSlider
+
+func _ready() -> void:
+	slider.value = Settings.get(setting_name)
+	label.text = tr(setting_name)
+	
+func _on_HSlider_value_changed(new_value: int):
+	Settings.set(setting_name, new_value)
+	sfx_effect.play()
+
+func _on_focus_entered() -> void:
+	slider.grab_focus()
+
+func _on_h_slider_focus_entered() -> void:
+	add_theme_stylebox_override("panel", load("res://interface/themes/olympic/focus.tres"))
+	label.set("theme_override_colors/font_color",Color(0,0,0))
+
+func _on_h_slider_focus_exited() -> void:
+	add_theme_stylebox_override("panel", load("res://interface/themes/olympic/normal.tres"))
+	label.set("theme_override_colors/font_color", null)
