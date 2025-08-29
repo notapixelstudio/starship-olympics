2b0e73ec1c26ff238b8cbbbe1f697dbdf19657c3
Nitaku <matteo.abrate@gmail.com>
2025-08-29 11:57:34 +0200

    remove Ship references from Cargo

--- godot/godot4/elements/cargos/Cargo.gd
+++ godot/godot4/elements/cargos/Cargo.gd
@@ -6,29 +6,18 @@ class_name Cargo
 var _self_scene : PackedScene
 var _untouchable_by = null
 
+func get_texture() -> Texture:
+	return %Sprite2D.texture
+
+func _ready():
+	_self_scene = PackedScene.new()
+	_self_scene.pack(self)
+	
 func place_and_push(global_pos: Vector2, vel: Vector2, rot: float) -> void:
 	global_position = global_pos + Vector2(drop_distance,0).rotated(rot)
 	linear_velocity = vel
 	reset_physics_interpolation() # TBD check if this suffices when https://github.com/godotengine/godot/pull/92218 is merged
 	# if it suffices, re-enable physics interpolation in Shadow.tscn
-	
-func touched_by(toucher : Ship):
-	if toucher == _untouchable_by:
-		return
-		
-	Events.sth_loaded.emit(toucher, self)
-	queue_free()
-
-func get_texture() -> Texture:
-	return %Sprite2D.texture
-
-func hit():
-	%SpriteAnimation.stop()
-	%SpriteAnimation.play('Hit')
-
-func _ready():
-	_self_scene = PackedScene.new()
-	_self_scene.pack(self)
 	
 func clone_as_new() -> Cargo:
 	return _self_scene.instantiate()
@@ -39,12 +28,26 @@ func _on_body_entered(body: Node) -> void:
 	if body is StaticBody2D:
 		Events.collision.emit(self, body)
 	
+func touched_by(actor) -> void:
+	if _is_untouchable_by(actor):
+		return
+		
+	Events.sth_loaded.emit(actor, self)
+	queue_free()
+	
+func _is_untouchable_by(sth) -> bool:
+	return sth == _untouchable_by
+
 func set_temp_untouchable_by(untoucher) -> void:
 	_untouchable_by = untoucher
 	%UntouchableTimer.start()
-
+	
 func _on_untouchable_timer_timeout() -> void:
 	_reset_untouchable()
 	
 func _reset_untouchable() -> void:
 	_untouchable_by = null
+	
+func hit():
+	%SpriteAnimation.stop()
+	%SpriteAnimation.play('Hit')
