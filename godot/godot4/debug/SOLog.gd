7eabe95f0b7d92e36b4be186b88a1987f0823887
ekorina <alnitak.nik@gmail.com>
2025-09-04 12:35:17 +0200

    updated test chambers

--- godot/godot4/debug/SOLog.gd
+++ godot/godot4/debug/SOLog.gd
@@ -4,6 +4,7 @@ extends "res://addons/debugToolbox/Log.gd"
 
 func _ready():
 	super._ready()
+	add_theme_font_size_override("normal_font_size", 40)
 	Events.log.connect(_on_log)
 	
 func _on_log(message: String):
