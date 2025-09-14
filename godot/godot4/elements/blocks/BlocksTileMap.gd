ab7f702b13fee9589a3ba607695fef9fde4b5a0e
Nitaku <matteo.abrate@gmail.com>
2025-09-14 22:42:54 +0200

    strange...

--- godot/godot4/elements/blocks/BlocksTileMap.gd
+++ godot/godot4/elements/blocks/BlocksTileMap.gd
@@ -16,6 +16,8 @@ const PIECES := [
 
 var _buffer : TileMapLayer
 
+var _pieces := {}
+
 var _tick := 0
 var _next_piece_id := 0
 
@@ -33,16 +35,26 @@ func get_all_falling_blocks_cells() -> Array[Vector2i]:
 	return get_used_cells_by_id(FALLING_BLOCKS_SOURCE_ID)
 	
 func get_all_pieces() -> Array[Dictionary]:
-	var pieces_index = {}
-	for cell in get_all_falling_blocks_cells():
-		var piece_id = get_cell_tile_data(cell).get_custom_data('piece_id')
-		if not pieces_index.has(piece_id):
-			pieces_index[piece_id] = []
-		pieces_index[piece_id].append(cell)
-		
+	#var pieces_index = {}
+	#for cell in get_all_falling_blocks_cells():
+		#var piece_id = get_cell_tile_data(cell).get_custom_data('piece_id')
+		#if not pieces_index.has(piece_id):
+			#pieces_index[piece_id] = []
+		#pieces_index[piece_id].append(cell)
+		#
+	#var pieces : Array[Dictionary] = []
+	#for id in pieces_index:
+		#var cells = pieces_index[id]
+		#pieces.append({
+			#'id': id,
+			#'cells': cells
+		#})
+		#
+	#return pieces
 	var pieces : Array[Dictionary] = []
-	for id in pieces_index:
-		var cells = pieces_index[id]
+	
+	for id in _pieces:
+		var cells = _pieces[id]
 		pieces.append({
 			'id': id,
 			'cells': cells
@@ -57,6 +69,7 @@ func tick() -> void:
 	# copy all placed blocks on the buffer
 	for cell in get_all_placed_blocks_cells():
 		_buffer.set_cell(cell, PLACED_BLOCKS_SOURCE_ID, get_cell_atlas_coords(cell))
+		
 	var still_falling_pieces = get_all_pieces()
 	var newly_stopped_found = true
 	while newly_stopped_found:
@@ -72,12 +85,15 @@ func tick() -> void:
 			if should_stop:
 				newly_stopped_found = true
 				still_falling_pieces.erase(piece)
+				_pieces.erase(piece['id'])
 				for cell in piece['cells']:
 					var atlas_coords = get_cell_atlas_coords(cell)
 					_buffer.set_cell(cell, PLACED_BLOCKS_SOURCE_ID, atlas_coords + Vector2i(0, -2))
 	for piece in still_falling_pieces:
+		_pieces[piece['id']] = []
 		for cell in piece['cells']:
 			_buffer.set_cell(cell+Vector2i(0,1), FALLING_BLOCKS_SOURCE_ID, get_cell_atlas_coords(cell))
+			_pieces[piece['id']].append(cell+Vector2i(0,1))
 	
 	if _tick % spawn_every_ticks == 0:
 		spawn_new_piece()
@@ -95,9 +111,11 @@ func spawn_new_piece() -> void:
 	spawn_piece(piece_to_spawn, from_where, random_piece_i)
 	
 func spawn_piece(cells, from_where, color_i) -> void:
+	_pieces[_next_piece_id] = []
 	for cell in cells:
 		# TBD check if space is available (game over?)
 		_buffer.set_cell(cell+from_where, FALLING_BLOCKS_SOURCE_ID, Vector2i(color_i,2))
-		_buffer.get_cell_tile_data(cell+from_where).set_custom_data('piece_id', _next_piece_id)
+		#_buffer.get_cell_tile_data(cell+from_where).set_custom_data('piece_id', _next_piece_id)
+		_pieces[_next_piece_id].append(cell+from_where)
 		
 	_next_piece_id += 1
