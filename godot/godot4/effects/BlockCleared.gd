b73b0ebde3ebd31d05bd1ca54cebc8cb27bfb6cc
ekorina <alnitak.nik@gmail.com>
2025-09-21 10:16:34 +0200

    feat(vfx): Distort grid on block clearing.

--- godot/godot4/effects/BlockCleared.gd
+++ godot/godot4/effects/BlockCleared.gd
@@ -24,6 +24,7 @@ func _ready() -> void:
 	
 func refresh() -> void:
 	%Sprite2D.region_rect = Rect2(tile_size.x*(Block.BlockTile.TILEMAP_START_X + color + (Block.BlockTile.COLORS if liquid else 0)), 0, tile_size.x, tile_size.y)
-
+	%GravitonField.position = Vector2(tile_size.x/2.0, tile_size.y/2.0)
+	 
 func notify_done() -> void:
 	done.emit()
