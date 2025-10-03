1fd2328850d633e5195849eac06eeed6294a1122
Matteo Abrate <matteo.abrate@gmail.com>
2025-10-03 18:23:11 +0200

    block rotation area ux improvements

--- godot/godot4/elements/blocks/BlockRotationArea.gd
+++ godot/godot4/elements/blocks/BlockRotationArea.gd
@@ -10,3 +10,17 @@ func set_cw(v:bool) -> void:
 
 func is_cw() -> bool:
 	return cw
+
+func _process(delta: float) -> void:
+	var on = false
+	for area in get_overlapping_areas():
+		if area.get('host') is Ship and area.get('host').is_holding_block():
+			on = true
+			break
+		
+	if on:
+		%Arrow.modulate = Color(0,0,0,0.8)
+		%Background.modulate = Color('#ffbf8e14')
+	else:
+		%Background.modulate = Color('#ffaf5e10')
+		%Arrow.modulate = Color(0,0,0,0.3)
