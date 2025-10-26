24da8e7850b87596cd6d9f311a661e6104bff211
Nitaku <matteo.abrate@gmail.com>
2025-10-26 18:42:00 +0100

    fix: recovering hall of fame

--- godot/special_scenes/combat_UI/gameover/WinnerBanner.gd
+++ godot/special_scenes/combat_UI/gameover/WinnerBanner.gd
@@ -2,7 +2,7 @@ extends Control
 
 signal champion_has_a_name 
 
-var this_champion : InfoChampion # InfoChampion
+var this_champion : Dictionary # InfoChampion
 @export var minigame_logo : PackedScene
 
 func _ready():
@@ -15,28 +15,18 @@ func set_player_name(player_name: String):
 
 const CARD_POOL_PATH = "res://map/draft/pool"
 
-func set_player(champion: InfoChampion):
+func set_player(champion: Dictionary):
 	this_champion = champion
+	return 
 	$"%PlayerName".text = champion.player.username if champion.player.username else champion.player.id
-	var info_player = InfoPlayer.new()
-	info_player.set_from_dictionary(champion.player)
+	var info_player := Player.new() #InfoPlayer.new()
+	# info_player.set_from_dictionary(champion.player)
 	$"%Headshot".set_player(info_player)
-	var matches = champion.session_info.get("matches", [])
-	var all_cards = null
-	if global.the_game != null:
-		all_cards = global.the_game.all_cards
-	else:
-		all_cards = CardPool.new() 
-	for a_match in matches:
-		var logo = minigame_logo.instantiate()
-		var card_id = a_match["card_id"]
-		var card: DraftCard = all_cards.get_card(card_id)
-		logo.texture = card.get_logo()
-		$"%StarsContainer".add_child(logo)
+	
 	$"%DateSession".text = cleanup_datetime_str(this_champion.session_info.get("timestamp_local", this_champion.session_info.get("timestamp_local", this_champion.session_info.timestamp)))
-	$"%Background".modulate = global.get_species(champion.player.species).get_color()
-	$"%PlayerName".modulate = global.get_species(champion.player.species).get_color()
-	$"%InsertName".modulate = global.get_species(champion.player.species).get_color()
+	$"%Background".modulate = Settings.get_species(champion.player.species).get_color()
+	$"%PlayerName".modulate = Settings.get_species(champion.player.species).get_color()
+	$"%InsertName".modulate = Settings.get_species(champion.player.species).get_color()
 	$"%InsertName".placeholder_text = $"%PlayerName".text
 	
 func insert_name():
@@ -52,15 +42,15 @@ func _on_InsertName_name_inserted(player_name: String):
 	$"%PlayerName".visible = true
 	$"%HBoxContainer".queue_free()
 	$"%PlayerName".text = player_name
-	this_champion.player.username = player_name
-	this_champion.store()
-	var this_game : TheGame = global.get("the_game")
+	this_champion.get("player", {}).set("username", player_name)
+	"""
 	if this_game:
 		var players = this_game.get_players()
 		for player in players:
 			if player.id == this_champion.player.id:
 				player.username = player_name
 	emit_signal("champion_has_a_name")
+	"""
 
 func cleanup_datetime_str(datetime_str: String):
 	return datetime_str.replace("T", " ")
