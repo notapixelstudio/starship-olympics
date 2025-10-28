1a163bb0e7558516a4f48ee408ced94ff260dd26
ekorina <alnitak.nik@gmail.com>
2025-10-28 17:37:02 +0100

    Boss phase now sets the collision polygon correctly.

--- godot/godot4/actors/enemies/bosses/DiamondBoss.gd
+++ godot/godot4/actors/enemies/bosses/DiamondBoss.gd
@@ -9,3 +9,12 @@ func next_phase() -> void:
 	%Sprite2D.texture = textures[_phase]
 	%Tentacles.position.y -= 60
 	%Tentacles.scale = 0.65**(_phase-1)*Vector2(1,1)
+	
+	# enable just the collision polygon for the current phase
+	for node in get_children():
+		if node is CollisionPolygon2D:
+			if node.name.ends_with(str(_phase)):
+				node.disabled = false
+			else:
+				node.disabled = true
+			
