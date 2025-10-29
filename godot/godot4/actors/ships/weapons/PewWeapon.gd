b3694a6072a3714c646a9d86114c45b5243a9b3f
Nitaku <matteo.abrate@gmail.com>
2025-10-29 11:56:17 +0100

    dependency from Ship removed from Pew

--- godot/godot4/actors/ships/weapons/PewWeapon.gd
+++ godot/godot4/actors/ships/weapons/PewWeapon.gd
@@ -28,10 +28,11 @@ func fire(source):
 			aim_angle = (aim_correction*source.get_target_velocity().normalized() + (1-aim_correction)*Vector2.RIGHT.rotated(global_rotation)).angle() if source.get_target_velocity().length() > 0.6 else global_rotation
 			
 		var angle = aim_angle + ( -aperture/2 + i*aperture/(amount-1) if amount > 1 else 0)
-		var pew = pew_scene.instantiate()
+		var pew : Pew = pew_scene.instantiate()
 		pew.global_position = global_position + Vector2(120, 0).rotated(angle)
 		pew.linear_velocity = Vector2(2500, 0).rotated(angle)
-		pew.set_ship(source)
+		pew.set_color(source.get_color())
+		pew.set_team(source.get_team())
 		Events.spawn_request.emit(pew)
 		
 func get_aim_compensation_target():
