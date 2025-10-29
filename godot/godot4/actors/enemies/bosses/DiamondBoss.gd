55d4a428ab6adcbe2e6860f0882bd1372ab199bd
ekorina <alnitak.nik@gmail.com>
2025-10-29 10:16:06 +0100

    Boss BGM + animation attempts.

--- godot/godot4/actors/enemies/bosses/DiamondBoss.gd
+++ godot/godot4/actors/enemies/bosses/DiamondBoss.gd
@@ -4,9 +4,16 @@ extends StaticBody2D
 
 var _phase := 1
 
-signal ready_for_next_phase
+signal _ready_for_next_phase
+signal next_phase_ready
 
 func next_phase() -> void:
+	await _ready_for_next_phase
+	
+	# create tween and use it to move the boss at the center of the arena
+	# wait for the tween to finish
+	await get_tree().create_tween().tween_property(self, "position", Vector2(0,0), 0.5).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE).finished
+
 	_phase += 1
 	%Sprite2D.texture = textures[_phase]
 	%Tentacles.position.y -= 60
@@ -19,6 +26,8 @@ func next_phase() -> void:
 				node.disabled = false
 			else:
 				node.disabled = true
-			
+				
+	%AnimationPlayer.play("Phase"+str(_phase))
+	
 func _notify_ready_for_next_phase() -> void:
-	ready_for_next_phase.emit()
+	_ready_for_next_phase.emit()
