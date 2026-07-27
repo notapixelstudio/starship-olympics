7e703ddd4fe74ff051ab5fdf00c3b074df7ab152
nitaku <matteo.abrate@gmail.com>
2026-07-27 21:06:20 +0200

    stub for minigame selection screen

---
+++ godot/godot4/ui/screens/minigame_selection/MinigameSelectionScreen.gd
@@ -0,0 +1,31 @@
+extends BackScreen
+
+@export var next_scene : PackedScene
+var _selection_completed := false
+
+#func enter():
+	#super.enter()
+	#_selection_completed = false
+	#%SelectionPanel.reset_ready_pilots()
+	#%SelectionPanel.enable()
+#
+#func exiting():
+	#%SelectionPanel.disable()
+	#super.exiting()
+	#
+#func _on_SelectionPanel_selection_completed():
+	#_selection_completed = true
+	#next.emit(next_scene.instantiate())
+	#
+#func exited():
+	#super.exited()
+	#if _selection_completed:
+		#var players = %SelectionPanel.get_players_data()
+		## put all players into the same team
+		#for player in players:
+			#player.set_team('GG')
+		#Events.campaign_game_start.emit(players)
+
+
+func _on_start_button_pressed() -> void:
+	next.emit(next_scene.instantiate())
