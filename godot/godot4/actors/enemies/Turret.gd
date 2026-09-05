4d7676bab7e364ec08fcf6bf19f5ab663d0908bc
nitaku <matteo.abrate@gmail.com>
2026-09-05 22:09:50 +0200

    improved take the crown level design for 1pve

--- godot/godot4/actors/enemies/Turret.gd
+++ godot/godot4/actors/enemies/Turret.gd
@@ -3,10 +3,12 @@ class_name Turret
 
 @export var enabled := true
 @export var wait_time := 2.0 : set = set_time
+@export var time_jitter := 0.0
 
 @export_group("Burst")
 @export var burst_shots := 1
 @export var burst_time := 0.2
+@export var burst_time_jitter := 0.0
 
 # Ship-like "interface"
 signal tap
@@ -15,6 +17,8 @@ func get_target_velocity() -> Vector2:
 	return Vector2.ZERO
 	
 # end
+
+signal alert_ended
 
 var _burst_count := 0
 
@@ -33,10 +37,10 @@ func _on_timer_timeout() -> void:
 		_burst_count += 1
 		
 	if _burst_count >= burst_shots:
-		%Timer.start(wait_time)
+		%Timer.start(wait_time+randf()*time_jitter)
 		_burst_count = 0
 	else:
-		%Timer.start(burst_time)
+		%Timer.start(burst_time+randf()*burst_time_jitter)
 	
 # TODO design how to handle color for rogue team
 func get_color() -> Color:
@@ -44,3 +48,8 @@ func get_color() -> Color:
 	
 func get_team() -> String:
 	return 'rogue'
+
+func alert():
+	%AnimationPlayer.play("Alert")
+	await %AnimationPlayer.animation_finished
+	alert_ended.emit()
