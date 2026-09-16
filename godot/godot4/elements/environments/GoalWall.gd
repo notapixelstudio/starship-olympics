50434b3a9c1e344e34470ccad11837a539e2a05f
nitaku <matteo.abrate@gmail.com>
2026-09-16 23:56:18 +0200

    improved test level

--- godot/godot4/elements/environments/GoalWall.gd
+++ godot/godot4/elements/environments/GoalWall.gd
@@ -1,8 +1,25 @@
 @tool
 extends "res://godot4/elements/environments/Wall.gd"
 
+var _on := false
+
+func _ready():
+	super()
+	_turn_on()
+	
+func _turn_on() -> void:
+	_on = true
+	%AnimationPlayer.stop(true)
+	%AnimationPlayer.play("up")
+
 func hit(sth:Ball) -> void:
-	Events.score.emit(1, sth.get_owner_ship(), sth.global_position)
-	%AnimationPlayer.stop()
-	%AnimationPlayer.play("hit")
-	sth.apply_central_impulse(1000*sth.linear_velocity.normalized())
+	if _on:
+		_on = false
+		Events.score.emit(1, sth.get_owner_ship(), sth.global_position)
+		%AnimationPlayer.stop(true)
+		%AnimationPlayer.play("hit")
+		sth.apply_central_impulse(1000*sth.linear_velocity.normalized())
+		%Timer.start()
+		
+func _on_timer_timeout() -> void:
+	_turn_on()
