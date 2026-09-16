b3655b1fad16fb3e20133e7b4b81b100c7030855
ekorina <alnitak.nik@gmail.com>
2026-09-16 14:02:13 +0200

    Values tweaked to kick the ball with more strength when TAPping rapidly.

--- godot/godot4/actors/managers/CargoManager.gd
+++ godot/godot4/actors/managers/CargoManager.gd
@@ -72,7 +72,7 @@ func kick_cargo(charge:float) -> void:
 	var compensated_angle = (host_forward*(1.0-COMPENSATION)+host_intended_forward*COMPENSATION).angle()
 	var spin = -(host_intended_forward.cross(host_forward))
 	
-	_launch_cargo(get_host().global_position, get_host().linear_velocity + Vector2(max(0,6000*(charge-0.05)),0).rotated(compensated_angle), compensated_angle, spin)
+	_launch_cargo(get_host().global_position, get_host().linear_velocity + Vector2(500+5500*charge,0).rotated(compensated_angle), compensated_angle, spin)
 
 func discard_cargo() -> void:
 	_launch_cargo(get_host().global_position, Vector2(200.0, 0).rotated(get_host().global_rotation), get_host().global_rotation)
